a7520a1124836b4cdb9f293015168317021371e1
rjkiv <rjkiv@users.noreply.github.com>
2023-09-23 14:19:24 -0700

    revert fn_8014F364 types

--- src/unknown/fn_8014F364.cpp
+++ src/unknown/fn_8014F364.cpp
@@ -1,7 +1,7 @@
 #include <unknown/fn_8014F364.h>
 
-void fn_8014F364(int32 param_1,int8 param_2){
-  *(int8 *)(param_1 + 0xe5) = 1;
-  *(int8 *)(param_1 + 0xe4) = param_2;
+void fn_8014F364(int param_1,char param_2){
+  *(char *)(param_1 + 0xe5) = 1;
+  *(char *)(param_1 + 0xe4) = param_2;
   return;
 }
