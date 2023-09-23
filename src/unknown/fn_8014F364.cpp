7b6b991a95730cc4639d6233da49f1a5a34279e5
rjkiv <rjkiv@users.noreply.github.com>
2023-09-23 14:17:57 -0700

    add fn_8014F364

---
+++ src/unknown/fn_8014F364.cpp
@@ -0,0 +1,7 @@
+#include <unknown/fn_8014F364.h>
+
+void fn_8014F364(int32 param_1,int8 param_2){
+  *(int8 *)(param_1 + 0xe5) = 1;
+  *(int8 *)(param_1 + 0xe4) = param_2;
+  return;
+}
---
+++ src/unknown/fn_8014F364.h
@@ -0,0 +1,3 @@
+#include <types.h>
+
+void fn_8014F364(int param_1,char param_2);
