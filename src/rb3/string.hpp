69f214b2cc186fdaeb8fc32ace019beb2e51c79f
rjkiv <rjkiv@users.noreply.github.com>
2023-09-28 14:33:19 -0700

    more string methods

--- src/rb3/string.hpp
+++ src/rb3/string.hpp
@@ -8,13 +8,26 @@ public:
 	char *text;
 	String();
 	String(const char *);
+	String(const char**); // fn_80361BC0
 	String(const String &);
+	String(unsigned int, char); // fn_80361D1C
 
+	unsigned int GetTextLength(); // fn_800A6E18
 	const char *GetText() const; // fn_8000DB9C
-	// void fn_80362260(int); // fn_80362260
-	// void fn_80361C90(int); // fn_80361C90
+	void fn_80362260(unsigned int); // fn_80362260
+	void Reserve(unsigned int); // fn_80361C90
+	void fn_80361F04();
 
-	void operator=(const char *);
+	bool AreStringsDifferent(const char*); // fn_80362144
+	bool AreStringsDifferent(const String*); // fn_80362188
+	int FindIndexOfSubstring(const char*, int); // fn_803622F8
+	int FindLastIndexOfChar(char); // fn_803623A0
+	int fn_803623E8(char*); // fn_803623E8
+	bool SubstrExistsInString(char*); // fn_80362530
+	void ReplaceCharsInString(char, char); // fn_80362778
+	int FindFirstIndexOfCharAtOffset(char, int); // fn_803622A4
+
+	String* operator=(const char *);
 	void ToLower();
 	virtual ~String();
 	virtual void fn_80361E34();
---
+++ src/rb3/unk_800A53B0.cpp
@@ -0,0 +1,10 @@
+#include "string.hpp"
+#include "textstream.hpp"
+#include "unknown.hpp"
+#include "std/string.h"
+
+// fn_800A6E18
+// probably inline
+unsigned int String::GetTextLength(){
+    return strlen(text);
+}
