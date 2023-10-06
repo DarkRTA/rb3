20922f0ab0f0d55ec32e966ec3c6a1373bce0a29
rjkiv <rjkiv@users.noreply.github.com>
2023-10-05 20:13:26 -0700

    textstream methods implemented

--- src/rb3/textstream.hpp
+++ src/rb3/textstream.hpp
@@ -1,11 +1,24 @@
 #ifndef RB3_TEXTSTREAM_HPP
 #define RB3_TEXTSTREAM_HPP
+#include "symbol.hpp"
 
 class TextStream {
 public:
 	TextStream();
-	virtual ~TextStream(); // fn_8000DD10
+	virtual ~TextStream(); // fn_8000DD10 - generic dtor fn
 	virtual void Print(const char*) = 0;
+
+	TextStream* operator<<(char); // fn_8037A68C
+	TextStream* operator<<(int); // fn_8037A6EC
+	TextStream* operator<<(long); // fn_8037A750
+	TextStream* operator<<(unsigned int); // fn_8037A7B4
+	TextStream* operator<<(unsigned short); // fn_8037A818
+	TextStream* operator<<(unsigned long); // fn_8037A87C
+	TextStream* operator<<(float); // fn_8037A8E0
+	TextStream* operator<<(const char*); // fn_8037A940
+	TextStream* operator<<(Symbol); // fn_8037A97C
+	TextStream* operator<<(bool); // fn_8037A9BC
+	void Space(int); // fn_8037AA10
 };
 
 #endif
--- src/rb3/unk_8037A49C.cpp
+++ src/rb3/unk_8037A49C.cpp
@@ -2,13 +2,109 @@
 #include "textfilestream.hpp"
 #include "textstream.hpp"
 #include "unknown.hpp"
+#include "std/string.h"
+#include "PowerPC_EABI_Support/MSL_C/MSL_Common/printf.h"
+
+// fn_8037A58C - TextFileStream ctor
+TextFileStream::TextFileStream(const char* c, bool b){
+    
+}
+
+// fn_8037A5F8
+void TextFileStream::Print(const char* c){
+    // while(*c != '\0'){
+    //     if(*c == '\n'){
+    //         if(c[1] == '\r') 
+    //     }
+    // }
+}
 
 // fn_8037A67C
 TextStream::TextStream()
 {
 }
 
-// fn_8037A58C - TextFileStream ctor
-TextFileStream::TextFileStream(){
-    
+extern char lbl_80874520[]; // C string modifiers
+
+// fn_8037A68C
+TextStream* TextStream::operator<<(char c){
+    char stack[1024];
+    snprintf(stack, 0x400, &lbl_80874520[0x0], c);
+    Print(stack);
+    return this;
+}
+
+// fn_8037A6EC
+TextStream* TextStream::operator<<(int i){
+    char stack[1024];
+    snprintf(stack, 0x400, &lbl_80874520[0x7], i);
+    Print(stack);
+    return this;
+}
+
+// fn_8037A750
+TextStream* TextStream::operator<<(long l){
+    char stack[1024];
+    snprintf(stack, 0x400, &lbl_80874520[0xa], l);
+    Print(stack);
+    return this;
+}
+
+// fn_8037A7B4
+TextStream* TextStream::operator<<(unsigned int ui){
+    char stack[1024];
+    snprintf(stack, 0x400, &lbl_80874520[0xe], ui);
+    Print(stack);
+    return this;
+}
+
+// fn_8037A818
+TextStream* TextStream::operator<<(unsigned short us){
+    char stack[1024];
+    snprintf(stack, 0x400, &lbl_80874520[0x11], us);
+    Print(stack);
+    return this;
+}
+
+// fn_8037A87C
+TextStream* TextStream::operator<<(unsigned long ul){
+    char stack[1024];
+    snprintf(stack, 0x400, &lbl_80874520[0x15], ul);
+    Print(stack);
+    return this;
+}
+
+// fn_8037A8E0
+TextStream* TextStream::operator<<(float f){
+    char stack[1024];
+    snprintf(stack, 0x400, &lbl_80874520[0x19], f);
+    Print(stack);
+    return this;
+}
+
+// fn_8037A940
+TextStream* TextStream::operator<<(const char* c){
+    Print(c);
+    return this;
+}
+
+// fn_8037A97C
+TextStream* TextStream::operator<<(Symbol s){
+    Print(s.m_string);
+    return this;
+}
+
+// fn_8037A9BC
+TextStream* TextStream::operator<<(bool b){
+    char* str = b ? &lbl_80874520[0x23] : &lbl_80874520[0x28];
+    Print(str);
+    return this;
+}
+
+// fn_8037AA10
+void TextStream::Space(int i){
+    while(i != 0){
+        Print(&lbl_80874520[0x2e]);
+        i--;
+    }
 }
