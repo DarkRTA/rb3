a625200dbeffedadd6f86f00cfd4f9c077a8e0ff
rjkiv <rjkiv@users.noreply.github.com>
2023-10-10 17:46:29 -0700

    more file ops

--- src/rb3/unk_802FA994.cpp
+++ src/rb3/unk_802FA994.cpp
@@ -49,3 +49,35 @@ char* fn_802FAAC0(char* arg){
     else strcpy(lbl_809074C8, arg);
     return lbl_809074C8;
 }
+
+extern "C" bool fn_802FAE68(char*, char*, bool);
+extern "C" bool fn_802FAF0C(char*, char*);
+
+// fn_802FAE68
+bool fn_802FAE68(char* arg0, char* arg1, bool arg2){
+    for(; *arg0 != 0; arg0++){
+        if(fn_802FAF0C(arg0, arg1)) return true;
+        if(!arg2 && (*arg0 == '/' || *arg0 == '\\'))  return false;
+    }
+    return (*arg1 == *arg0);
+}
+
+// fn_802FAF0C
+bool fn_802FAF0C(char* param1, char* param2){
+    if(param2 == 0) return false;
+    while(*param2 != '\0') {
+        if(*param2 == '*') return fn_802FAE68(param1, param2 + 1, 0);
+        if(*param2 == '&') return fn_802FAE68(param1, param2 + 1, 1);
+        if(*param1 == '\0') break;
+        if(*param2 == '?'){
+            if((*param1 == '\\') || (*param1 == '/')) return 0;
+        }
+        else if((*param2 == '/') || (*param2 == '\\')){
+            if((*param1 != '/') && (*param1 != '\\')) return 0;
+        }
+        else if(*param2 != *param1) return 0;
+        param2++;
+        param1++;
+    }
+    return (*param2 - *param1) == 0;
+}
