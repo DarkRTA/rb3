033b037033b00b85f2273ff9e0f734a6bab79197
rjkiv <rjkiv@users.noreply.github.com>
2023-10-11 17:28:04 -0700

    filecache methods

--- src/rb3/unk_802FB548.cpp
+++ src/rb3/unk_802FB548.cpp
@@ -1,7 +1,15 @@
 #include "std/string.h"
 #include "unknown.hpp"
+#include "filecachefile.hpp"
 
 // fn_802FB6BC
 void FileQualifiedFilename(char* a, int b, const char* c){
     strcpy(a, c);
+}
+
+// fn_802FB6C4 - FileCacheFile : File ctor
+
+// fn_802FB744 - FileCacheFile : File dtor
+FileCacheFile::~FileCacheFile(){
+
 }
