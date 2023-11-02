8d649327bdb9eb4e8d80a088412a51049ac90e28
rjkiv <rjkiv@users.noreply.github.com>
2023-11-01 18:24:58 -0700

    add placement new code

--- src/rb3/unk_80313E98.cpp
+++ src/rb3/unk_80313E98.cpp
@@ -118,8 +118,7 @@ extern DataNode* NodesAlloc(int);
 DataArray::DataArray(int i) : symbol(), mNodeCount(i), mRefCount(1), mLine(0), mUnknown(0) {
     mNodes = NodesAlloc(i * sizeof(DataNode));
     for(int n = 0; n < i; n++){
-        DataNode* dn = &mNodes[n];
-        if(dn != nullptr) dn = new DataNode();
+        new (&mNodes[n]) DataNode();
     }
 }
 
--- src/rb3/unk_80322664.cpp
+++ src/rb3/unk_80322664.cpp
@@ -1,22 +1,19 @@
 #include "data.hpp"
 #include "string.hpp"
 #include "std/string.h"
+#include "common.hpp"
 
 extern DataArray* fn_8035CF9C(int, int, int);
 
 // fn_803231CC
 DataNode::DataNode(const char* c){
-    DataArray* da = DataArray::fn_8035CF9C(0x10, 0x10, 1);
-    da = new DataArray(c, strlen(c) + 1);
-    value.dataArray = da;
+    value.dataArray = new (DataArray::fn_8035CF9C(0x10, 0x10, 1)) DataArray(c, strlen(c) + 1);
     type = STRING_VALUE;
 }
 
 // fn_8032324C
 DataNode::DataNode(const String& s){
-    DataArray* da = fn_8035CF9C(0x10, 0x10, 1);
-    da = new DataArray(s.c_str(), s.length() + 1);
-    value.dataArray = da;
+    value.dataArray = new (DataArray::fn_8035CF9C(0x10, 0x10, 1)) DataArray(s.c_str(), s.length() + 1);
     type = STRING_VALUE;
 }
 
