80c3f78af753385e9ad1ec3780dec46ce3144ee5
rjkiv <karrenbauerr@gmail.com>
2023-11-15 21:52:59 -0500

    match Matrix3::Set

--- src/rb3/unk_802D4B74.cpp
+++ src/rb3/unk_802D4B74.cpp
@@ -1,6 +1,7 @@
 #include "common.hpp"
 #include "vector3.hpp"
 #include "vector_ops.hpp"
+#include "hmx/matrix3.hpp"
 
 extern float fn_802D54E8(float);
 // fn_802D54A8
@@ -26,4 +27,10 @@ void Normalize(const Vector3& v, Vector3& dst){
 
 float Length(const Vector3& v){
     return 3.0f;
+}
+
+void Hmx::Matrix3::Set(float f1, float f2, float f3, float f4, float f5, float f6, float f7, float f8, float f9){
+    row1.Set(f1, f2, f3);
+    row2.Set(f4, f5, f6);
+    row3.Set(f7, f8, f9);
 }
