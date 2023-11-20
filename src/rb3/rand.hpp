32c5506f02a1ef2104a9f29e3eb0f31989c71beb
rjkiv <karrenbauerr@gmail.com>
2023-11-20 16:50:06 -0500

    Rand::Int() matched

--- src/rb3/rand.hpp
+++ src/rb3/rand.hpp
@@ -5,6 +5,11 @@ class Rand {
 public:
 	Rand(int); // fn_802DD9A4
 	void Seed(int); // fn_802DDA04
+	int Int();
+
+	unsigned int unk00;
+	unsigned int unk04;
+	unsigned int unk_arr[0x100];
 };
 
 #endif
--- src/rb3/unk_802DCEDC.cpp
+++ src/rb3/unk_802DCEDC.cpp
@@ -3,9 +3,33 @@
 
 extern unsigned int gMainThreadID;
 extern "C" unsigned int OSGetCurrentThread();
+extern Rand gRand;
 
 bool MainThread(){
     bool ret = true;
     if((gMainThreadID != 0) && (gMainThreadID != OSGetCurrentThread())) ret = false;
     return ret;
+}
+
+void Rand::Seed(int seed){
+    for(int i = 0; i < 0x100; i++){
+        int j = seed * 0x41C64E6D + 0x3039;
+        seed = j * 0x41C64E6D + 0x3039;
+        unk_arr[i] = (seed & 0x7FFF0000U);
+    }
+    unk00 = 0;
+    unk04 = 0x67;
+}
+
+void SeedRand(int seed){
+    gRand.Seed(seed);
+}
+
+int Rand::Int(){
+    unsigned int u3 = unk_arr[unk00];
+    unsigned int u1 = unk_arr[unk04];
+    unk_arr[unk00] = u3 ^ u1;
+    if(0xF9 <= ++unk00) unk00 = 0;
+    if(0xF9 <= ++unk04) unk04 = 0;
+    return u3 ^ u1;
 }
