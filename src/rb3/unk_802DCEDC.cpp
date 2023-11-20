99df69036082ab63ca6da49202620aaa2d36667f
rjkiv <karrenbauerr@gmail.com>
2023-11-20 15:34:34 -0500

    MainThread

--- src/rb3/unk_802DCEDC.cpp
+++ src/rb3/unk_802DCEDC.cpp
@@ -1,2 +1,11 @@
 #include "rand.hpp"
 #include "common.hpp"
+
+extern unsigned int gMainThreadID;
+extern "C" unsigned int OSGetCurrentThread();
+
+bool MainThread(){
+    bool ret = true;
+    if((gMainThreadID != 0) && (gMainThreadID != OSGetCurrentThread())) ret = false;
+    return ret;
+}
