c7d7d9379d5f7860dffd05334bef7863c8bf0d90
rjkiv <karrenbauerr@gmail.com>
2023-11-22 11:04:44 -0500

    move object to hmx folder

---
+++ src/rb3/hmx/object.hpp
@@ -0,0 +1,13 @@
+#ifndef HMX_OBJECT_HPP
+#define HMX_OBJECT_HPP
+#include "rb3/objref.hpp"
+
+namespace Hmx {
+	class Object : ObjRef {
+	public:
+		Object(); // fn_8033560c
+		virtual ~Object(); // fn_803356ec
+	};
+}
+
+#endif
