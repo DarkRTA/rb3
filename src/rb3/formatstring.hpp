2d3d01bbc51e0e8039409880d65cf6b1dbc412c6
rjkiv <karrenbauerr@gmail.com>
2023-11-24 10:58:07 -0500

    UpdateType matched

--- src/rb3/formatstring.hpp
+++ src/rb3/formatstring.hpp
@@ -3,15 +3,17 @@
 
 class FormatString {
 public:
-	char* unk0;
+	char* format;
 	int unk4;
 	int unk8;
 	char unk_arr[0x800];
-	int unk80c;
+	char* unk80c;
+    int formatType; // 0 = integer, 1 = string, 2 = float, 3 = unknown
 
 	FormatString(const char*);
 	void InitArray(const char*, bool);
 	void UpdateType();
+	FormatString& operator<<(unsigned int);
 };
 
 #endif
--- src/rb3/unk_80350BA0.cpp
+++ src/rb3/unk_80350BA0.cpp
@@ -1,6 +1,7 @@
 #include "formatstring.hpp"
 #include "common.hpp"
 #include "std/string.h"
+#include "PowerPC_EABI_Support/MSL_C/MSL_Common/printf.h"
 
 extern int NextBuf();
 
@@ -13,7 +14,51 @@ FormatString::FormatString(const char* str){
 
 void FormatString::InitArray(const char* cc, bool b){
     if(!StrNCopy(unk_arr, cc, 0x800)) strlen(cc);
-    unk0 = &unk_arr[0];
+    format = &unk_arr[0];
     if(b) UpdateType();
-    
+}
+
+void FormatString::UpdateType(){
+    if(unk80c != 0){
+        format = unk80c;
+        unk80c = 0;
+    }
+
+    int val = 0;
+    char* ptr = format;
+    for(; *ptr != '\0'; ptr++){
+        if(*ptr == '%'){
+            if(ptr[1] == '%') ptr++; // '%' escape
+            else if(val != 0) break;
+            else val = 1;
+        }
+        else if (val == 1)
+        {
+            if (*ptr < 'A' || *ptr > 'z') continue;
+
+            if (*ptr == 'f' || *ptr == 'g'){
+                formatType = 2;
+            }
+            else if (*ptr == 's'){
+                formatType = 1;
+            } else {
+                formatType = 0;
+            }
+
+            val = 2;
+        }
+    }
+
+    if(val == 0) formatType = 3;
+    unk80c = ptr;
+} 
+
+FormatString& FormatString::operator<<(unsigned int ui){
+    char tmp_char = *unk80c;
+    *unk80c = '\0';
+    int snp = snprintf(&unk_arr[unk4 + 0x800 - unk8], unk8, format, ui);
+    *unk80c = tmp_char;
+    unk8 -= snp;
+    UpdateType();
+    return *this;
 }
