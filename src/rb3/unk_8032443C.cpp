a9db752d6b611b57f67c1842418b346912601f6b
rjkiv <karrenbauerr@gmail.com>
2023-11-26 12:11:34 -0500

    begin DataPushVar

--- src/rb3/unk_8032443C.cpp
+++ src/rb3/unk_8032443C.cpp
@@ -6,3 +6,10 @@ extern Hmx::Object* gDataThis;
 Hmx::Object* DataThis(){
     return gDataThis;
 }
+
+extern DataNode* gVarStackPtr;
+
+void DataPushVar(DataNode* dn){
+    *gVarStackPtr = *dn;
+    *(++gVarStackPtr) = *dn;
+}
