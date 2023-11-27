3f07720d64e3fa473a0477fcf11161f403c69916
rjkiv <karrenbauerr@gmail.com>
2023-11-26 22:02:57 -0500

    DataMergeTags

--- src/rb3/unk_8032443C.cpp
+++ src/rb3/unk_8032443C.cpp
@@ -2,6 +2,26 @@
 #include "hmx/object.hpp"
 #include "varstack.hpp"
 
+void DataMergeTags(DataArray* da, DataArray* db){
+    if(da == 0 || db == 0 || db == da){
+        return;
+    }
+    else for(int i = 0; i < db->GetNodeCount(); i++){
+        DataNode* dn_b = db->GetNodeAtIndex(i);
+        if(dn_b->GetType() == 0x10){
+            DataArray* inner_arr = dn_b->value.dataArray;
+            if(inner_arr->GetNodeCount() != 0){
+                DataArray* found = da->FindArray(inner_arr->GetDataNodeValueAtIndex(0).intVal, false);
+                if(found == 0){
+                    da->Resize(da->GetNodeCount() + 1);
+                    *(da->GetNodeAtIndex(da->GetNodeCount() - 1)) = DataNode(inner_arr, kDataArray);
+                }
+                else DataMergeTags(found, inner_arr);
+            }
+        }
+    }
+}
+
 extern Hmx::Object* gDataThis;
 
 Hmx::Object* DataThis(){
