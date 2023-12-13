0a060c13eec9ee625a59079824a2d45ac8893621
Nathan <nathanhurst03@gmail.com>
2023-12-12 20:13:56 -0700

    Fix json-c warnings - Remove concat sequence in debug macros - Remove size_t from types.h, already defined by stddef.h

--- src/types.h
+++ src/types.h
@@ -13,7 +13,6 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int uint;
 typedef unsigned long u32;
-typedef unsigned long size_t;
 typedef unsigned long long u64;
 
 typedef volatile u8 vu8;
