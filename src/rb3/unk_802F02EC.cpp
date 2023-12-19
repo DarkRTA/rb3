f733eb7dddd9ea2972730fd4938587af04b5aca2
rjkiv <karrenbauerr@gmail.com>
2023-12-19 12:56:33 -0500

    more msg symbols

--- src/rb3/unk_802F02EC.cpp
+++ src/rb3/unk_802F02EC.cpp
@@ -1,5 +1,15 @@
 #include "storagechangedmsg.hpp"
+#include "contentinstalledmsg.hpp"
 #include "symbol.hpp"
+
+ContentInstalledMsg::ContentInstalledMsg(DataArray* da) : Message(da) {
+
+}
+
+Symbol ContentInstalledMsg::Type(){
+    static Symbol t("content_installed");
+    return t;
+}
 
 StorageChangedMsg::StorageChangedMsg(DataArray* da) : Message(da) {
 
