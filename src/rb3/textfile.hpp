44c99330de961bc6baf8738b6a13d949fa378f84
rjkiv <karrenbauerr@gmail.com>
2023-12-27 12:46:31 -0500

    textfile symbols

--- src/rb3/textfile.hpp
+++ src/rb3/textfile.hpp
@@ -7,14 +7,14 @@
 class TextFile : public Hmx::Object, TextStream {
 public:
     int unk20;
-    
+
     TextFile();
+    virtual ~TextFile();
+    virtual Symbol ClassName() const; // fn_8033CE5C
+    virtual void SetType(Symbol); // fn_8033CE60
+    virtual DataNode Handle(DataArray*, bool); // fn_8033CA48
+    virtual void SetName(const char*, ObjectDir*); // fn_8033C93C
+    virtual void Print(const char *); // fn_8033C990
 };
 
-// struct TextFile : TextStream {
-//     virtual void virt1();
-//     int unk20;
-//     TextFile();
-//     static Symbol StaticClassName();
-// };
 #endif
