5c88bbb22ff90d85389ea31c8851822b5c4a2f8b
rjkiv <76180273+rjkiv@users.noreply.github.com>
2023-12-29 09:36:21 -0500

    Tweak RG and key messages to match other message syntax in the code (#54)
    * instrument handle symbols
    
    * a bunch more handle methods
    
    * replace generic Hmx Object dtor
    
    * change RG and key symbols to match the other Type symbols
    
    * refactor Type functions
    
    * change Type functions
    
    * add guard and local symbols for rg
    
    * add key guard and local symbols
    
    * more msg ctors
    
    * user symbols
    
    * hxguid symbols
    
    * fix hxguid dupe ctors
    
    * add character RTTI

--- src/rb3/key_messages.hpp
+++ src/rb3/key_messages.hpp
@@ -6,6 +6,7 @@ class KeysAccelerometerMsg : Message {
 public:
     KeysAccelerometerMsg(int, int, int, int); // fn_803157A8
     virtual ~KeysAccelerometerMsg(); // fn_800A7FF4
+    static Symbol Type();
 };
 
 class KeyboardConnectedAccessoriesMsg : Message {
@@ -13,48 +14,56 @@ public:
     KeyboardConnectedAccessoriesMsg(int, int); // fn_80315520
     virtual ~KeyboardConnectedAccessoriesMsg(); // fn_800A7FF4
     virtual void Unk1() = 0;
+    static Symbol Type();
 };
 
 class KeyboardExpressionPedalMsg : Message {
 public:
     KeyboardExpressionPedalMsg(int, int); // fn_80315448
     virtual ~KeyboardExpressionPedalMsg(); // fn_800A7FF4
+    static Symbol Type();
 };
 
 class KeyboardHighHandPlacementMsg : Message {
 public:
     KeyboardHighHandPlacementMsg(int, int); // fn_80315998
     virtual ~KeyboardHighHandPlacementMsg(); // fn_800A7FF4
+    static Symbol Type();
 };
 
 class KeyboardKeyReleasedMsg : Message {
 public:
     KeyboardKeyReleasedMsg(int, int); // fn_80315298
     virtual ~KeyboardKeyReleasedMsg(); // fn_800A7FF4
+    static Symbol Type();
 };
 
 class KeyboardLowHandPlacementMsg : Message {
 public:
     KeyboardLowHandPlacementMsg(int, int); // fn_803158C0
     virtual ~KeyboardLowHandPlacementMsg(); // fn_800A7FF4
+    static Symbol Type();
 };
 
 class KeyboardModMsg : Message {
 public:
     KeyboardModMsg(int, int); // fn_80315370
     virtual ~KeyboardModMsg(); // fn_800A7FF4
+    static Symbol Type();
 };
 
 class KeyboardStompBoxMsg : Message {
 public:
     KeyboardStompBoxMsg(int, int); // fn_803156D0
     virtual ~KeyboardStompBoxMsg(); // fn_800A7FF4
+    static Symbol Type();
 };
 
 class KeyboardSustainMsg : Message {
 public:
     KeyboardSustainMsg(int, int); // fn_803155F8
     virtual ~KeyboardSustainMsg(); // fn_800A7FF4
+    static Symbol Type();
 };
 
 #endif
--- src/rb3/rg_messages.cpp
+++ src/rb3/rg_messages.cpp
@@ -1,132 +1,125 @@
 #include "data.hpp"
 #include "symbol.hpp"
 #include "stringstrummedmsg.hpp"
-#include "rgaccelerometermsg.hpp"
-#include "rgconnectedaccessoriesmsg.hpp"
-#include "rgpitchbendmsg.hpp"
-#include "rgmutingmsg.hpp"
-#include "rgstompboxmsg.hpp"
-#include "rgprogramchangemsg.hpp"
-#include "rgswingmsg.hpp"
-#include "rgfretbuttondownmsg.hpp"
-#include "rgfretbuttonupmsg.hpp"
-
-// fn_80313F64
-Symbol GetStringStrummedSymbol() {
-    static Symbol lbl_8091A254("string_strummed");
-    return lbl_8091A254;
-}
+#include "stringstoppedmsg.hpp"
+#include "rg_messages.hpp"
 
 // fn_80313E98
 StringStrummedMsg::StringStrummedMsg(int i1, int i2, int i3, int i4)
     : Message(
-        GetStringStrummedSymbol(), DataNode(i1), DataNode(i2), DataNode(i3), DataNode(i4)
+        Type(), DataNode(i1), DataNode(i2), DataNode(i3), DataNode(i4)
     ) {
 }
 
-// fn_80313FB0
-Symbol GetStringStoppedSymbol() {
-    static Symbol lbl_8091A258("string_stopped");
-    return lbl_8091A258;
+// fn_80313F64
+Symbol StringStrummedMsg::Type() {
+    static Symbol t("string_strummed");
+    return t;
 }
 
-// fn_803140C8
-Symbol GetRGAccelerometerSymbol() {
-    static Symbol lbl_8091A25C("rg_accelerometer");
-    return lbl_8091A25C;
+// fn_80313FB0
+Symbol StringStoppedMsg::Type() {
+    static Symbol t("string_stopped");
+    return t;
 }
 
 // fn_80313FFC
 RGAccelerometerMsg::RGAccelerometerMsg(int i1, int i2, int i3, int i4)
     : Message(
-        GetRGAccelerometerSymbol(), DataNode(i1), DataNode(i2), DataNode(i3), DataNode(i4)
+        Type(), DataNode(i1), DataNode(i2), DataNode(i3), DataNode(i4)
     ) {
 }
 
-// fn_803141A0
-Symbol GetRGConnectedAccessoriesSymbol() {
-    static Symbol lbl_8091A260("rg_connected_accessories");
-    return lbl_8091A260;
+// fn_803140C8
+Symbol RGAccelerometerMsg::Type() {
+    static Symbol t("rg_accelerometer");
+    return t;
 }
 
 // fn_80314114
 RGConnectedAccessoriesMsg::RGConnectedAccessoriesMsg(int i1, int i2)
-    : Message(GetRGConnectedAccessoriesSymbol(), DataNode(i1), DataNode(i2)) {
+    : Message(Type(), DataNode(i1), DataNode(i2)) {
 }
 
-// fn_80314278
-Symbol GetRGPitchBendSymbol() {
-    static Symbol lbl_8091A264("rg_pitch_bend");
-    return lbl_8091A264;
+// fn_803141A0
+Symbol RGConnectedAccessoriesMsg::Type() {
+    static Symbol t("rg_connected_accessories");
+    return t;
 }
 
 // fn_803141EC
 RGPitchBendMsg::RGPitchBendMsg(int i1, int i2)
-    : Message(GetRGPitchBendSymbol(), DataNode(i1), DataNode(i2)) {
+    : Message(Type(), DataNode(i1), DataNode(i2)) {
 }
 
-// fn_80314350
-Symbol GetRGMutingSymbol() {
-    static Symbol lbl_8091A268("rg_muting");
-    return lbl_8091A268;
+// fn_80314278
+Symbol RGPitchBendMsg::Type() {
+    static Symbol t("rg_pitch_bend");
+    return t;
 }
 
 // fn_803142C4
 RGMutingMsg::RGMutingMsg(int i1, int i2)
-    : Message(GetRGMutingSymbol(), DataNode(i1), DataNode(i2)) {
+    : Message(Type(), DataNode(i1), DataNode(i2)) {
 }
 
-// fn_80314428
-Symbol GetRGStompBoxSymbol() {
-    static Symbol lbl_8091A26C("rg_stomp_box");
-    return lbl_8091A26C;
+// fn_80314350
+Symbol RGMutingMsg::Type() {
+    static Symbol t("rg_muting");
+    return t;
 }
 
 // fn_8031439C
 RGStompBoxMsg::RGStompBoxMsg(int i1, int i2)
-    : Message(GetRGStompBoxSymbol(), DataNode(i1), DataNode(i2)) {
+    : Message(Type(), DataNode(i1), DataNode(i2)) {
 }
 
-// fn_80314500
-Symbol GetRGProgramChangeSymbol() {
-    static Symbol lbl_8091A270("rg_program_change");
-    return lbl_8091A270;
+// fn_80314428
+Symbol RGStompBoxMsg::Type() {
+    static Symbol t("rg_stomp_box");
+    return t;
 }
 
 // fn_80314474
 RGProgramChangeMsg::RGProgramChangeMsg(int i1, int i2)
-    : Message(GetRGProgramChangeSymbol(), DataNode(i1), DataNode(i2)) {
+    : Message(Type(), DataNode(i1), DataNode(i2)) {
 }
 
-// fn_803145D8
-Symbol GetRGSwingSymbol() {
-    static Symbol lbl_8091A274("rg_swing");
-    return lbl_8091A274;
+// fn_80314500
+Symbol RGProgramChangeMsg::Type() {
+    static Symbol t("rg_program_change");
+    return t;
 }
 
 // fn_8031454C
 RGSwingMsg::RGSwingMsg(int i1, int i2)
-    : Message(GetRGSwingSymbol(), DataNode(i1), DataNode(i2)) {
+    : Message(Type(), DataNode(i1), DataNode(i2)) {
 }
 
-// fn_803146D0
-Symbol GetRGFretButtonDownSymbol() {
-    static Symbol lbl_8091A278("rg_fret_button_down");
-    return lbl_8091A278;
+// fn_803145D8
+Symbol RGSwingMsg::Type() {
+    static Symbol t("rg_swing");
+    return t;
 }
 
 // fn_80314624
 RGFretButtonDownMsg::RGFretButtonDownMsg(int i1, int i2, int i3)
-    : Message(GetRGFretButtonDownSymbol(), DataNode(i1), DataNode(i2), DataNode(i3)) {
+    : Message(Type(), DataNode(i1), DataNode(i2), DataNode(i3)) {
 }
 
-// fn_803147C8
-Symbol GetRGFretButtonUpSymbol() {
-    static Symbol lbl_8091A27C("rg_fret_button_up");
-    return lbl_8091A27C;
+// fn_803146D0
+Symbol RGFretButtonDownMsg::Type() {
+    static Symbol t("rg_fret_button_down");
+    return t;
 }
 
 // fn_8031471C
 RGFretButtonUpMsg::RGFretButtonUpMsg(int i1, int i2, int i3)
-    : Message(GetRGFretButtonUpSymbol(), DataNode(i1), DataNode(i2), DataNode(i3)) {
+    : Message(Type(), DataNode(i1), DataNode(i2), DataNode(i3)) {
+}
+
+// fn_803147C8
+Symbol RGFretButtonUpMsg::Type() {
+    static Symbol t("rg_fret_button_up");
+    return t;
 }
