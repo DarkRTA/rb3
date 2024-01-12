657e2b9878686ef0a7d157e48693cf2886dd95c6
rjkiv <rjkiv@users.noreply.github.com>
2024-01-12 12:58:37 -0700

    hash to six bits

--- src/rb3/bytegrinder.cpp
+++ src/rb3/bytegrinder.cpp
@@ -2,14 +2,13 @@
 #include "data.hpp"
 
 namespace {
-    int GetEncMethod(int i){
+    int GetEncMethod(int ver){
         int ret = 0;
-        if(i - 0xc > 1){
-            switch(i){
-                case 0xe: ret = 1; break;
-                case 0xf: ret = 2; break;
-                case 0x10: ret = 3; break;
-            }
+        switch(ver){
+            case 0xc: case 0xd: ret = 0; break;
+            case 0xe: ret = 1; break;
+            case 0xf: ret = 2; break;
+            case 0x10: ret = 3; break;
         }
         return ret;
     }
@@ -33,5 +32,25 @@ DataNode hashTo5Bits(DataArray *da) {
         return DataNode(kDataInt, 0);
     }
 
+    return DataNode(kDataInt, (void*)hashValue);
+}
+
+DataNode hashTo6Bits(DataArray* da) {
+    static int hashMapping[0x100];
+
+    int i = da->GetIntAtIndex(1) & 0xFF;
+    
+    bool hasEnoughElements = da->GetNodeCount() > 2;
+
+    int hashValue = hashMapping[i];
+
+    if (hasEnoughElements) {
+        i = da->GetIntAtIndex(1);
+        for (int idx = 0; idx < sizeof(hashMapping) / sizeof(*hashMapping); idx++) {
+            hashMapping[idx] = (i >> 2) & 0x3F;
+            i = (i * 0x19660D) + 0x3C6EF35F;
+        }
+        return DataNode(kDataInt, 0);
+    }
     return DataNode(kDataInt, (void*)hashValue);
 }
