b036f8e78ba36a2dd5cdb14c76a4a4c71cfcb6d9
rjkiv <rjkiv@users.noreply.github.com>
2024-01-12 13:59:15 -0700

    random sequence 32a

--- src/rb3/bytegrinder.cpp
+++ src/rb3/bytegrinder.cpp
@@ -1,5 +1,6 @@
 #include "bytegrinder.hpp"
 #include "data.hpp"
+#include "string.h"
 
 namespace {
     int GetEncMethod(int ver){
@@ -53,4 +54,32 @@ DataNode hashTo6Bits(DataArray* da) {
         return DataNode(kDataInt, 0);
     }
     return DataNode(kDataInt, (void*)hashValue);
+}
+
+DataNode getRandomSequence32A(DataArray* da){
+    static int seed;
+    static bool usedUp[0x20];
+    bool enough = da->GetNodeCount() > 1;
+    
+    if(enough){
+        int dataint = da->GetIntAtIndex(1);
+        memset(usedUp, 0, 0x20);
+        if(dataint != 0){
+            seed = dataint;
+        }
+        return DataNode(kDataInt, (void*)0x610A660F);
+    }
+    else {
+        bool loop = true;
+        int idx = 0;
+        while(loop){
+            seed = seed * 0x19660D + 0x3C6EF35F;
+            idx = (seed >> 2 & 0x1F);
+            if(usedUp[idx] == false){
+                loop = false;
+                usedUp[idx] = true;
+            }
+        }
+        return DataNode(kDataInt, (void*)idx);
+    }
 }
