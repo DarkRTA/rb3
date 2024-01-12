47b966ff8838505e4774ba05839bc3740d2fdf61
rjkiv <rjkiv@users.noreply.github.com>
2024-01-11 17:38:55 -0700

    rndhighlightable ctor

--- configure.py
+++ configure.py
@@ -246,7 +246,7 @@ config.libs = [
             Object(Matching, "rb3/trigtable.cpp"),
 
             Object(NonMatching, "rb3/Rnd/rndbitmap.cpp"),
-            Object(NonMatching, "rb3/Rnd/rnddrawable.cpp"),
+            # Object(NonMatching, "rb3/Rnd/rnddrawable.cpp"),
 
             Object(NonMatching, "rb3/hmx/object.cpp"),
             Object(NonMatching, "rb3/app.cpp"),
--- src/rb3/Rnd/rndhighlightable.hpp
+++ src/rb3/Rnd/rndhighlightable.hpp
@@ -2,7 +2,7 @@
 #define RND_RNDHIGHLIGHTABLE_HPP
 #include "rb3/hmx/object.hpp"
 
-class RndHighlightable : public Hmx::Object {
+class RndHighlightable : public virtual Hmx::Object {
 public:
     RndHighlightable();
     virtual ~RndHighlightable();
