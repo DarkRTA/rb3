dc62b3f77e803bad12005abb8c54a379fd5784e9
rjkiv <rjkiv@users.noreply.github.com>
2024-01-12 20:37:16 -0700

    stubzero

--- configure.py
+++ configure.py
@@ -303,6 +303,7 @@ config.libs = [
             Object(NonMatching, "rb3/fx/fxsendwah.cpp"),
 
             Object(Matching, "rb3/stubs/stubvoid.cpp"),
+            Object(Matching, "rb3/stubs/stubzero.cpp"),
 
             Object(NonMatching, "rb3/unknown/800/unk_8000D980.cpp"),
             Object(NonMatching, "rb3/unknown/800/unk_8000F9B0.cpp"),
