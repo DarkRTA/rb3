859721a263ff1d5cf88ec28c766818ed2c9b3fe5
rjkiv <rjkiv@users.noreply.github.com>
2024-01-15 15:14:21 -0700

    dofproc work

--- src/rb3/dofproc.cpp
+++ src/rb3/dofproc.cpp
@@ -7,3 +7,33 @@ DOFProc::DOFProc(){
 DOFProc::~DOFProc(){
     
 }
+
+Symbol DOFProc::StaticClassName(){
+    static Symbol name("DOFProc");
+    return name;
+}
+
+Symbol DOFProc::ClassName() const {
+    return StaticClassName();
+}
+
+extern DataArray* SystemConfig(Symbol, Symbol, Symbol);
+extern char* PathName(const Hmx::Object*);
+
+void DOFProc::SetType(Symbol s){
+    static DataArray* types = SystemConfig(StaticClassName(), "types", "objects");
+    if(s.IsNull()){
+        SetTypeDef(nullptr);
+    }
+    else {
+        DataArray* found = types->FindArray(s, false);
+        if(found){
+            SetTypeDef(found);
+        }
+        else {
+            PathName(this);
+            ClassName();
+            SetTypeDef(nullptr);
+        }
+    }
+}
