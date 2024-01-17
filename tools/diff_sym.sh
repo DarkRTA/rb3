a31c344bd126c562dc2d8a875206d4ff579fa7b4
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-01-17 13:03:00 -0700

    make diff script point to objdump (#79)
    Co-authored-by: rjkiv <rjkiv@users.noreply.github.com>

--- tools/diff_sym.sh
+++ tools/diff_sym.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git --no-pager diff --no-index \
-	<(llvm-objdump -D --disassemble-symbols=$1 build/SZBE69/main.elf) \
-	<(llvm-objdump -D --disassemble-symbols=$1 ignore/orig.elf)
+	<(build/binutils/powerpc-eabi-objdump -D --disassemble-symbols=$1 build/SZBE69/main.elf) \
+	<(build/binutils/powerpc-eabi-objdump -D --disassemble-symbols=$1 ignore/orig.elf)
