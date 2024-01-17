a9a87ed4dcd8267db88c4cbec549bd270174c798
rjkiv <rjkiv@users.noreply.github.com>
2024-01-16 22:07:26 -0700

    match symbol set 2

--- configure.py
+++ configure.py
@@ -301,7 +301,7 @@ config.libs = [
 
             Object(Matching, "rb3/stringtablelocks.cpp"),
             Object(Matching, "rb3/symbolset1.cpp"),
-            Object(NonMatching, "rb3/symbolset2.cpp"),
+            Object(Matching, "rb3/symbolset2.cpp"),
             Object(NonMatching, "rb3/symbolset3.cpp"),
             Object(NonMatching, "rb3/symbolset4.cpp"),
 
---
+++ src/rb3/symbolset4.cpp
@@ -0,0 +1,6 @@
+#include "stringtablelocks.hpp"
+#include "symbol.hpp"
+
+StringTableLockBegin lock4;
+
+StringTableLockEnd unlock4;
