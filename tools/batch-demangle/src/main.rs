8e2f5a401506593f6b8183f643b8b8502e86299c
Nathan <nathanhurst03@gmail.com>
2024-01-21 05:17:10 -0700

    Ghidra symbol importer upgrades (#86)
    * Only output demangled name on success
    
    * Clean up parsing of symbol info
    
    * Split symbol namespaces and names
    
    * Don't treat operators with <> as templates
    
    * Handle function pointer types correctly when splitting demangled functions
    
    * Fix namespace handling in import script
    
    * Slight bit of script cleanup
    - Tidy up imports
    - Use SourceType explicitly in code
    - Add a couple comments about what's happening
    
    * Convert namespaces to classes where possible
    
    * Remove duplicated symbols.txt from batch-demangle folder
    
    * Clean up label creation
    
    * Remove spaces in template parameters

--- tools/batch-demangle/src/main.rs
+++ tools/batch-demangle/src/main.rs
@@ -1,6 +1,10 @@
 use clap::Parser;
 use cwdemangle::*;
 use std::fs::read_to_string;
+use std::collections::hash_set::HashSet;
+
+#[macro_use]
+extern crate lazy_static;
 
 #[derive(Parser)]
 struct Args {
@@ -12,18 +16,138 @@ fn main() {
     let f = read_to_string(args.input).unwrap();
 
     for line in f.lines() {
-        let parts = line.split(" ").collect::<Vec<_>>();
-        let sym = parts[0];
-        let demangled = match demangle(parts[0], &DemangleOptions {
+        // Symbol info: symbol = section:0x<address>; // type:<type> [flags...]
+        let (sym, remaining) = line.split_once(" = ").unwrap();
+        let (_section, remaining) = remaining.split_once(':').unwrap();
+        let (addr, _remaining) = remaining.split_once("; // ").unwrap();
+
+        match demangle(sym, &DemangleOptions {
             omit_empty_parameters: false
         }) {
-            Some(x) => x,
-            None => "@@@@@".into()
+            Some(demangled) => {
+                if !demangled.contains('(') {
+                    // Variable: [namespace::]name
+                    let (namespace, name) = split_namespace(demangled.as_str());
+                    println!("{addr}|||{sym}|||{demangled}|||{namespace}|||{name}");
+                } else {
+                    // Function: [return-type] [namespace::]name([parameter_types]) [cv-qualifier]
+                    let (prolog, _parameters, _epilog) = split_function(demangled.as_str());
+
+                    let (_return_type, qualified_name) = split_return(prolog);
+                    let (namespace, name) = split_namespace(qualified_name);
+
+                    println!("{addr}|||{sym}|||{demangled}|||{namespace}|||{name}");
+                }
+            },
+            None => {
+                println!("{addr}|||{sym}");
+            }
+        };
+    }
+}
+
+fn split_function<'a>(demangled_fn: &'a str) -> (&'a str, &'a str, &'a str) {
+    // Search from the end by depth, to handle function pointer types correctly
+    let mut parentheses_depth = 0;
+    for (index, value) in demangled_fn.rmatch_indices(&['(', ')']) {
+        parentheses_depth += match value {
+            // We're searching in reverse, so closing parentheses increase our depth
+            "(" => -1,
+            ")" => 1,
+            _ => 0
         };
 
-        let addr_idx = parts[2].find("0x").unwrap();
-        let addr = &parts[2][addr_idx..addr_idx + 10];
-
-        println!("{sym}|||{demangled}|||{addr}");
+        if parentheses_depth == 0 {
+            let parameter_start = index;
+            let parameter_end = demangled_fn.rfind(')').unwrap();
+            let (prolog, remaining) = demangled_fn.split_at(parameter_start);
+            let (parameters, epilog) = remaining.split_at(parameter_end - parameter_start + 1);
+            return (prolog, parameters, epilog);
+        }
     }
+
+    if !demangled_fn.contains('(') {
+        panic!("Non-function symbol {demangled_fn} passed to split_function!");
+    } else {
+        unreachable!("This branch indicates mismatched parentheses, which would be a demangler bug");
+    }
+}
+
+fn split_return<'a>(prolog: &'a str) -> (&'a str, &'a str) {
+    const PATTERN: &str = " ";
+    match prolog.find(PATTERN) {
+        Some(space_index) => match prolog.find('<') {
+            // Template args, make sure space doesn't come from there
+            Some(template_index) => match space_index < template_index {
+                true => {
+                    let (ret, _) = prolog.split_at(space_index);
+                    let (_, qualified) = prolog.split_at(space_index + PATTERN.len());
+                    (ret, qualified)
+                },
+                false => ("", prolog)
+            },
+            None => {
+                let (ret, _) = prolog.split_at(space_index);
+                let (_, qualified) = prolog.split_at(space_index + PATTERN.len());
+                (ret, qualified)
+            }
+        },
+        None => ("", prolog)
+    }
+}
+
+lazy_static! {
+    static ref NON_TEMPLATE_NAMES: HashSet<&'static str> = HashSet::from_iter([
+        "operator<",
+        "operator>",
+        "operator<<",
+        "operator>>",
+        "operator>>=",
+        "operator<<=",
+        "operator<=",
+        "operator>=",
+        "operator->*",
+        "operator->",
+    ]);
+}
+
+fn split_namespace(prolog: &str) -> (String, String) {
+    const PATTERN: &str = "::";
+
+    let mut namespaces = Vec::<String>::new();
+
+    // Ensure template arguments aren't split up
+    let mut template_depth = 0;
+    let mut template_buffer = String::new();
+    for split in prolog.split(PATTERN) {
+        // Ensure special names that contain <> but aren't templates are added as-is
+        if NON_TEMPLATE_NAMES.contains(split) {
+            namespaces.push(split.to_string());
+            continue;
+        }
+
+        template_depth += split.matches('<').count();
+
+        if template_depth > 0 {
+            template_buffer += split;
+
+            template_depth -= split.matches('>').count();
+            if template_depth < 1 {
+                namespaces.push(template_buffer);
+                template_buffer = String::new();
+            } else {
+                // Keep namespace delimiter in the final namespace
+                template_buffer += PATTERN;
+            }
+        } else {
+            namespaces.push(split.to_string());
+        }
+    }
+
+    assert!(template_buffer.is_empty());
+
+    // Split off name, and re-join namespaces with a padded separator to make later splitting easier
+    let name = namespaces.pop().unwrap();
+    let namespaces = namespaces.join(" :: ");
+    return (namespaces, name);
 }
--- tools/ghidra-scripts/dtk_symbol_importer.py
+++ tools/ghidra-scripts/dtk_symbol_importer.py
@@ -3,38 +3,74 @@
 #@category Import
 #@menupath Tools.Import From dtk Symbols File
 
-import re
-from ghidra.program.model.symbol.SourceType import *
-from ghidra.program.model.symbol import SymbolUtilities
-import string
 import subprocess
 
+from ghidra.program.model.symbol import *
+
+# Read/demangle symbols file
 f = askFile("Symbols File", "OK")
 demangler_output = subprocess.check_output(['rb3-batch-demangle', f.absolutePath])
 
-for sym in currentProgram.getSymbolTable().getSymbolIterator():
-    if sym.getSource() == USER_DEFINED:
+# Delete non-primary/user existing symbols, as we'll be replacing them
+symbolTable = currentProgram.getSymbolTable()
+for sym in symbolTable.getSymbolIterator():
+    if sym.getSource() == SourceType.USER_DEFINED:
         continue
     if sym.isPrimary():
         continue
     sym.delete()
 
+# Apply demangled symbols to the program
+created_namespaces = []
 for l in demangler_output.splitlines():
     tokens = l.split("|||")
-    lbl = tokens[0]
-    name = SymbolUtilities.replaceInvalidChars(str(tokens[1]).split("(")[0], False)
 
-    if name == "@@@@@":
+    address = toAddr(tokens[0])
+    lbl = str(tokens[1])
+    if len(tokens) > 3:
+        demangled = str(tokens[2])
+        namespaces = str(tokens[3]).replace(", ", ",") # Remove spaces in template parameters
+        name = str(tokens[4]).replace(", ", ",")       # to match built-in demangler
+
+        print("Creating label {} in namespace {} at address {}".format(name, namespaces.replace(" :: ", "::"), address))
+    else:
+        demangled = None
+        namespaces = ""
         name = lbl
 
-    address = toAddr(tokens[2])
-    print("Created label {} at address {}".format(name, address))
+        print("Creating label {} in global namespace at address {}".format(name, address))
+
+    name = SymbolUtilities.replaceInvalidChars(name, True)
+
+    if namespaces != "":
+        # Recursively create each namespace, Ghidra doesn't handle this for us
+        namespaces = namespaces.split(" :: ")
+        parent_namespace = None
+        for namespace_lbl in namespaces:
+            namespace_lbl = SymbolUtilities.replaceInvalidChars(namespace_lbl, True)
+            namespace = createNamespace(parent_namespace, namespace_lbl)
+            created_namespaces.append(namespace)
+            parent_namespace = namespace
+    else:
+        namespace = None
 
     sym = getSymbolAt(address)
+    primary = True if sym is None else sym.getSource() != SourceType.USER_DEFINED
 
-    if sym == None:
-        createLabel(address, lbl, False, IMPORTED)
-        createLabel(address, name, True, ANALYSIS)
-    else:
-        createLabel(address, lbl, False, IMPORTED)
-        createLabel(address, name, sym.getSource() != USER_DEFINED, ANALYSIS)
+    createLabel(address, lbl, False, SourceType.IMPORTED)
+    createLabel(address, name, namespace, primary, SourceType.ANALYSIS)
+
+# Fix up class namespaces
+class_symbols = ["__vt", "__vtable", "__RTTI", "__superclasses", "__classname"]
+for namespace in created_namespaces:
+    if namespace.getSymbol().getSymbolType() == SymbolType.CLASS:
+        continue
+
+    constructor_name = namespace.getName().split("<")[0]
+    destructor_name = "~" + constructor_name
+    for sym in symbolTable.getSymbols(namespace):
+        sym_name = sym.getName()
+        if sym_name in class_symbols or sym_name == constructor_name or sym_name == destructor_name:
+            print("Converting namespace {} to class".format(namespace.getName()))
+            symbolTable.convertNamespaceToClass(namespace)
+            break
