6751cb39c5a767515e4c05a2aa199b9f6fb57524
rjkiv <rjkiv@users.noreply.github.com>
2024-01-25 14:04:04 -0700

    more work on objectdir header

--- src/rb3/objectdir.hpp
+++ src/rb3/objectdir.hpp
@@ -7,22 +7,32 @@
 class ObjectDir : public virtual Hmx::Object {
 public:
     ObjectDir();
+    virtual Symbol ClassName() const; // fn_800103C8
+    virtual void SetType(Symbol); // fn_800102A0
+    virtual DataNode Handle(DataArray*, bool); // fn_80336C94
+    virtual bool SyncProperty(DataNode&, DataArray*, int, PropOp); // fn_80337B7C
+    virtual void Save(BinStream&);
+    virtual void Copy(const Hmx::Object*, Hmx::Object::CopyType);
+    virtual void Load(BinStream&);
+    virtual ~ObjectDir();
 
     static ObjectDir* sMainDir;
 
     Hmx::Object* FindObject(const char*, bool);
+    static Symbol StaticClassName();
 
     char kh[0x20]; // should be a KeylessHash<const char*, ObjectDir::Entry*>
     StringTable table;
-    FilePath fpath;
-    bool unk44, unk45;
+    FilePath proxyFile;
+    bool unk44;
+    bool inlineProxyAllowed;
     int unk48;
-    char list1[0x8];
+    char subdirs[0x8]; // should be a vector
     bool unk54;
     int unk58;
-    const char* unk5c;
+    const char* pathName;
     FilePath fpath2;
-    char list2[0x8];
+    char vector2[0x8];
     int unk74;
     int unk78;
     const char* unk7c;
