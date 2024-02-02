fb36a85143f5a838bb3ba80fb86812e7fccb552c
rjkiv <rjkiv@users.noreply.github.com>
2024-02-01 17:04:40 -0700

    add songmetadata header

---
+++ src/rb3/songmetadata.hpp
@@ -0,0 +1,29 @@
+#ifndef RB3_SONGMETADATA_HPP
+#define RB3_SONGMETADATA_HPP
+#include "hmx/object.hpp"
+#include "symbol.hpp"
+#include "data.hpp"
+
+class SongMetadata : public Hmx::Object {
+public:
+    SongMetadata();
+    SongMetadata(DataArray*, DataArray*, bool); // https://decomp.me/scratch/vjSkv
+    virtual ~SongMetadata();
+    virtual DataNode Handle(DataArray*, bool);
+    virtual void Save(BinStream&);
+    virtual void Load(BinStream&);
+
+    void Initialize();
+
+    short version;
+    Symbol shortname; // song shortname?
+    int songId;
+    bool unk28;
+    Symbol songSrc; // game_origin
+    float previewStart;
+    float previewEnd;
+    int unk38; // actually a DataArraySongInfo*
+    int unk3c;
+};
+
+#endif
