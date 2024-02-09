2302063ce724ba1438a194d4ca60b384b5e093c1
rjkiv <rjkiv@users.noreply.github.com>
2024-02-08 17:51:15 -0700

    move objptr

--- src/system/obj/ObjPtr_p.h
+++ src/system/obj/ObjPtr_p.h
@@ -1,28 +1,28 @@
-#ifndef RB3_OBJPTR_HPP
-#define RB3_OBJPTR_HPP
+#ifndef OBJ_OBJPTR_H
+#define OBJ_OBJPTR_H
 #include "Object.h"
 
 template <class T1, class T2> class ObjPtr : public ObjRef {
 public:
 
     ObjPtr(Hmx::Object* obj, T1* cls){
-        parent = obj;
-        child = cls;
+        mOwner = obj;
+        mPtr = cls;
         if(cls != 0){
             cls->AddRef(this);
         }
     }
 
     virtual ~ObjPtr(){
-        if(child != 0) child->Release(this);
+        if(mPtr != 0) mPtr->Release(this);
     }
 
     virtual Hmx::Object* RefOwner(){ 
-        return parent; 
+        return mOwner; 
     }
 
     virtual void Replace(Hmx::Object* o1, Hmx::Object* o2){
-        if (child == o1) {
+        if (mPtr == o1) {
             *this = dynamic_cast<T1*>(o2);
         }
     }
@@ -32,15 +32,15 @@ public:
     }
 
     void operator=(T1* t){
-        if(child != t){
-            if(child != 0) child->Release(this);
-            child = t;
+        if(mPtr != t){
+            if(mPtr != 0) mPtr->Release(this);
+            mPtr = t;
             if(t != 0) t->AddRef(this);
         }
     }
 
-    Hmx::Object* parent;
-    T1* child;
+    Hmx::Object* mOwner;
+    T1* mPtr;
 };
 
 #endif
