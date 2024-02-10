8e2262e838f2e6804a6d3103d19132846646f830
rjkiv <rjkiv@users.noreply.github.com>
2024-02-10 13:59:48 -0700

    more adjacency work

--- src/system/math/strips/Adjacency.cpp
+++ src/system/math/strips/Adjacency.cpp
@@ -77,7 +77,8 @@ bool Adjacencies::UpdateLink(unsigned int firsttri, unsigned int secondtri, unsi
         unsigned int edge2 = tri2->FindEdge(ref0, ref1);
         if((edge2 & 0xFF) == 0xFF) return false;
         else {
-            
+            tri1->ATri[edge & 0xFF] = secondtri | (edge2 << 0x1E);
+            tri2->ATri[edge2 & 0xFF] = firsttri | (edge << 0x1E);
             return true;
         }
     }
@@ -112,3 +113,26 @@ bool Adjacencies::UpdateLink(unsigned int firsttri, unsigned int secondtri, unsi
 //   }
 //   return uVar2;
 // }
+
+unsigned int AdjTriangle::FindEdge(unsigned int ui1, unsigned int ui2){
+    unsigned int edge = 0xFF;
+    if(VRef[0] == ui1 && VRef[1] == ui2) edge = 0;
+    else if(VRef[0] == ui2 && VRef[1] == ui1) edge = 0;
+    else if(VRef[0] == ui1 && VRef[2] == ui2) edge = 1;
+    else if(VRef[0] == ui2 && VRef[2] == ui1) edge = 1;
+    else if(VRef[1] == ui1 && VRef[2] == ui2) edge = 2;
+    else if(VRef[1] == ui2 && VRef[2] == ui1) edge = 2;
+    return edge;
+}
+
+unsigned int AdjTriangle::OppositeVertex(unsigned int ui1, unsigned int ui2){
+    unsigned int vert = -1;
+    if(VRef[0] == ui1 && VRef[1] == ui2) vert = VRef[2];
+    else if(VRef[0] == ui2 && VRef[1] == ui1) vert = VRef[2];
+    else if(VRef[0] == ui1 && VRef[2] == ui2) vert = VRef[1];
+    else if(VRef[0] == ui2 && VRef[2] == ui1) vert = VRef[1];
+    else if(VRef[1] == ui1 && VRef[2] == ui2) vert = VRef[0];
+    else if(VRef[1] == ui2 && VRef[2] == ui1) vert = VRef[0];
+
+    return vert;
+}
