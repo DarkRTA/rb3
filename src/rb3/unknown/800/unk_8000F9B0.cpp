2b3c0af22dae8d88b5520630613e6231e70b1d52
rjkiv <rjkiv@users.noreply.github.com>
2024-02-11 12:59:46 -0700

    work on message header

--- src/rb3/unknown/800/unk_8000F9B0.cpp
+++ src/rb3/unknown/800/unk_8000F9B0.cpp
@@ -10,24 +10,6 @@ bool Symbol::IsNull() {
     return m_string == gNullStr;
 }
 
-extern DataArray *fn_8035CF9C(int, int, int);
-// fn_8000E048
-Message::Message(Symbol s) {
-    DataArray *da = new (fn_8035CF9C(0x10, 0x10, 1)) DataArray(2);
-    unk4 = da;
-
-    unk4->GetNodeAtIndex(1)->operator=(DataNode(s));
-}
-
-// fn_8001008C
-Message::Message(Symbol s, const DataNode &dn) {
-    DataArray *da = new (fn_8035CF9C(0x10, 0x10, 1)) DataArray(3);
-    unk4 = da;
-
-    unk4->GetNodeAtIndex(1)->operator=(DataNode(s));
-    unk4->GetNodeAtIndex(2)->operator=(dn);
-}
-
 // fn_8000FCA4
 bool Symbol::operator==(const Symbol &sym) {
     return m_string == sym.m_string;
--- src/system/utl/Message.h
+++ src/system/utl/Message.h
@@ -2,21 +2,51 @@
 #define UTL_MESSAGE_H
 #include "Data.h"
 #include "Symbol.h"
+#include <new>
 
 // every method in here is weak
 class Message {
 public:
     Message(); // fn_8000FFE0
-    Message(Symbol, const DataNode &, const DataNode &); // fn_8000E048
-    Message(Symbol, const DataNode &, const DataNode &,
-            const DataNode &); // fn_800B7D98
-    Message(
-		Symbol, const DataNode &, const DataNode &, const DataNode &,
-		const DataNode &); // fn_80177AD4
-    Message(Symbol, const DataNode &); // fn_8001008C
-    Message(Symbol); // fn_8000FFE0
-    Message(DataArray *); // fn_800AE714
-    virtual ~Message(); // fn_8000DF60
+
+    Message(Symbol type){
+        mData = new (_PoolAlloc(0x10, 0x10, FastPool)) DataArray(2);
+        mData->Node(1) = DataNode(type);
+    }
+
+    Message(Symbol type, const DataNode & arg1){
+        mData = new (_PoolAlloc(0x10, 0x10, FastPool)) DataArray(3);
+        mData->Node(1) = DataNode(type);
+        mData->Node(2) = arg1;
+    }
+
+    Message(Symbol type, const DataNode & arg1, const DataNode & arg2){
+        mData = new (_PoolAlloc(0x10, 0x10, FastPool)) DataArray(4);
+        mData->Node(1) = DataNode(type);
+        mData->Node(2) = arg1;
+        mData->Node(3) = arg2;
+    }
+    
+    Message(Symbol type, const DataNode & arg1, const DataNode & arg2, const DataNode & arg3){
+        mData = new (_PoolAlloc(0x10, 0x10, FastPool)) DataArray(5);
+        mData->Node(1) = DataNode(type);
+        mData->Node(2) = arg1;
+        mData->Node(3) = arg2;
+        mData->Node(4) = arg3;
+    }
+            
+    Message(Symbol type, const DataNode & arg1, const DataNode & arg2, const DataNode & arg3, const DataNode & arg4){
+        mData = new (_PoolAlloc(0x10, 0x10, FastPool)) DataArray(6);
+        mData->Node(1) = DataNode(type);
+        mData->Node(2) = arg1;
+        mData->Node(3) = arg2;
+        mData->Node(4) = arg3;
+        mData->Node(5) = arg4;
+    }
+    
+    Message(DataArray* da) : mData(da) { da->AddRef(); }
+    
+    virtual ~Message(){ mData->Release(); }
 
     DataArray* mData;
     DataArray* GetArray(){ return mData; }
