0047f8be3f8231f6994675de6f1b2a926db546c7
rjkiv <rjkiv@users.noreply.github.com>
2024-02-18 10:37:58 -0700

    more work on symbols

--- src/system/utl/Symbols.h
+++ src/system/utl/Symbols.h
@@ -1,14 +1,29 @@
 #ifndef UTL_SYMBOLS_H
 #define UTL_SYMBOLS_H
+#include "utl/StringTable.h"
+#include "utl/Symbol.h"
+
+extern StringTable* gStringTable;
+extern bool gLiteralSymbolStaticInitialization;
 
 class BeginLiteralSymbols {
 public:
-    BeginLiteralSymbols();
+    BeginLiteralSymbols(){
+        if(gStringTable == 0){
+            Symbol::PreInit(0x81700, 0x13c00);
+        }
+        gLiteralSymbolStaticInitialization = true;
+    }
 };
 
 class EndLiteralSymbols {
 public:
-    EndLiteralSymbols();
+    EndLiteralSymbols(){
+        gLiteralSymbolStaticInitialization = false;
+    }
+
 };
+
+
 
 #endif
