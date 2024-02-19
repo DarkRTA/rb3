ab646292c1d7aaff517a0ab7d21815061de5de00
rjkiv <rjkiv@users.noreply.github.com>
2024-02-19 13:39:43 -0700

    begin binstream re-integration

--- src/system/utl/BinStream.cpp
+++ src/system/utl/BinStream.cpp
@@ -3,8 +3,24 @@
 #include "utl/Str.h"
 #include "utl/Symbol.h"
 #include "utl/TextStream.h"
+#include "os/Debug.h"
 #include <string.h>
 
 const char *BinStream::Name() const {
     return "<unnamed>";
+}
+
+BinStream& BinStream::operator<<(const char* str){
+    if(str == 0) KASSERTSTR_FAIL(0x23, "str");
+    int size = strlen(str);
+    WriteEndian(&size, 4);
+    Write(str, size);
+    return *this;
+}
+
+BinStream::BinStream(bool b) : mLittleEndian(b), mCrypto(0) {
+}
+
+BinStream::~BinStream() {
+    delete mCrypto;
 }
