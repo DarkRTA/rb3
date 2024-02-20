7eca887e4a7cbc8647bbe5fc8e0143a2e60f16e4
rjkiv <rjkiv@users.noreply.github.com>
2024-02-20 12:39:44 -0700

    fill out the rest of Str

--- src/system/utl/PoolAlloc.h
+++ src/system/utl/PoolAlloc.h
@@ -1,10 +1,6 @@
 #ifndef UTL_POOLALLOC_H
 #define UTL_POOLALLOC_H
-
-// enum PoolType {
-//     MainPool,
-//     FastPool
-// };
+#include <new>
 
 void* _PoolAlloc(int, int, PoolType);
 void _PoolFree(int, PoolType, void*);
