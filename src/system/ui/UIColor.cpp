65ad08f0692efcc54078f1c15a6273376411f9e3
rjkiv <rjkiv@users.noreply.github.com>
2024-02-24 10:45:41 -0700

    UIColor work

--- src/system/ui/UIColor.cpp
+++ src/system/ui/UIColor.cpp
@@ -1,4 +1,6 @@
 #include "ui/UIColor.h"
+#include "os/Debug.h"
+#include "obj/MessageTimer.h"
 
 UIColor::UIColor() : mColor() {
     
@@ -10,4 +12,22 @@ const Hmx::Color& UIColor::GetColor() const {
 
 void UIColor::SetColor(const Hmx::Color& color){
     mColor = color;
+}
+
+void UIColor::Save(BinStream&){
+    MILO_ASSERT(0, 0x24);
+}
+
+DataNode UIColor::Handle(DataArray* _msg, bool _warn){
+    Symbol sym = _msg->Sym(1);
+    MessageTimer timer((MessageTimer::Active()) ? this : 0, sym);
+    {
+        DataNode handled = Hmx::Object::Handle(_msg, false);
+        if(handled.Type() != kDataUnhandled){
+            return DataNode(handled);
+        }
+    }
+    if(_warn) MILO_WARN("%s(%d): %s unhandled msg: %s", __FILE__, 0x3A, PathName(this), sym);
+    return DataNode(kDataUnhandled, 0);
+    
 }
