048ce027f111840148f384a8d0a6610962ca0ef9
rjkiv <rjkiv@users.noreply.github.com>
2024-02-25 15:10:24 -0700

    logfile work

---
+++ src/system/utl/KeylessHash.h
@@ -0,0 +1,30 @@
+#ifndef UTL_KEYLESSHASH_H
+#define UTL_KEYLESSHASH_H
+
+// forward declaration
+namespace Hmx {
+    class Object;
+}
+
+struct Entry {
+    const char* name;
+    Hmx::Object* obj;
+};
+
+template <class T1, class T2> class KeylessHash {
+    Entry* mEntries;
+    int mSize;
+    bool mOwnEntries;
+    int mNumEntries;
+    Entry mEmpty;
+    Entry mRemoved;
+
+    KeylessHash(int, const T2&, const T2&, T2*);
+    ~KeylessHash();
+    T1& Find(const T1&);
+    int Insert(const T2&);
+    void Resize(int, T2*);
+    T2* FirstFrom(T2*);
+};
+
+#endif
--- src/system/utl/LogFile.cpp
+++ src/system/utl/LogFile.cpp
@@ -1,4 +1,6 @@
 #include "utl/LogFile.h"
+#include "os/File_Wii.h"
+#include "utl/MakeString.h"
 
 LogFile::LogFile(const char* file_pattern) : mFilePattern(file_pattern), mSerialNumber(0), mDirty(0), mFile(0), mActive(0){
 
@@ -14,5 +16,21 @@ void LogFile::Print(const char* str){
         mFile->Print(str);
         mFile->mFile.Flush();
         mDirty = true;
+    }
+}
+
+void LogFile::AdvanceFile(){
+    FileStat stat;
+    if(mDirty || !mFile){
+        delete mFile;
+        mFile = 0;
+        const char* str;
+        while(true){
+            str = MakeString(mFilePattern.mStr, mSerialNumber);
+            if(FileGetStat(str, &stat) < 0) break;
+            mSerialNumber++;
+        }
+        mFile = new TextFileStream(str, false);
+        mDirty = false;
     }
 }
