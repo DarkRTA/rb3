b9261db49e87689f302d90e6f70ba5f1ffe317af
Nathan <nathanhurst03@gmail.com>
2024-02-28 04:12:44 -0700

    Fix operator new/delete conflict from MemMgr.h

--- src/system/utl/MemMgr.h
+++ src/system/utl/MemMgr.h
@@ -1,11 +1,12 @@
 #ifndef UTL_MEMMGR_H
 #define UTL_MEMMGR_H
+#include <stddef.h>
 #include "utl/PoolAlloc.h"
 
-void* operator new(unsigned long);
-void operator delete(void*);
-void* operator new[](unsigned long);
-void operator delete[](void*);
+void* operator new(size_t) throw();
+void operator delete(void*) throw();
+void* operator new[](size_t) throw();
+void operator delete[](void*) throw();
 
 void* _MemOrPoolAlloc(int, PoolType);
 void _MemOrPoolFree(int, PoolType, void*);
