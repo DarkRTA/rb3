50b909413407108ff9d477bbcd088d779f28b387
rjkiv <rjkiv@users.noreply.github.com>
2024-02-28 21:23:18 -0700

    oh sweet, striper is open source

---
+++ src/system/math/strips/StdAfx.h
@@ -0,0 +1,40 @@
+#pragma once
+
+// #include <cstdint>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+	typedef char			sbyte;
+	typedef unsigned char		ubyte;
+	typedef short		sword;
+	typedef unsigned short		uword;
+	typedef int			sdword;
+	typedef unsigned int		udword;
+	typedef long long		sqword;
+	typedef unsigned long long	uqword;
+	typedef float				sfloat;
+
+	#define	null	0
+	#define RELEASE(x)		{ delete x;		x = null; }
+	#define RELEASEARRAY(x)	{ delete []x;	x = null; }
+
+	inline void ZeroMemory(void* addr, udword size)
+	{
+		memset(addr, 0, size);
+	}
+
+	inline void CopyMemory(void* dest, const void* src, udword size)
+	{
+		memcpy(dest, src, size);
+	}
+
+	inline void FillMemory(void* dest, udword size, ubyte val)
+	{
+		memset(dest, val, size);
+	}
+
+#include "RevisitedRadix.h"
+#include "CustomArray.h"
+#include "Adjacency.h"
+#include "Striper.h"
