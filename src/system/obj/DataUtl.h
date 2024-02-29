17faba803526534eb306af9a253fc91206e8e159
rjkiv <rjkiv@users.noreply.github.com>
2024-02-29 14:34:48 -0700

    datautl work

--- src/system/obj/DataUtl.h
+++ src/system/obj/DataUtl.h
@@ -2,7 +2,16 @@
 #define OBJ_DATAUTL_H
 #include "obj/Data.h"
 
+void DataInit();
+void DataTerminate();
+void DataMacroWarning(bool);
+void DataSetMacro(Symbol, DataArray*);
+DataArray* DataGetMacro();
+Symbol DataGetMacroByInt(int, const char*);
+Hmx::Object* DataThis();
 void DataMergeTags(DataArray*, DataArray*);
 void DataReplaceTags(DataArray*, DataArray*);
+void DataPushVar(DataNode*);
+void DataPopVar();
 
 #endif
