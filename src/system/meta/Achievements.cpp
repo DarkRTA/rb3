8dcf4c743629c5e6efed2d08d3e0a5ad23e2c817
rjkiv <rjkiv@users.noreply.github.com>
2024-02-29 19:51:47 -0700

    make achievement handler nicer

--- src/system/meta/Achievements.cpp
+++ src/system/meta/Achievements.cpp
@@ -24,7 +24,7 @@ void Achievements::Submit(LocalUser* l, Symbol s, int i) {return;}
 void Achievements::SetAllowAchievements(bool b) {mAllowAchievements = b;}
 
 BEGIN_HANDLERS(Achievements);
-    HANDLE_ACTION(set_allow_achievements, int x = _msg->Int(2); SetAllowAchievements(x-1+1););
+    HANDLE_ACTION(set_allow_achievements, SetAllowAchievements(_msg->Int(2) != 0));
     HANDLE_SUPERCLASS(Hmx::Object);
     HANDLE_CHECK(0x74);
 END_HANDLERS;
