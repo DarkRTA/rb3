1b99397d31181319556f73bc37d9e1280b9dbb28
rjkiv <rjkiv@users.noreply.github.com>
2024-03-01 14:51:35 -0700

    sort matched

--- src/system/math/Sort.cpp
+++ src/system/math/Sort.cpp
@@ -3,8 +3,7 @@
 int HashString(const char* str, int i){
     int ret = 0;
     for(unsigned char* u = (unsigned char*)str; *u != 0; u++){
-        ret = *u + ret * 0x7F;
-        ret = ret - (ret / i) * i;
+        ret = (*u + ret * 0x7F) % i;
     }
     return ret;
 }
