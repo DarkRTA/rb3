a6eb145f41fe7e27b9ac1935f3fd314578e50b57
rjkiv <rjkiv@users.noreply.github.com>
2024-03-01 14:22:52 -0700

    rndpollable work

--- src/system/rndobj/Poll.cpp
+++ src/system/rndobj/Poll.cpp
@@ -1,5 +1,17 @@
 #include "rndobj/Poll.h"
 #include "utl/Messages.h"
+#include "utl/Symbols.h"
+
+BEGIN_HANDLERS(RndPollable);
+    HANDLE_ACTION(enter, Enter());
+    HANDLE_ACTION(poll, Poll());
+    static Symbol _s("exit");
+    if(sym == _s){
+        Exit();
+        return DataNode(0);
+    }
+    HANDLE_CHECK(0x1A);
+END_HANDLERS;
 
 void RndPollable::Enter(){
     HandleType(enter_msg.Data());
