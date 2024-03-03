08d822ccd75b1ef97a292252284522e74a355ac6
rjkiv <rjkiv@users.noreply.github.com>
2024-03-03 13:41:00 -0700

    chunks header

---
+++ src/system/utl/Chunks.h
@@ -0,0 +1,55 @@
+#ifndef UTL_CHUNKS_H
+#define UTL_CHUNKS_H
+#include "utl/BinStream.h"
+#include "utl/ChunkIDs.h"
+
+class ChunkHeader {
+public:
+    ChunkID mID;
+    int mLength;
+    bool mIsList;
+
+    void Read(BinStream&);
+};
+
+class IListChunk {
+public:
+    IListChunk* mParent;
+    BinStream& mBaseBinStream;
+    ChunkHeader* mHeader;
+    int mStartMarker;
+    int mEndMarker;
+    bool mLocked;
+    ChunkHeader mSubHeader;
+    bool mSubChunkValid;
+    bool mRecentlyReset;
+    int mSubChunkMarker;
+
+    IListChunk(BinStream&, bool);
+    IListChunk(IListChunk&);
+    ~IListChunk();
+};
+
+class IDataChunk : public BinStream {
+public:
+    IListChunk* mParent;
+    BinStream& mBaseBinStream;
+    ChunkHeader* mHeader;
+    int mStartMarker;
+    int mEndMarker;
+    bool mFailed;
+    bool mEof;
+
+    IDataChunk(IListChunk&);
+    virtual ~IDataChunk();
+    virtual void Flush(){}
+    virtual int Tell();
+    virtual bool Eof();
+    virtual bool Fail(); // weak
+    virtual void ReadImpl(void*, int);
+    virtual void WriteImpl(const void*, int){}
+    virtual void SeekImpl(int, SeekType);
+
+};
+
+#endif
