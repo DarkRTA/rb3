0f432de085e22641c68a394c85464c8642e3f44b
Nathan <nathanhurst03@gmail.com>
2024-03-03 23:37:26 -0700

    Replace STLport C stdlib headers with include path to regular ones Greatly simplifies namespace stuff

--- cflags_common.py
+++ cflags_common.py
@@ -1,6 +1,10 @@
 cflags_includes = [
+    # C/C++ stdlib
     # STLport requires that it comes first in the include path list
     "-i src/system/stlport",
+    "-i src/sdk/PowerPC_EABI_Support/MSL/MSL_C",
+    # Not included since it's all wrapped by stlport
+    # "-i src/sdk/PowerPC_EABI_Support/MSL/MSL_C++",
 
     "-i src/system/speex/include",
 
