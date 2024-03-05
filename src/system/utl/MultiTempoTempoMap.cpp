fbcb3d3aaa5bb0e70dc0c654a1ed8ebe750bcd95
rjkiv <rjkiv@users.noreply.github.com>
2024-03-04 17:04:34 -0700

    multitempotempomap header and ctor/dtor

---
+++ src/system/utl/MultiTempoTempoMap.cpp
@@ -0,0 +1,9 @@
+#include "utl/MultiTempoTempoMap.h"
+
+MultiTempoTempoMap::MultiTempoTempoMap() : mStartLoopTick(-1.0f), mEndLoopTick(-1.0f) {
+    
+}
+
+MultiTempoTempoMap::~MultiTempoTempoMap(){
+    
+}
