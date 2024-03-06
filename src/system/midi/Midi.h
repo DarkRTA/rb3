5ff2f94ff04cbfcd266da9e150a2eb467623e9c3
rjkiv <rjkiv@users.noreply.github.com>
2024-03-06 11:18:56 -0700

    match midireceiver

--- src/system/midi/Midi.h
+++ src/system/midi/Midi.h
@@ -27,7 +27,6 @@ public:
     const char* GetFilename() const;
     void SkipCurrentTrack();
 
-    // total size: 0x60
     class BinStream * mStream; // offset 0x0, size 0x4
     bool mStreamCreatedHere; // offset 0x4, size 0x1
     class String mStreamName; // offset 0x8, size 0xC
@@ -41,13 +40,14 @@ public:
     int mCurTick; // offset 0x2C, size 0x4
     unsigned char mPrevStatus; // offset 0x30, size 0x1
     class String mCurTrackName; // offset 0x34, size 0xC
-    std::vector<Midi> mMidiList; // offset 0x40, size 0xC
-    int mMidiListTick; // offset 0x4C, size 0x4
-    bool (* mLessFunc)(struct Midi &, struct Midi &); // offset 0x50, size 0x4
-    bool mOwnMaps; // offset 0x54, size 0x1
-    class MultiTempoTempoMap * mTempoMap; // offset 0x58, size 0x4
-    class MeasureMap * mMeasureMap; // offset 0x5C, size 0x4
-    bool mFail; // added for RB3
+    std::vector<String> mTrackNames;
+    std::vector<Midi> mMidiList; 
+    int mMidiListTick; 
+    bool (* mLessFunc)(struct Midi &, struct Midi &); 
+    bool mOwnMaps;
+    class MultiTempoTempoMap * mTempoMap; 
+    class MeasureMap * mMeasureMap;
+    bool mFail;
 };
 
 class MidiReceiver {
@@ -59,10 +59,10 @@ public:
     virtual void OnAllTracksRead() = 0;
     virtual void OnMidiMessage(int, unsigned char, unsigned char, unsigned char) = 0;
     virtual void OnText(int, const char*, unsigned char) = 0;
-    virtual void OnTempo(int, int);
-    virtual void OnTimeSig(int, int, int);
-    virtual bool OnAcceptMaps(TempoMap*, MeasureMap*);
-    virtual void SetMidiReader(MidiReader*);
+    virtual void OnTempo(int, int){}
+    virtual void OnTimeSig(int, int, int){}
+    virtual bool OnAcceptMaps(TempoMap*, MeasureMap*){ return false; }
+    virtual void SetMidiReader(MidiReader* mr){ mReader = mr; }
 
     void Error(const char*, int);
     void SkipCurrentTrack();
