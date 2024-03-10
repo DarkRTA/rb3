7970faf4105baf9df992fdbaacd3d47fbe352749
rjkiv <rjkiv@users.noreply.github.com>
2024-03-10 12:25:56 -0700

    utf8 work

--- src/system/utl/UTF8.h
+++ src/system/utl/UTF8.h
@@ -4,5 +4,9 @@
 
 unsigned int DecodeUTF8(unsigned short&, const char*);
 unsigned int EncodeUTF8(String&, unsigned int);
+void UTF8toASCIIs(char*, int, const char*, char);
+void ASCIItoUTF8(char*, int, const char*);
+int UTF8StrLen(const char*);
+const char* UTF8strchr(const char*, unsigned short);
 
 #endif
