df72edba8f496ada11baafbf40cd13cd369a6ef1
rjkiv <rjkiv@users.noreply.github.com>
2024-03-10 11:53:53 -0700

    mapfile header

--- src/system/os/MapFile_Wii.h
+++ src/system/os/MapFile_Wii.h
@@ -1,9 +1,26 @@
 #ifndef OS_MAPFILEWII_H
 #define OS_MAPFILEWII_H
 #include "utl/Str.h"
+#include "os/File.h"
+
+class WiiMapFile {
+public:
+    WiiMapFile(const char*);
+    ~WiiMapFile();
+    const char* GetFunction(unsigned int, bool);
+    void ReadLine(char*, int);
+    bool ParseStack(const char*, unsigned int*, int, char*);
+
+    File* mFile;
+    int mStart;
+};
 
 bool HasMoreParams(String);
 int GetVarLength(String&);
 void AddParams(String&, String&, String);
+
+void TryDemangleParams(String&, String&, String, String);
+void TryDemangleClassAndFunc(String);
+void TryDemangle(char*, const char*, bool);
 
 #endif
