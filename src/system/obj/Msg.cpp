3b0f506250ac9ba43c748c4f97d017f06295d56f
rjkiv <rjkiv@users.noreply.github.com>
2024-03-13 10:37:25 -0700

    msgsource work

--- src/system/obj/Msg.cpp
+++ src/system/obj/Msg.cpp
@@ -9,6 +9,8 @@ void MsgSource::Sink::Export(DataArray* da){
         case kExportType: obj->Export(da, true); break;
     }
 }
+    
+MsgSource::MsgSource() : mSinks(), mEventSinks(), mExporting(0) {}
 
 MsgSource::~MsgSource(){
     for(std::list<Sink>::iterator it = mSinks.begin(); it != mSinks.end(); it++){
@@ -26,6 +28,43 @@ MsgSource::~MsgSource(){
 BEGIN_HANDLERS(MsgSource);
     HANDLE(add_sink, OnAddSink);
     HANDLE(remove_sink, OnRemoveSink);
-    StaticClassName();
-    HANDLE_SUPERCLASS(Hmx::Object);
+    if(ClassName() == StaticClassName()){
+        HANDLE_SUPERCLASS(Hmx::Object);
+    }
+    Export(_msg, false);
 END_HANDLERS;
+
+DataNode MsgSource::OnAddSink(DataArray* da){
+    if(da->Size() > 3) {
+        SinkMode mode = (da->Size() > 4) ? (SinkMode)(da->Int(4)) : kHandle;
+        DataArray* arr = da->Array(3);
+        Hmx::Object* obj = da->GetObj(2);
+        if(arr->Size() == 0){
+            AddSink(obj, Symbol(), Symbol(), mode);
+        }
+        else {
+            for(int i = 0; i < arr->Size(); i++){
+                DataNode node(arr->Evaluate(i));
+                if(node.Type() == kDataArray){
+                    AddSink(obj, node.LiteralArray(0)->LiteralSym(0), node.LiteralArray(0)->LiteralSym(1), mode);
+                }
+                else {
+                    AddSink(obj, node.LiteralSym(0), Symbol(), mode);
+                }
+            }
+        }
+    }
+    else AddSink(da->GetObj(2), Symbol(), Symbol(), kHandle);
+    return DataNode(0);
+}
+
+DataNode MsgSource::OnRemoveSink(DataArray* da){
+    if(da->Size() > 3) {
+        Hmx::Object* obj = da->GetObj(2);
+        for(int i = 3; i < da->Size(); i++){
+            RemoveSink(obj, da->Sym(i));
+        }
+    }
+    else RemoveSink(da->GetObj(2), Symbol());
+    return DataNode(0);
+}
