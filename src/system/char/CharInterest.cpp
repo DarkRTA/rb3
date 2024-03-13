677a704870e1a9d9d0f80aca58f3479aad2918d7
rjkiv <rjkiv@users.noreply.github.com>
2024-03-12 18:48:33 -0700

    charinterest work

--- src/system/char/CharInterest.cpp
+++ src/system/char/CharInterest.cpp
@@ -1,4 +1,5 @@
 #include "char/CharInterest.h"
+#include "math/MathFuncs.h"
 
 CharInterest::CharInterest() : mMaxViewAngle(20.0f), mPriority(1.0f), mMinLookTime(1.0f), mMaxLookTime(3.0f), mRefractoryPeriod(6.1f), mDartOverride(this, 0), mCategoryFlags(0), mOverrideMinTargetDistance(0), mMinTargetDistanceOverride(35.0f) {
     SyncMaxViewAngle();
@@ -7,3 +8,15 @@ CharInterest::CharInterest() : mMaxViewAngle(20.0f), mPriority(1.0f), mMinLookTi
 CharInterest::~CharInterest(){
     
 }
+
+void CharInterest::SyncMaxViewAngle(){
+    mMaxViewAngleCos = cos_f(mMaxViewAngle * 0.017453292f);
+}
+
+SAVE_OBJ(CharInterest, 0x52);
+
+BEGIN_HANDLERS(CharInterest)
+    HANDLE_SUPERCLASS(RndTransformable)
+    HANDLE_SUPERCLASS(Hmx::Object)
+    HANDLE_CHECK(0x141)
+END_HANDLERS
--- src/system/rndobj/Draw.cpp
+++ src/system/rndobj/Draw.cpp
@@ -1,5 +1,5 @@
 #include "rndobj/Draw.h"
 
 RndDrawable::RndDrawable() : mOrder(0.0f), mSphere() {
-
+    
 }
