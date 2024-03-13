338b172821df65b1c5b45ff0991d7fcb102fd61e
rjkiv <rjkiv@users.noreply.github.com>
2024-03-12 20:45:14 -0700

    texblendcontroller header

---
+++ src/system/rndobj/TexBlendController.cpp
@@ -0,0 +1,7 @@
+#include "rndobj/TexBlendController.h"
+#include "rndobj/Tex.h"
+#include "rndobj/Trans.h"
+
+RndTexBlendController::RndTexBlendController() : mMesh(0, 0), mObject1(0, 0), mObject2(0, 0), mReferenceDistance(0.0f), mMinDistance(0.0f), mMaxDistance(0.0f), mTex(0, 0) {
+    
+}
---
+++ src/system/rndobj/TexBlendController.h
@@ -0,0 +1,32 @@
+#ifndef RNDOBJ_TEXBLENDCONTROLLER_H
+#define RNDOBJ_TEXBLENDCONTROLLER_H
+#include "obj/Object.h"
+#include "obj/ObjPtr_p.h"
+
+// forward decs
+class RndMesh;
+class RndTex;
+class RndTransformable;
+
+class RndTexBlendController : public Hmx::Object {
+public:
+    RndTexBlendController();
+    virtual ~RndTexBlendController();
+    OBJ_CLASSNAME(TexBlendController);
+    OBJ_SET_TYPE(TexBlendController);
+    virtual DataNode Handle(DataArray*, bool);
+    virtual bool SyncProperty(DataNode&, DataArray*, int, PropOp);
+    virtual void Save(BinStream&);
+    virtual void Copy(const Hmx::Object*, Hmx::Object::CopyType);
+    virtual void Load(BinStream&);
+
+    ObjPtr<RndMesh, ObjectDir> mMesh;
+    ObjPtr<RndTransformable, ObjectDir> mObject1;
+    ObjPtr<RndTransformable, ObjectDir> mObject2;
+    float mReferenceDistance;
+    float mMinDistance;
+    float mMaxDistance;
+    ObjPtr<RndTex, ObjectDir> mTex;
+};
+
+#endif
