5d7ae277ffc3279ef0ed738e2a689cfa400b068d
rjkiv <rjkiv@users.noreply.github.com>
2024-03-17 20:34:12 -0700

    charnecktwist work

--- src/system/char/CharNeckTwist.h
+++ src/system/char/CharNeckTwist.h
@@ -1,17 +1,17 @@
-#ifndef CHAR_CHARTRANSCOPY_H
-#define CHAR_CHARTRANSCOPY_H
+#ifndef CHAR_CHARNECKTWIST_H
+#define CHAR_CHARNECKTWIST_H
 #include "char/CharPollable.h"
 #include "obj/ObjPtr_p.h"
 #include "rndobj/Trans.h"
 
-class CharTransCopy : public CharPollable {
+class CharNeckTwist : public CharPollable {
 public:
-    CharTransCopy();
-    OBJ_CLASSNAME(CharTransCopy);
-    OBJ_SET_TYPE(CharTransCopy);
+    CharNeckTwist();
+    OBJ_CLASSNAME(CharNeckTwist);
+    OBJ_SET_TYPE(CharNeckTwist);
     virtual DataNode Handle(DataArray*, bool);
     virtual void Poll();
-    virtual ~CharTransCopy();
+    virtual ~CharNeckTwist(){}
     virtual void PollDeps(std::list<Hmx::Object*>&, std::list<Hmx::Object*>&);
     virtual bool SyncProperty(DataNode&, DataArray*, int, PropOp);
     virtual void Save(BinStream&);
@@ -21,8 +21,8 @@ public:
     DELETE_OVERLOAD;
     DECLARE_REVS;
 
-    ObjPtr<RndTransformable, class ObjectDir> mSrc;
-    ObjPtr<RndTransformable, class ObjectDir> mDest;
+    ObjPtr<RndTransformable, ObjectDir> mTwist;
+    ObjPtr<RndTransformable, ObjectDir> mHead;
 };
 
 #endif
