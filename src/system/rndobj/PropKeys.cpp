ccb3296dce4bd3de78d452eed38c429e6e7dd306
rjkiv <rjkiv@users.noreply.github.com>
2024-03-18 13:45:17 -0700

    some more work with propkeys

--- src/system/rndobj/PropKeys.cpp
+++ src/system/rndobj/PropKeys.cpp
@@ -2,7 +2,9 @@
 
 unsigned short PropKeys::gRev = 0;
 
-PropKeys::PropKeys(Hmx::Object* o1, Hmx::Object* o2) : mTarget(o1, o2), mProp(0), mKeysType(kFloat), mInterpHandler(), mLastKeyFrameIndex(-2) {
+PropKeys::PropKeys(Hmx::Object* o1, Hmx::Object* o2) : mTarget(o1, o2), mProp(0), mTrans(0),
+    mKeysType(kFloat), mInterpolation(kStep), mPropExceptionID(kNoException), 
+    mInterpHandler(), mLastKeyFrameIndex(-2) {
     
 }
 
@@ -12,3 +14,69 @@ PropKeys::~PropKeys(){
         mProp = 0;
     }
 }
+
+void PropKeys::SetProp(DataNode& node){
+    if(node.Type() == kDataArray){
+        DataArray* nodeArr = node.Array(0);
+        if(mProp){
+            mProp->Release();
+            mProp = 0;
+        }
+        mProp = nodeArr->Clone(true, false, 0);
+
+    }
+    else MILO_WARN("unknown prop set type");
+    SetPropExceptionID();
+}
+
+void PropKeys::ChangeFrame(int i, float f, bool b){
+    switch(mKeysType){
+        case kFloat:
+            AsFloatKeys();
+            break;
+        case kColor:
+            AsColorKeys();
+            break;
+        case kObject:
+            AsObjectKeys();
+            break;
+        case kBool:
+            AsBoolKeys();
+            break;
+        case kSymbol:
+            AsSymbolKeys();
+            break;
+        case kVector3:
+            AsVector3Keys();
+            break;
+        case kQuat:
+            AsQuatKeys();
+            break;
+        default:
+            MILO_WARN("can not replace frame, unknown type");
+            break;
+    }
+    if(b) ReSort();
+}
+
+float FloatKeys::StartFrame(){
+    if(size() != 0){
+        return (*this)[0];
+    }
+    else return 0.0f;
+}
+
+float FloatKeys::EndFrame(){
+    if(size() != 0){
+        return this->back();
+    }
+    else return 0.0f;
+}
+
+bool FloatKeys::FrameFromIndex(int idx, float& f){
+    if(size() <= idx) return false;
+    else f = (*this)[idx];
+    return true;
+}
+
+int FloatKeys::NumKeys(){ return size(); }
--- src/system/rndobj/PropKeys.h
+++ src/system/rndobj/PropKeys.h
@@ -45,11 +45,11 @@ class PropKeys : public ObjRef {
 public:
     PropKeys(Hmx::Object*, Hmx::Object*);
     virtual ~PropKeys();
-    virtual Hmx::Object* RefOwner();
-    virtual void Replace(Hmx::Object*, Hmx::Object*);
+    virtual Hmx::Object* RefOwner(){ return 0; }
+    virtual void Replace(Hmx::Object*, Hmx::Object*){}
     virtual float StartFrame(){ return 0.0f; }
     virtual float EndFrame(){ return 0.0f; }
-    virtual int FrameFromIndex(int, float&){ return 0; }
+    virtual bool FrameFromIndex(int, float&){ return 0; }
     virtual float SetFrame(float f1, float f2){ return f1; }
     virtual void CloneKey(int){}
     virtual int SetKey(float);
@@ -60,7 +60,7 @@ public:
     virtual void Save(BinStream&);
     virtual void Load(BinStream&);
     virtual void Copy(const PropKeys*);
-    virtual int AsFloatKeys(){ MILO_ASSERT(false, 0xA7); return 0; }
+    virtual Keys<float>& AsFloatKeys(){ MILO_ASSERT(false, 0xA7); }
     virtual int AsColorKeys(){ MILO_ASSERT(false, 0xA9); return 0; }
     virtual int AsObjectKeys(){ MILO_ASSERT(false, 0xAB); return 0; }
     virtual int AsBoolKeys(){ MILO_ASSERT(false, 0xAD); return 0; }
@@ -75,19 +75,42 @@ public:
     virtual int Vector3At(float, Vector3&){ MILO_ASSERT(false, 0xC0); return -1; }
     virtual int SymbolAt(float, Symbol&){ MILO_ASSERT(false, 0xC2); return -1; }
 
+    void SetProp(DataNode&);
+    void SetPropExceptionID();
+    void ChangeFrame(int, float, bool);
+    void ReSort();
+
     static unsigned short gRev;
 
     ObjOwnerPtr<Hmx::Object, ObjectDir> mTarget;
     DataArray* mProp;
-
-    // only one word here - could be any one or a combo of these
-    AnimKeysType mKeysType;
-    // Interpolation mInterpolation;
-    // ExceptionID mPropExceptionID;
-    // RndEnviron* mTrans;
-
+    RndEnviron* mTrans;
     Symbol mInterpHandler;
-    int mLastKeyFrameIndex;
+    AnimKeysType mKeysType : 1;
+    Interpolation mInterpolation : 1;
+    ExceptionID mPropExceptionID : 1;
+    int mLastKeyFrameIndex : 1;
+};
+
+class FloatKeys : public PropKeys, public Keys<float> {
+public:
+    virtual ~FloatKeys(){}
+    virtual float StartFrame();
+    virtual float EndFrame();
+    virtual bool FrameFromIndex(int, float&);
+    virtual float SetFrame(float f1, float f2);
+    virtual void CloneKey(int);
+    virtual int SetKey(float);
+    virtual int RemoveKey(int);
+    virtual int NumKeys();
+    virtual void SetToCurrentVal(int);
+    virtual void Save(BinStream&);
+    virtual void Load(BinStream&);
+    virtual void Copy(const PropKeys*);
+    virtual Keys<float>& AsFloatKeys(){
+        return *this;
+    }
+    virtual int FloatAt(float, float&);
 };
 
 #endif
