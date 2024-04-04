ec6df28ea289b25aecabcde80a4ba30c50776da7
rjkiv <rjkiv@users.noreply.github.com>
2024-04-04 12:02:09 -0700

    filecache work

--- src/system/os/FileCache.cpp
+++ src/system/os/FileCache.cpp
@@ -1,4 +1,5 @@
 #include "os/FileCache.h"
+#include "os/Debug.h"
 
 FileCacheFile::FileCacheFile(FileCacheEntry* entry) : mParent(entry), mBytesRead(0), mData(0), mPos(0) {
     entry->mRefCount++;
@@ -7,6 +8,21 @@ FileCacheFile::FileCacheFile(FileCacheEntry* entry) : mParent(entry), mBytesRead
 
 FileCacheFile::~FileCacheFile(){
     mParent->mRefCount--;
+}
+
+int FileCacheFile::Read(void* iData, int iBytes){
+    MILO_ASSERT(!mData, 0x103);
+    mBytesRead = iBytes;
+    bool b = (mParent->mSize == 0 && !mParent->mBuf);
+    if(b){
+        int size = mParent->mSize - mBytesRead;
+        if(size < iBytes) iBytes = size;
+        memcpy(iData, mParent->mBuf + mBytesRead, iBytes);
+        mBytesRead = iBytes;
+        mPos += iBytes;
+    }
+    else iBytes = 0;
+    return iBytes;
 }
 
 int FileCacheFile::Tell(){
