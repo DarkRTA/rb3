c0bc2040187a1e8328b515bc1856444244e19d2f
rjkiv <rjkiv@users.noreply.github.com>
2024-04-07 12:48:46 -0700

    usbmidikeyboard work

--- src/system/os/UsbMidiKeyboard.h
+++ src/system/os/UsbMidiKeyboard.h
@@ -1,23 +1,50 @@
 #ifndef OS_USBMIDIKEYBOARD_H
 #define OS_USBMIDIKEYBOARD_H
+#include "os/CritSec.h"
 
 class UsbMidiKeyboard {
 public:
     UsbMidiKeyboard();
     ~UsbMidiKeyboard();
 
-    bool mKeyPressed[8][128];
-    int mKeyVelocity[8][128];
-    int mModVal[8];
-    int mExpressionPedal[8];
-    int mConnectedAccessories[8];
-    bool mSustain[8];
-    bool mStompPedal[8];
-    int mAccelerometer[8][3];
-    int mLowHandPlacement[8];
-    int mHighHandPlacement[8];
+    static bool mUsbMidiKeyboardExists;
+    static void Init();
+    static void Terminate();
+    static void Poll();
+
+    int GetAccelAxisVal(int, int);
+    bool GetKeyPressed(int, int);
+    int GetKeyVelocity(int, int);
+    void SetAccelerometer(int, int, int, int);
+    void SetSustain(int, bool);
+    void SetStompPedal(int, bool);
+    void SetModVal(int, int);
+    void SetExpressionPedal(int, int);
+    void SetConnectedAccessories(int, int);
+    void SetLowHandPlacement(int, int);
+    void SetHighHandPlacement(int, int);
+    void SetKeyPressed(int, int, bool);
+    void SetKeyVelocity(int, int, int);
+
+    bool mKeyPressed[4][128];
+    int mKeyVelocity[4][128];
+    int mModVal[4];
+    int mExpressionPedal[4];
+    int mConnectedAccessories[4];
+    bool mSustain[4];
+    bool mStompPedal[4];
+    int mAccelerometer[4][3];
+    int mLowHandPlacement[4];
+    int mHighHandPlacement[4];
     int mPadNum;
 
+};
+
+class StaticCriticalSection : public CriticalSection {
+public:
+    StaticCriticalSection();
+    ~StaticCriticalSection();
+    static StaticCriticalSection* Instance();
 };
 
 #endif
