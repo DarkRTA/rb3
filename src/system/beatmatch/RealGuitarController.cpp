0e98349be99a4e5e38eee4f6ec318a6d8e84a747
rjkiv <rjkiv@users.noreply.github.com>
2024-04-10 00:28:39 -0700

    expand upon string strummed msg

--- src/system/beatmatch/RealGuitarController.cpp
+++ src/system/beatmatch/RealGuitarController.cpp
@@ -1,5 +1,6 @@
 #include "beatmatch/RealGuitarController.h"
 #include "beatmatch/RGState.h"
+#include "os/UsbMidiGuitar.h"
 
 RealGuitarController::RealGuitarController(User* user, const DataArray* cfg, BeatMatchControllerSink* bsink, bool disabled, bool lefty) : 
     BeatMatchController(user, cfg, false), mDisabled(disabled), mSink(bsink) {
@@ -26,5 +27,27 @@ int RealGuitarController::GetRGFret(int string) const {
 }
 
 int RealGuitarController::OnMsg(const StringStrummedMsg& msg){
-    
+    if(!IsOurPadNum(msg.GetPadNum())) return 0;
+    int fretnum = msg.GetFretNum();
+    if(fretnum > 0x16){
+        MILO_WARN("Warning: Fret # is %d:", fretnum);
+        return 0;
+    }
+    int i4 = 5 - msg.GetStringNum();
+    if(mLefty){
+        i4 = msg.GetStringNum();
+    }
+    int u5 = PackRGData(i4, fretnum);
+    mStrings[i4] = fretnum;
+    if(mDisabled) return 0;
+    if(msg.GetVelocity() < UsbMidiGuitar::E3CheatGetMinVelocity()){
+        RegisterRGFret(i4, fretnum);
+        mSink->FretButtonDown(u5, -1);
+        mSink->NonStrumSwing(u5, true, false);
+    }
+    else {
+        mSink->RGFretButtonDown(u5);
+        RegisterRGStrum(i4);
+    }
+    return 0;
 }
