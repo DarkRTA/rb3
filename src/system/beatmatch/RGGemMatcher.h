3f02b2d3f81a25382f2ed7ccea12a77bc6028e6c
rjkiv <rjkiv@users.noreply.github.com>
2024-04-19 22:56:32 -0700

    rg gem matcher header

---
+++ src/system/beatmatch/RGGemMatcher.h
@@ -0,0 +1,31 @@
+#ifndef BEATMATCH_RGGEMMATCHER_H
+#define BEATMATCH_RGGEMMATCHER_H
+#include "beatmatch/RGState.h"
+#include "beatmatch/GameGem.h"
+
+enum RGMatchType {
+    i, d, k
+};
+
+class RGGemMatcher {
+public:
+    RGGemMatcher();
+    void Reset();
+    void Swing(int, float);
+    void FretDown(int, float);
+    void FretUp(int, float);
+    bool FretMatch(const GameGem&, float, float, float, float, bool, bool, RGMatchType) const;
+    bool FretMatchImpl(const GameGem&, float, float, float, float, bool, bool, RGMatchType) const;
+    RGState* GetState();
+    RGState* GetState() const;
+    void ClearStringSwings();
+    void ClearNonStrums();
+    bool FretHistoryMatch(int, int, float, float, RGMatchType) const;
+    void AddFretHistory(int, int, float);
+
+    RGState mState;
+    float mStringSwings[6];
+    float mStringNonStrum[6];
+};
+
+#endif
