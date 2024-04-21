b366c2de1141355d0a7eb274f21d4e0714c37b6d
rjkiv <rjkiv@users.noreply.github.com>
2024-04-20 22:46:34 -0700

    a few more abstract classes

---
+++ src/system/beatmatch/HxSongData.h
@@ -0,0 +1,19 @@
+#ifndef BEATMATCH_HXSONGDATA_H
+#define BEATMATCH_HXSONGDATA_H
+
+// forward decs
+class TempoMap;
+class BeatMap;
+class MeasureMap;
+
+class HxSongData {
+public:
+    HxSongData(){}
+    virtual ~HxSongData(){}
+    virtual void CalcSongPos(float) = 0;
+    virtual TempoMap* GetTempoMap() const = 0;
+    virtual BeatMap* GetBeatMap() const = 0;
+    virtual MeasureMap* GetMeasureMap() const = 0;
+};
+
+#endif
