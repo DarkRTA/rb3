d03cf80f83c8fdb5f86f87f74ea78ffb5024aee7
rjkiv <rjkiv@users.noreply.github.com>
2024-05-15 16:49:03 -0700

    charpollgroup work

--- src/system/char/CharPollGroup.cpp
+++ src/system/char/CharPollGroup.cpp
@@ -1,9 +1,63 @@
 #include "char/CharPollGroup.h"
 
+INIT_REVS(CharPollGroup);
+
 CharPollGroup::CharPollGroup() : mPolls(this, kObjListNoNull), mChangedBy(this, 0), mChanges(this, 0) {
 
 }
 
 CharPollGroup::~CharPollGroup(){
     
+}
+
+void CharPollGroup::Enter(){
+    for(ObjPtrList<CharPollable, ObjectDir>::iterator it = mPolls.begin(); it != mPolls.end(); ++it){
+        (*it)->Enter();
+    }
+}
+
+void CharPollGroup::Exit(){
+    for(ObjPtrList<CharPollable, ObjectDir>::iterator it = mPolls.begin(); it != mPolls.end(); ++it){
+        (*it)->Exit();
+    }
+}
+
+void CharPollGroup::Poll(){
+    if(mWeightOwner->mWeight != 0.0f){
+        for(ObjPtrList<CharPollable, ObjectDir>::iterator it = mPolls.begin(); it != mPolls.end(); ++it){
+            (*it)->Poll();
+        }
+    }
+}
+
+void CharPollGroup::ListPollChildren(std::list<RndPollable*>& l) const {
+    for(ObjPtrList<CharPollable, ObjectDir>::iterator it = mPolls.begin(); it != mPolls.end(); ++it){
+        l.push_back(*it);
+    }
+}
+
+void CharPollGroup::PollDeps(std::list<Hmx::Object*>& changedBy, std::list<Hmx::Object*>& change){
+    if(mChangedBy || mChanges){
+        changedBy.push_back(mChangedBy);
+        change.push_back(mChanges);
+    }
+    else {
+        for(ObjPtrList<CharPollable, ObjectDir>::iterator it = mPolls.begin(); it != mPolls.end(); ++it){
+            (*it)->PollDeps(changedBy, change);
+        }
+    }
+}
+
+SAVE_OBJ(CharPollGroup, 0x58)
+
+void CharPollGroup::Load(BinStream& bs){
+    LOAD_REVS(bs);
+    ASSERT_REVS(3, 0);
+    Hmx::Object::Load(bs);
+    if(gRev > 2) CharWeightable::Load(bs);
+    bs >> mPolls;
+    if(gRev > 1){
+        bs >> mChangedBy;
+        bs >> mChanges;
+    }
 }
