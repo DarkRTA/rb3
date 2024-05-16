74bf56064b7b2c3e854c6fe764775eeeed2065da
rjkiv <rjkiv@users.noreply.github.com>
2024-05-15 19:33:20 -0700

    charweightsetter work

--- src/system/char/CharWeightSetter.cpp
+++ src/system/char/CharWeightSetter.cpp
@@ -9,3 +9,21 @@ void CharWeightSetter::SetWeight(float weight){
     mBaseWeight = weight;
     mWeight = weight;
 }
+
+void CharWeightSetter::PollDeps(std::list<Hmx::Object*>& changedBy, std::list<Hmx::Object*>& change){
+    changedBy.push_back(mDriver);
+    changedBy.push_back(mBase);
+    for(ObjPtrList<CharWeightSetter, ObjectDir>::iterator it = mMinWeights.begin(); it != mMinWeights.end(); ++it){
+        changedBy.push_back(*it);
+    }
+    for(ObjPtrList<CharWeightSetter, ObjectDir>::iterator it = mMaxWeights.begin(); it != mMaxWeights.end(); ++it){
+        changedBy.push_back(*it);
+    }
+    for(std::vector<ObjRef*>::reverse_iterator it = mRefs.rbegin(); it != mRefs.rend(); it++){
+        CharWeightable* weightowner = dynamic_cast<CharWeightable*>((*it)->RefOwner());
+        if(weightowner) change.push_back(weightowner);
+    }
+}
+
+SAVE_OBJ(CharWeightSetter, 0x73)
+
