e6b8b53281b25ef28613285daacc599f62aeafff
First Last <69217234+ieee802dot11ac@users.noreply.github.com>
2024-05-17 15:44:40 -0700

    revert config.py

--- configure.py
+++ configure.py
@@ -139,7 +139,7 @@ if not is_windows():
 # Tool versions
 config.binutils_tag = "2.41-1"
 config.compilers_tag = "20231018"
-config.dtk_tag = "v0.8.1"
+config.dtk_tag = "v0.7.2"
 config.sjiswrap_tag = "v1.1.1"
 config.wibo_tag = "0.6.11"
 
