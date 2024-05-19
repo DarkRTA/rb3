f81d770b43bdac305c6faab3bacc95e615fe89fe
rjkiv <rjkiv@users.noreply.github.com>
2024-05-19 12:34:00 -0700

    paneldir work

--- src/system/ui/PanelDir.cpp
+++ src/system/ui/PanelDir.cpp
@@ -1,5 +1,7 @@
 #include "ui/PanelDir.h"
+#include "ui/UIComponent.h"
+#include "rndobj/Cam.h"
 
-PanelDir::PanelDir(){
+PanelDir::PanelDir() : mFocusComponent(0), mPanel(0), mCam(this, 0), mCanEndWorld(1), mUseSpecifiedCam(0), mShowEditModePanels(0), mShowFocusComponent(1) {
     
 }
