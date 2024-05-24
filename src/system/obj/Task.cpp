3c9c957929f0429d2da98100139b23a8ec30212a
rjkiv <rjkiv@users.noreply.github.com>
2024-05-24 10:40:37 -0700

    task work

---
+++ src/system/obj/Task.cpp
@@ -0,0 +1,43 @@
+#include "obj/Task.h"
+#include "obj/DataUtl.h"
+
+MessageTask::MessageTask(Hmx::Object* o, DataArray* msg) : mObj(this, o), mMsg(msg) {
+    MILO_ASSERT(msg, 0x1D);
+    msg->AddRef();
+}
+
+MessageTask::~MessageTask(){
+    if(mMsg){
+        mMsg->Release();
+        mMsg = 0;
+    }
+}
+
+void MessageTask::Replace(Hmx::Object* from, Hmx::Object* to){
+    Hmx::Object::Replace(from, to);
+    if(from == mObj){
+        if(!to) delete this;
+        else mObj = to;
+    }
+}
+
+void MessageTask::Poll(float f){
+    if(mObj && mMsg){
+        DataNode r = mObj->Handle(mMsg, false);
+        if(r.Type() != kDataUnhandled) delete this;
+    }
+}
+
+ScriptTask::ScriptTask(DataArray* arr1, bool b, DataArray* arr2) : mThis(this, DataThis()), mScript(arr1), mOnce(b) {
+    arr1->AddRef();
+    static DataNode* task = DataVariable("task");
+    DataNode obj(*task);
+    *task = DataNode(this);
+    Var var;
+    var.var = task;
+    var.value = obj;
+    mVars.push_back(var);
+    if(!arr2) arr2 = arr1;
+    UpdateVarsObjects(arr2);
+    *task = obj;
+}
