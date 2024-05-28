3073e04fcbcae97ad25f7b5328189f0b984040db
rjkiv <rjkiv@users.noreply.github.com>
2024-05-28 16:47:13 -0700

    uiproxy header added

---
+++ src/system/ui/UIProxy.h
@@ -0,0 +1,34 @@
+#ifndef UI_UIPROXY_H
+#define UI_UIPROXY_H
+#include "ui/UIComponent.h"
+#include "obj/Dir.h"
+#include "rndobj/Dir.h"
+
+class UIProxy : public UIComponent {
+public:
+    UIProxy();
+    OBJ_CLASSNAME(UIProxy)
+    OBJ_SET_TYPE(UIProxy)
+    virtual DataNode Handle(DataArray*, bool);
+    virtual bool SyncProperty(DataNode&, DataArray*, int, PropOp);
+    virtual void Save(BinStream&);
+    virtual void Copy(const Hmx::Object*, Hmx::Object::CopyType);
+    virtual void Load(BinStream&);
+    virtual void DrawShowing();
+    virtual int CollideShowing(const Segment&, float&, Plane&);
+    virtual int CollidePlane(const Plane&);
+    virtual ~UIProxy();
+    virtual void SetTypeDef(DataArray*);
+    virtual void PreLoad(BinStream&);
+    virtual void PostLoad(BinStream&);
+    virtual void Poll();
+
+    ObjDirPtr<RndDir> mDir; // 0x10c
+    ObjPtr<RndEnviron, ObjectDir> mEnv; // 0x118
+    RndEnviron* mMainTrans; // 0x124
+    Transform mOldXfm; // 0x128
+    bool mSyncOnMove; // 0x158
+    bool mPolled; // 0x159
+};
+
+#endif
