7f5acb62a3843d54de2473b5c00a5ff6adc59c67
rjkiv <rjkiv@users.noreply.github.com>
2024-05-29 14:50:35 -0700

    worldcrowd virtual funcs

--- src/system/world/Crowd.h
+++ src/system/world/Crowd.h
@@ -5,6 +5,25 @@
 class WorldCrowd : public RndDrawable {
 public:
     WorldCrowd();
+    OBJ_CLASSNAME(WorldCrowd);
+    OBJ_SET_TYPE(WorldCrowd);
+    virtual DataNode Handle(DataArray*, bool);
+    virtual bool SyncProperty(DataNode&, DataArray*, int, PropOp);
+    virtual void Save(BinStream&);
+    virtual void Copy(const Hmx::Object*, Hmx::Object::CopyType);
+    virtual void Load(BinStream&);
+    virtual void UpdateSphere();
+    virtual float GetDistanceToPlane(const Plane&, Vector3&);
+    virtual bool MakeWorldSphere(Sphere&, bool);
+    virtual void Mats(std::list<class RndMat*>&, bool);
+    virtual void DrawShowing();
+    virtual void ListDrawChildren(std::list<RndDrawable*>&);
+    virtual void CollideList(const Segment&, std::list<Collision>&);
+    virtual ~WorldCrowd();
+    virtual void ListPollChildren(std::list<RndPollable*>&) const;
+    virtual void Poll();
+    virtual void Enter();
+    virtual void Exit();
 };
 
 #endif
