74c8239ef5b80d93a328d969c725ce44489e7bf1
Nathan <nathanhurst03@gmail.com>
2024-05-31 04:08:23 -0600

    Final fixes for global `-ipa file`

--- src/system/utl/HangBlock_Wii.cpp
+++ src/system/utl/HangBlock_Wii.cpp
@@ -2,6 +2,8 @@
 #include "os/Timer.h"
 #include "os/CritSec.h"
 
+#include "decomp.h"
+
 int AutoHangHelper::sLast = 0;
 int AutoHangHelper::sCount = 0;
 int AutoHangHelper::sCrit = 0;
@@ -13,6 +15,11 @@ namespace {
     CriticalSection g_AutoHelpHangerCrit;
 }
 
+DECOMP_FORCEACTIVE(HangBlock_Wii,
+    gHangMs,
+    gHangFrac
+)
+
 void AutoHangHelper::Init(){
-    
+
 }
