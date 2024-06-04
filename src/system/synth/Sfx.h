11550e2bc0b2d7954b4cc854846545f99c927434
rjkiv <rjkiv@users.noreply.github.com>
2024-06-03 20:56:56 -0700

    sfx work

--- src/system/synth/Sfx.h
+++ src/system/synth/Sfx.h
@@ -1,12 +1,39 @@
 #ifndef SYNTH_SFX_H
 #define SYNTH_SFX_H
 #include "synth/Sequence.h"
+#include "obj/ObjPtr_p.h"
+#include "obj/ObjVector.h"
+#include "synth/MoggClipMap.h"
+#include "synth/SfxMap.h"
+#include "synth/SfxInst.h"
+
+class FxSend;
 
 class Sfx : public Sequence {
 public:
     Sfx();
+    virtual ~Sfx(){}
+    OBJ_CLASSNAME(Sfx);
+    OBJ_SET_TYPE(Sfx);
+    virtual DataNode Handle(DataArray*, bool);
+    virtual bool SyncProperty(DataNode&, DataArray*, int, PropOp);
+    virtual void Save(BinStream&);
+    virtual void Copy(const Hmx::Object*, Hmx::Object::CopyType);
+    virtual void Load(BinStream&);
+    virtual SeqInst* MakeInstImpl();
+    virtual void SynthPoll();
 
-    int dummysfx;
+    NEW_OVERLOAD;
+    DELETE_OVERLOAD;
+
+    void Pause(bool);
+
+    ObjVector<SfxMap> mMaps; // 0x68
+    ObjVector<MoggClipMap> mMoggClipMaps; // 0x74
+    ObjPtr<FxSend, ObjectDir> mSend; // 0x80
+    float mReverbMixDb; // 0x8c
+    bool mReverbEnable; // 0x90
+    ObjPtrList<SfxInst, ObjectDir> mSfxInsts; // 0x94
 };
 
 #endif
--- src/system/synth/SfxInst.h
+++ src/system/synth/SfxInst.h
@@ -1,13 +1,29 @@
 #ifndef SYNTH_SFXINST_H
 #define SYNTH_SFXINST_H
 #include "synth/SeqInst.h"
-#include "synth/Sfx.h"
+#include "synth/MoggClipMap.h"
+
+class Sfx;
 
 class SfxInst : public SeqInst {
 public:
     SfxInst(Sfx*);
+    virtual ~SfxInst(){}
+    virtual void Stop();
+    virtual bool IsRunning();
+    virtual void UpdateVolume();
+    virtual void SetPan(float);
+    virtual void SetTranspose(float);
+    virtual void StartImpl();
 
-    int dummysfxinst;
+    void Pause(bool);
+
+    NEW_POOL_OVERLOAD(SfxInst);
+    DELETE_POOL_OVERLOAD(SfxInst);
+
+    std::vector<int> mSamples; // 0x34
+    ObjPtrList<MoggClipMap, ObjectDir> mMoggClips; // 0x3c
+    float mStartProgress; // 0x4c
 };
 
 #endif
