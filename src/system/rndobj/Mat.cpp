40bfe74bf14527adb32356a9870a3c4e1fb105a3
rjkiv <rjkiv@users.noreply.github.com>
2024-06-10 20:34:25 -0700

    rndmat sucks

--- src/system/rndobj/Mat.cpp
+++ src/system/rndobj/Mat.cpp
@@ -1,6 +1,7 @@
 #include "rndobj/Mat.h"
 #include "obj/ObjMacros.h"
 #include "obj/Object.h"
+#include "utl/Symbols.h"
 
 MatShaderOptions::MatShaderOptions() : i4(1), i1(1), b(0) {
 
@@ -46,7 +47,58 @@ BEGIN_LOADS(RndMat)
     } else mRefractStrength = 0;
 END_LOADS
 
-
+#pragma push
+#pragma pool_data off
 BEGIN_PROPSYNCS(RndMat)
-    if (sym == "intensify") { bool x = mIntensify; return PropSync(x, _val, _prop, _i + 1, _op); }
+    {
+        static Symbol _s("intensify");
+        bool bit = mIntensify;
+        if(sym == _s){
+            bool ret = PropSync(bit, _val, _prop, _i + 1, _op);
+            mIntensify = bit;
+            if(!(_op & (kPropSize|kPropGet))){
+                unkb4p1 = 0;
+            }
+            return ret;
+        }
+    }
+    SYNC_PROP_MODIFY_ALT(color, mColor, unkb4p1 = 0)
+    SYNC_PROP_MODIFY(alpha, mColor.alpha, unkb4p1 = 0)
+    {
+        static Symbol _s("use_environ");
+        bool bit = mUseEnviron;
+        if(sym == _s){
+            bool ret = PropSync(bit, _val, _prop, _i + 1, _op);
+            mUseEnviron = bit;
+            if(!(_op & (kPropSize|kPropGet))){
+                unkb4p1 = 0;
+            }
+            return ret;
+        }
+    }
+    {
+        static Symbol _s("blend");
+        Blend bit = mBlend;
+        if(sym == _s){
+            bool ret = PropSync((int&)bit, _val, _prop, _i + 1, _op);
+            mBlend = bit;
+            if(!(_op & (kPropSize|kPropGet))){
+                unkb4p1 = 0;
+            }
+            return ret;
+        }
+    }
+    {
+        static Symbol _s("z_mode");
+        int bit = unkb0p2;
+        if(sym == _s){
+            bool ret = PropSync(bit, _val, _prop, _i + 1, _op);
+            unkb0p2 = bit;
+            if(!(_op & (kPropSize|kPropGet))){
+                unkb4p1 = 0;
+            }
+            return ret;
+        }
+    }
 END_PROPSYNCS
+#pragma pop
