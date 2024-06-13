d8459b9c0cf621a03b402a618e6f3a29d3519a25
rjkiv <rjkiv@users.noreply.github.com>
2024-06-13 15:09:07 -0700

    uiliststate work

--- src/system/ui/UIListState.h
+++ src/system/ui/UIListState.h
@@ -1,5 +1,6 @@
 #ifndef UI_UILISTSTATE_H
 #define UI_UILISTSTATE_H
+#include "ui/UIListProvider.h"
 #include "types.h"
 #include <vector>
 
@@ -9,23 +10,42 @@ class UIListState { // 0x48
 public:
     UIListState(class UIListProvider*, UIListStateCallback*);
 
+    int Display2Showing(int) const;
+    int Display2Data(int) const;
+    int Showing2Data(int) const;
+    int SelectedDisplay() const;
+    bool IsScrolling() const;
+    int MinDisplay() const;
+    int MaxDisplay() const;
+    float Speed() const;
+    bool ScrollPastMinDisplay() const;
+    bool ScrollPastMaxDisplay() const;
     bool CanScrollBack(bool) const;
     bool CanScrollNext(bool) const;
+    float StepPercent() const;
+    UIListProvider* Provider();
+    UIListProvider* Provider() const;
+    int WrapShowing(int) const;
+    int NumDisplayWithData() const;
+
+    int NumShowing() const { return mProvider->NumData() - mHiddenData.size(); }
 
     bool mCircular; // 0x0
     int mNumDisplay; // 0x4
     int mGridSpan; // 0x8
-    float unk_0xC;
-    int unk_0x10;
-    u8 unk_0x14;
-    int unk_0x18;
-    u8 unk_0x1C;
-    int unk_0x20;
-    std::vector<int> unk_0x24;
-    int unk_0x2C, unk_0x30, unk_0x34;
-    float unk_0x38, unk_0x3C;
-    int unk_0x40;
-    // TODO: fill out the rest
+    float mSpeed; // 0xc
+    int mMinDisplay; // 0x10
+    bool mScrollPastMinDisplay; // 0x14
+    int mMaxDisplay; // 0x18
+    bool mScrollPastMaxDisplay; // 0x1c
+    UIListProvider* mProvider; // 0x20
+    std::vector<int> mHiddenData; // 0x24
+    int mFirstShowing; // 0x2c
+    int mTargetShowing; // 0x30
+    int mSelectedDisplay; // 0x34
+    float mStepPercent; // 0x38
+    float mStepTime; // 0x3c
+    UIListStateCallback* mCallback; // 0x40
 };
 
 class UIListStateCallback {
