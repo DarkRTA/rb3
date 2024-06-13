1862a33b7fbbbddc08874478bffe0f90fc99762c
rjkiv <rjkiv@users.noreply.github.com>
2024-06-13 10:53:07 -0700

    match uilisthighlight

--- src/system/ui/UIListHighlight.cpp
+++ src/system/ui/UIListHighlight.cpp
@@ -1,4 +1,5 @@
 #include "ui/UIListHighlight.h"
+#include "ui/UIList.h"
 #include "utl/Symbols.h"
 
 INIT_REVS(UIListHighlight)
@@ -23,8 +24,17 @@ BEGIN_COPYS(UIListHighlight)
     COPY_MEMBER_FROM(h, mMesh)
 END_COPYS
 
-void UIListHighlight::Draw(const UIListWidgetDrawState&, const UIListState&, const Transform&, UIComponent::State, Box*, DrawCommand){
-
+void UIListHighlight::Draw(const UIListWidgetDrawState& drawstate, const UIListState& liststate, const Transform& tf, UIComponent::State compstate, Box* box, DrawCommand cmd){
+    if(!mMesh || cmd == kDrawFirst) return;
+    Transform& worldxfm = mMesh->WorldXfm();
+    Transform xfm1 = worldxfm;
+    Transform xfm2 = xfm1;
+    if(ParentList()){
+        ParentList()->AdjustTransSelected(xfm2);
+    }
+    CalcXfm(tf, drawstate.mHighlightPos, xfm2);
+    DrawMesh(mMesh, drawstate.mHighlightElementState, compstate, xfm2, box);
+    mMesh->SetWorldXfm(xfm1);
 }
 
 BEGIN_HANDLERS(UIListHighlight)
