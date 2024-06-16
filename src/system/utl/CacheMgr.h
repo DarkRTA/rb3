7bf311403a6612770146de2089cfaaa02afa082a
rjkiv <rjkiv@users.noreply.github.com>
2024-06-16 12:27:56 -0700

    cachemgr matched

--- src/system/utl/CacheMgr.h
+++ src/system/utl/CacheMgr.h
@@ -9,21 +9,26 @@ void CacheMgrInit();
 void CacheMgrTerminate();
 
 class CacheMgr {
-protected:
+public:
     enum OpType {};
 
     struct CacheIDStoreEntry {
+        CacheIDStoreEntry(Symbol s, CacheID* cid) : symbol(s), id(cid) {}
         Symbol symbol;
         CacheID* id;
     };
 
-public:
     CacheMgr();
     virtual ~CacheMgr();
+    virtual void Poll() = 0;
     virtual bool SearchAsync(const char*, CacheID**);
-    bool ShowUserSelectUIAsync(LocalUser*, unsigned long long, const char*, const char*, CacheID**);
-    bool CreateCacheIDFromDeviceID(unsigned int, const char*, const char*, CacheID**);
+    virtual bool ShowUserSelectUIAsync(LocalUser*, unsigned long long, const char*, const char*, CacheID**);
+    virtual bool CreateCacheIDFromDeviceID(unsigned int, const char*, const char*, CacheID**);
     virtual bool CreateCacheID(const char*, const char*, const char*, const char*, const char*, int, CacheID**);
+    virtual bool MountAsync(CacheID*, Cache*, Hmx::Object*) = 0;
+    virtual bool UnmountAsync(Cache**, Hmx::Object*) = 0;
+    virtual bool DeleteAsync(CacheID*) = 0;
+
     bool IsDone();
     CacheResult GetLastResult();
     void AddCacheID(CacheID*, Symbol);
@@ -32,13 +37,13 @@ public:
     void SetOp(CacheMgr::OpType);
     CacheMgr::OpType GetOp();
     void SetLastResult(CacheResult);
-private:
+
     std::vector<CacheMgr::CacheIDStoreEntry> mCacheIDStore;
     CacheMgr::OpType mOp;       // 0x0c
     CacheResult mLastResult;    // 0x10
     int mUnk;                   // 0x1c
 };
 
-static CacheMgr* TheCacheMgr;
+extern CacheMgr* TheCacheMgr;
 
 #endif // UTL_CACHEMGR_H
