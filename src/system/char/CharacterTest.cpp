ed227df4679a06e080af4bfee8ad6f73d0a61a1d
rjkiv <rjkiv@users.noreply.github.com>
2024-06-17 16:02:15 -0700

    charactertest work

--- src/system/char/CharacterTest.cpp
+++ src/system/char/CharacterTest.cpp
@@ -6,9 +6,11 @@
 #include "char/Waypoint.h"
 #include "utl/Symbols.h"
 
+INIT_REVS(CharacterTest)
+
 CharacterTest::CharacterTest(class Character* thechar) : mMe(thechar), mDriver(thechar, 0), mClip1(thechar, 0), mClip2(thechar, 0), mFilterGroup(thechar, 0), 
-    mTeleportTo(thechar, 0), mWalkPath(thechar, kObjListNoNull), mShowDistMap(), mTransition(0), unk5c(1), unk5d(0), unk5e(0), unk5f(0), unk60(0), unk68(0), unk6c(0), 
-    mOverlay(RndOverlay::Find("char_test", true)) {
+    mTeleportTo(thechar, 0), mWalkPath(thechar, kObjListNoNull), mShowDistMap(), mTransition(0), mCycleTransition(1), mMetronome(0), mZeroTravel(0), 
+    mShowScreenSize(0), mShowFootExtents(0), unk68(0), unk6c(0), mOverlay(RndOverlay::Find("char_test", true)) {
     mShowDistMap = none;
 }
 
@@ -23,3 +25,14 @@ CharacterTest::~CharacterTest(){
         }
     }
 }
+
+BEGIN_LOADS(CharacterTest)
+    LOAD_REVS(bs)
+    if(gRev > 0xF){
+        TheDebugFailer << MakeString("%s can't load new %s version %d > %d", PathName(mMe), "CharacterTesting", gRev, (unsigned short)0xF);
+    }
+    if(gAltRev != 0){
+        TheDebugFailer << MakeString("%s can't load new %s alt version %d > %d", PathName(mMe), "CharacterTesting", gAltRev, (unsigned short)0);
+    }
+    if(gRev != 0xD) mDriver.Load(bs, false, mMe);
+END_LOADS
