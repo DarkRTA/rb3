4565740aaade23310dac97272f8e945e382ad62a
rjkiv <rjkiv@users.noreply.github.com>
2024-06-19 16:55:26 -0700

    rndmorph tu added

---
+++ src/system/rndobj/Morph.cpp
@@ -0,0 +1,5 @@
+#include "rndobj/Morph.h"
+
+RndMorph::RndMorph() : mPoses(this), mTarget(this, 0), mNormals(0), mSpline(0), mIntensity(1.0f) {
+
+}
--- src/system/rndobj/Morph.h
+++ src/system/rndobj/Morph.h
@@ -1,9 +1,16 @@
 #ifndef RNDOBJ_MORPH_H
 #define RNDOBJ_MORPH_H
 #include "rndobj/Anim.h"
+#include "obj/ObjVector.h"
+#include "rndobj/Mesh.h"
 
 class RndMorph : public RndAnimatable {
 public:
+    class Pose {
+    public:
+        Pose(Hmx::Object*);
+    };
+
     RndMorph();
     OBJ_CLASSNAME(Morph);
     OBJ_SET_TYPE(Morph);
@@ -20,9 +27,13 @@ public:
     NEW_OVERLOAD;
     DELETE_OVERLOAD;
     NEW_OBJ(RndMorph)
-    static void Init(){
-        REGISTER_OBJ_FACTORY(RndMorph)
-    }
+    static void Init(){ REGISTER_OBJ_FACTORY(RndMorph) }
+
+    ObjVector<Pose> mPoses; // 0x10
+    ObjPtr<RndMesh, ObjectDir> mTarget; // 0x1c
+    bool mNormals; // 0x28
+    bool mSpline; // 0x29
+    float mIntensity; // 0x2c
 };
 
 #endif
