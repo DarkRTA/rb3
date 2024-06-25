f07d13b2e48679f0e63d711291958b66406e36ba
Nathan <nathanhurst03@gmail.com>
2024-06-25 16:34:56 -0600

    setjmp/longjmp

--- src/sdk/PowerPC_EABI_Support/MSL/MSL_C/setjmp.h
+++ src/sdk/PowerPC_EABI_Support/MSL/MSL_C/setjmp.h
@@ -1,13 +1,14 @@
 #ifndef _SETJMP_H
 #define _SETJMP_H
 
+#include "PowerPC_EABI_Support/Runtime/Gecko_setjmp.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-struct jmp_buf; /* TODO */
-#define setjmp(env)
-void longjmp(struct jmp_buf env, int status);
+typedef struct __jmp_buf jmp_buf;
+#define setjmp(env) __setjmp(&env)
 
 #ifdef __cplusplus
 }
---
+++ src/sdk/PowerPC_EABI_Support/Runtime/Gecko_setjmp.h
@@ -0,0 +1,26 @@
+#ifndef RUNTIME_GECKO_SETJMP_H
+#define RUNTIME_GECKO_SETJMP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct __jmp_buf {
+    unsigned long lr;
+    unsigned long cr;
+    unsigned long sp;
+    unsigned long toc;
+    unsigned long _padding;
+    unsigned long gprs[32 - 13];
+    double fprs[(32 - 14) * 2];
+    double fpscr;
+};
+
+int __setjmp(struct __jmp_buf* env);
+void longjmp(struct __jmp_buf* env, int status);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
