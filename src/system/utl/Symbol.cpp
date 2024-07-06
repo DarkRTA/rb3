0bf45b4337bdf0628fa935054f0c5c5ea5e5d1b8
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-07-05 20:25:00 -0400

    symbol cache lookup

--- src/system/utl/Symbol.cpp
+++ src/system/utl/Symbol.cpp
@@ -6,16 +6,44 @@
 #include "utl/PoolAlloc.h"
 #include <new>
 
-bool gLiteralSymbolStaticInitialization = false;
-static KeylessHash<const char*, const char*>* gHashTable;
 static StringTable* gStringTable;
+static KeylessHash<const char*, const char*>* gHashTable;
+bool gLiteralSymbolStaticInitialization = false;
+
+const char* SymbolCacheLookup(const char* cc){
+    const char** found = gHashTable ? gHashTable->Find(cc) : 0;
+    return found ? *found : 0;
+}
+
+// undefined4 SymbolCacheLookup(char *param_1)
+
+// {
+//   undefined4 *puVar1;
+//   undefined4 uVar2;
+//   char *local_8 [2];
+  
+//   if (gHashTable == (KeylessHash<> *)0x0) {
+//     puVar1 = (undefined4 *)0x0;
+//   }
+//   else {
+//     local_8[0] = param_1;
+//     puVar1 = (undefined4 *)KeylessHash<>::Find(gHashTable,local_8);
+//   }
+//   if (puVar1 == (undefined4 *)0x0) {
+//     uVar2 = 0;
+//   }
+//   else {
+//     uVar2 = *puVar1;
+//   }
+//   return uVar2;
+// }
 
 Symbol::Symbol(const char* str){
     if(str == 0 || *str == '\0') mStr = gNullStr;
     else {
         if(gHashTable){
-            const char* found = *gHashTable->Find(str);
-            if(found) mStr = found;
+            const char** found = gHashTable->Find(str);
+            if(found) mStr = *found;
             else {
                 if(gLiteralSymbolStaticInitialization) mStr = str;
                 else mStr = gStringTable->Add(str);
