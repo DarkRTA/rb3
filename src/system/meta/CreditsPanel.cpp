ba52c0ab98bfc0909af7c8c4ad33fe9dbc85b122
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-07-08 12:09:53 -0400

    begin working on creditspanel

---
+++ src/system/meta/CreditsPanel.cpp
@@ -0,0 +1,9 @@
+#include "meta/CreditsPanel.h"
+
+CreditsPanel::CreditsPanel() : mLoader(0), mNames(0), mStream(0), mAutoScroll(1), mSavedSpeed(-1.0f), mPaused(0) {
+
+}
+
+CreditsPanel::~CreditsPanel(){
+    
+}
---
+++ src/system/meta/CreditsPanel.h
@@ -0,0 +1,37 @@
+#ifndef META_CREDITSPANEL_H
+#define META_CREDITSPANEL_H
+#include "ui/UIListProvider.h"
+#include "ui/UIPanel.h"
+#include "synth/Stream.h"
+#include "obj/DataFile.h"
+
+class CreditsPanel : public UIListProvider, public UIPanel {
+public:
+    CreditsPanel();
+    virtual ~CreditsPanel();
+    virtual void Text(int, int, UIListLabel*, UILabel*) const;
+    virtual RndMat* Mat(int, int, UIListMesh*) const;
+    virtual int NumData() const;
+    OBJ_CLASSNAME(CreditsPanel);
+    OBJ_SET_TYPE(CreditsPanel);
+    virtual DataNode Handle(DataArray*, bool);
+    virtual void Load();
+    virtual bool IsLoaded() const;
+    virtual void FinishLoad();
+    virtual void Unload();
+    virtual void Enter();
+    virtual void Exit();
+    virtual bool Exiting() const;
+    virtual void Poll();
+
+    bool mCheatOn; // 0x3c
+    DataLoader* mLoader; // 0x40
+    DataArray* mNames; // 0x44
+    UIList* mList; // 0x48
+    Stream* mStream; // 0x4c
+    bool mAutoScroll; // 0x50
+    float mSavedSpeed; // 0x54
+    bool mPaused; // 0x58
+};
+
+#endif
