1bae15e54de9d589fcfd7f343267f5b3b9824ad9
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-07-08 10:43:00 -0400

    FSSS nearly done

--- src/system/meta/FixedSizeSaveableStream.cpp
+++ src/system/meta/FixedSizeSaveableStream.cpp
@@ -1,4 +1,5 @@
 #include "meta/FixedSizeSaveableStream.h"
+#include "meta/FixedSizeSaveable.h"
 
 FixedSizeSaveableStream::FixedSizeSaveableStream(void* v, int i, bool b) : BufStream(v, i, b), m_iCurrentID(0), m_iTableOffset(0) {
 
@@ -12,10 +13,62 @@ void FixedSizeSaveableStream::InitializeTable(){
     m_mapIDToSymbol.clear();
     m_mapSymbolToID.clear();
     m_iTableOffset = Tell();
+    FixedSizeSaveable::SaveSymbolTable(*this, FixedSizeSaveable::GetMaxSymbols(), 0x36);
+}
+
+void FixedSizeSaveableStream::SaveTable(){
+    int start = Tell();
+    Seek(m_iTableOffset, BinStream::kSeekBegin);
+    FixedSizeSaveable::SaveSymbolTable(*this, FixedSizeSaveable::GetMaxSymbols(), 0x36);
+    Seek(start, BinStream::kSeekBegin);
+}
+
+int FixedSizeSaveableStream::GetSymbolTableSize(int){
+    return FixedSizeSaveable::GetMaxSymbols() * 0x36 + 4;
+}
+
+void FixedSizeSaveableStream::LoadTable(int){
+    m_mapIDToSymbol.clear();
+    m_mapSymbolToID.clear();
+    FixedSizeSaveable::LoadSymbolTable(*this, FixedSizeSaveable::GetMaxSymbols(), 0x36);
 }
 
 std::map<Symbol, int>& FixedSizeSaveableStream::GetSymbolToIDMap(){
     return m_mapSymbolToID;
+}
+
+bool FixedSizeSaveableStream::HasSymbol(Symbol s) const {
+    return GetID(s) != -1;
+}
+
+bool FixedSizeSaveableStream::HasID(int i) const {
+    return GetSymbol(i) != "";
+}
+
+int FixedSizeSaveableStream::GetID(Symbol s) const {
+    std::map<Symbol, int>::const_iterator it = m_mapSymbolToID.find(s);
+    if(it != m_mapSymbolToID.end()) return it->second;
+    else return -1;
+}
+
+Symbol FixedSizeSaveableStream::GetSymbol(int i) const {
+    std::map<int, Symbol>::const_iterator it = m_mapIDToSymbol.find(i);
+    if(it != m_mapIDToSymbol.end()) return it->second;
+    else return "";
+}
+
+int FixedSizeSaveableStream::AddSymbol(Symbol i_symSymbol){
+    MILO_ASSERT(!HasSymbol( i_symSymbol ), 0x83);
+    m_iCurrentID++;
+    MILO_ASSERT(!HasID( m_iCurrentID ), 0x86);
+    m_mapSymbolToID[i_symSymbol] = m_iCurrentID;
+    m_mapIDToSymbol[m_iCurrentID] = i_symSymbol;
+    return m_iCurrentID;
+}
+
+void FixedSizeSaveableStream::SetSymbolID(Symbol s, int i){
+    m_mapSymbolToID[s] = i;
+    m_mapIDToSymbol[i] = s;
 }
 
 int FixedSizeSaveableStream::ReadInt(){
