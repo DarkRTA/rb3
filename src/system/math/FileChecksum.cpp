c5a8076ddc391e03312b081952c3b952d50cfcc2
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-07-08 19:52:27 -0400

    filechecksum nearly matched

--- src/system/math/FileChecksum.cpp
+++ src/system/math/FileChecksum.cpp
@@ -1,13 +1,11 @@
 #include "math/FileChecksum.h"
+#include "os/File.h"
 #include <vector>
 
 std::vector<ChecksumData> gChecksumData;
 
 void SetFileChecksumData(FileChecksum* fc, int i){
-    ChecksumData data;
-    data.start = fc;
-    data.end = &fc[i];
-    gChecksumData.push_back(data);
+    gChecksumData.push_back(ChecksumData(fc, &fc[i]));
 }
 
 void ClearFileChecksumData(){
@@ -16,4 +14,20 @@ void ClearFileChecksumData(){
 
 bool HasFileChecksumData(){
     return false;
+}
+
+unsigned char* GetFileChecksum(const char* cc, bool b){
+    const char* fname = b ? FileGetName(cc) : FileLocalize(cc, 0);
+    std::vector<ChecksumData>::iterator it = gChecksumData.begin();
+    std::vector<ChecksumData>::iterator itEnd = gChecksumData.end();
+    for(; it != itEnd; it++){
+        FileChecksum* sum = (*it).start;
+        FileChecksum* sumEnd = (*it).end;
+        for(; sum != sumEnd; sum++){
+            if(strcmp(sum->file, fname) == 0){
+                return sum->checksum;
+            }
+        }
+    }
+    return 0;
 }
