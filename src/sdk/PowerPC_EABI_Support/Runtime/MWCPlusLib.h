8bb13e05ade3f1125d256a7068477f71ef40f4bf
Nathan <nathanhurst03@gmail.com>
2024-07-13 14:21:04 -0600

    Runtime/global_destructor_chain.c

--- src/sdk/PowerPC_EABI_Support/Runtime/MWCPlusLib.h
+++ src/sdk/PowerPC_EABI_Support/Runtime/MWCPlusLib.h
@@ -7,28 +7,15 @@
 extern "C" {
 #endif
 
-#define CTORARG_TYPE int
-#define CTORARG_PARTIAL (0)
-#define CTORARG_COMPLETE (1)
+typedef void *ConstructorDestructor;
 
-#define CTORCALL_COMPLETE(ctor, objptr) (((void (*)(void*, CTORARG_TYPE))ctor)(objptr, CTORARG_COMPLETE))
+extern void __construct_array(void *ptr, ConstructorDestructor ctor, ConstructorDestructor dtor, size_t size, size_t n);
+extern void __destroy_arr(void *block, ConstructorDestructor *dtor, size_t size, size_t n);
 
-#define DTORARG_TYPE int
-
-#define DTORCALL_COMPLETE(dtor, objptr) (((void (*)(void*, DTORARG_TYPE))dtor)(objptr, -1))
-#define DTORCALL_PARTIAL(dtor,objptr) (((void (*)(void*, DTORARG_TYPE))dtor)(objptr, 0))
-
-
-typedef void* ConstructorDestructor;
-
-
-extern void __construct_array(void* ptr, ConstructorDestructor ctor, ConstructorDestructor dtor, size_t size, size_t n);
-extern void __destroy_arr(void* block, ConstructorDestructor* dtor, size_t size, size_t n);
-extern void* __construct_new_array(void* block, ConstructorDestructor ctor, ConstructorDestructor dtor_arg, size_t size, size_t n);
-extern void __destroy_new_array(void* block, ConstructorDestructor dtor);
+extern void *__construct_new_array(void *block, ConstructorDestructor ctor, ConstructorDestructor dtor_arg, size_t size, size_t n);
+extern void __destroy_new_array(void *block, ConstructorDestructor dtor);
 extern void __destroy_new_array2();
 extern void __destroy_new_array3();
-
 
 #ifdef __cplusplus
 }
