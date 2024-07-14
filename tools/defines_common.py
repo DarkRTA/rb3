42e8d486d7cc565cbc52fa573fff5e888062508e
Nathan <nathanhurst03@gmail.com>
2024-07-14 15:20:52 -0600

    Allow more concisely referencing MSL_C/MSL_C++ headers specifically

--- tools/defines_common.py
+++ tools/defines_common.py
@@ -14,6 +14,9 @@ cflags_includes = [
     # Not included since it's all wrapped by stlport
     # "-i src/sdk/PowerPC_EABI_Support/MSL/MSL_C++",
 
+    # To allow referring directly to the MSL SDK instead of going through STLport
+    "-i src/sdk/PowerPC_EABI_Support/MSL",
+
     # SDK
     "-i src/sdk",
     "-i src/sdk/RVL_SDK",
