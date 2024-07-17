a12bdc37c78f8bccd77d047f387c3ec960d6a370
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-07-16 21:41:58 -0400

    i hate inlining

--- src/system/math/Vec.h
+++ src/system/math/Vec.h
@@ -219,13 +219,7 @@ inline float Distance(const Vector3& v1, const Vector3& v2){
 }
 
 inline void Subtract(const Vector3 &v1, const Vector3 &v2, Vector3 &dst) {
-#ifdef VERSION_SZBE69_B8
-    dst.z = v1.z - v2.z;
-    dst.y = v1.y - v2.y;
-    dst.x = v1.x - v2.x;
-#else
     dst.Set(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-#endif
 }
 
 float Length(const Vector3&);
@@ -251,6 +245,8 @@ inline float LengthSquared(const Vector3& v){
     return x * x + y * y + z * z;
 }
 
+// https://decomp.me/scratch/TWZ2T
+// why oh why does debug behave this way
 inline float DistanceSquared(const Vector3& v1, const Vector3& v2){
 #ifdef VERSION_SZBE69_B8
     float zdiff = v1.z - v2.z;
@@ -267,13 +263,7 @@ inline float DistanceSquared(const Vector3& v1, const Vector3& v2){
 float RecipSqrtAccurate(float);
 
 inline void Add(const Vector3& v1, const Vector3& v2, Vector3& dst){
-#ifdef VERSION_SZBE69_B8
-    dst.z = v1.z + v2.z;
-    dst.y = v1.y + v2.y;
-    dst.x = v1.x + v2.x;
-#else
     dst.Set(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-#endif
 }
 
 inline void Interp(const Vector2& v1, const Vector2& v2, float f, Vector2& res){
