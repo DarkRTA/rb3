2ec6a809db8a110ad801879716933a6321f9bfd9
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-07-22 14:59:26 -0400

    cross

--- src/system/math/Vec.h
+++ src/system/math/Vec.h
@@ -295,8 +295,19 @@ inline void ScaleAddEq(Vector3& v1, const Vector3& v2, float f){
 }
 
 inline void Cross(const Vector3 &v1, const Vector3 &v2, Vector3 &dst) {
+    float x1, x2, y2, z1, z2, y1;
+    
+    x2 = v2.x;
+    y1 = v1.y;
+    y2 = v2.y;
+    z1 = v1.z;
+    x1 = v1.x;
+    z2 = v2.z;
+
     dst.Set(
-        v2.y * v1.x - v1.y * v2.x, v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z
+        y1 * z2 - z1 * y2,
+        z1 * x2 - x1 * z2,
+        x1 * y2 - y1 * x2
     );
 }
 
