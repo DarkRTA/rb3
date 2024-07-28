a4708baa33145190f5ad46db305932a65d521339
Nathan <nathanhurst03@gmail.com>
2024-07-28 06:21:30 -0600

    compressed_pair fixes - Selector pattern needs to stay - Add default constructor

--- src/sdk/PowerPC_EABI_Support/MSL/MSL_C++/MSL_Common/detail/pair.h
+++ src/sdk/PowerPC_EABI_Support/MSL/MSL_C++/MSL_Common/detail/pair.h
@@ -7,9 +7,48 @@ namespace Metrowerks {
 
     namespace details {
 
-        template <typename T1, typename T2, bool T1Empty, bool T2Empty>
+        template <
+            typename T1,
+            typename T2,
+            bool T1Empty = std::is_empty<T1>::value,
+            bool T2Empty = std::is_empty<T2>::value>
+        struct compressed_pair_selector {
+            static const int value = 0;
+        };
+
+        template <typename T1, typename T2>
+        struct compressed_pair_selector<T1, T2, true, false> {
+            static const int value = 1;
+        };
+
+        template <typename T1, typename T2>
+        struct compressed_pair_selector<T1, T2, false, true> {
+            static const int value = 2;
+        };
+
+        template <typename T1, typename T2>
+        struct compressed_pair_selector<T1, T2, true, true> {
+            static const int value = 3;
+        };
+
+        template <typename T>
+        struct compressed_pair_selector<T, T, false, false> {
+            static const int value = 0;
+        };
+
+        template <typename T>
+        struct compressed_pair_selector<T, T, true, true> {
+            static const int value = 1;
+        };
+
+        // Note to future self: template signature must stay as-is for symbols
+        template <
+            typename T1,
+            typename T2,
+            int Selector = compressed_pair_selector<T1, T2>::value>
         class compressed_pair_imp {
         public:
+            compressed_pair_imp() {}
             compressed_pair_imp(const T1 &first, const T2 &second)
                 : m_First(first), m_Second(second) {}
             compressed_pair_imp(const T1 &first) : m_First(first) {}
@@ -26,9 +65,10 @@ namespace Metrowerks {
             T2 m_Second;
         };
 
-        template <typename T1, typename T2, bool T2Empty>
-        class compressed_pair_imp<T1, T2, true, T2Empty> : public T1 {
+        template <typename T1, typename T2>
+        class compressed_pair_imp<T1, T2, 1> : private T1 {
         public:
+            compressed_pair_imp() {}
             compressed_pair_imp(const T1 &first, const T2 &second)
                 : T1(first), m_Second(second) {}
             compressed_pair_imp(const T1 &first) : T1(first) {}
@@ -44,9 +84,10 @@ namespace Metrowerks {
             T2 m_Second;
         };
 
-        template <typename T1, typename T2, bool T1Empty>
-        class compressed_pair_imp<T1, T2, T1Empty, true> : public T2 {
+        template <typename T1, typename T2>
+        class compressed_pair_imp<T1, T2, 2> : private T2 {
         public:
+            compressed_pair_imp() {}
             compressed_pair_imp(const T1 &first, const T2 &second)
                 : m_First(first), T2(second) {}
             compressed_pair_imp(const T1 &first) : m_First(first) {}
@@ -64,17 +105,15 @@ namespace Metrowerks {
 
     }
 
-    // clang-format off: wastes space here
     template <typename T1, typename T2>
-    struct compressed_pair
-        : public details::compressed_pair_imp<T1, T2, std::is_empty<T1>::value, std::is_empty<T2>::value>
-    {
-        // clang-format on
-        compressed_pair(const T1 &first, const T2 &second)
-            : compressed_pair_imp(first, second) {}
+    struct compressed_pair : public details::compressed_pair_imp<T1, T2> {
+        compressed_pair() {}
         compressed_pair(const T1 &first) : compressed_pair_imp(first) {}
         compressed_pair(const T2 &second) : compressed_pair_imp(second) {}
+        compressed_pair(const T1 &first, const T2 &second)
+            : compressed_pair_imp(first, second) {}
     };
+
 }
 
 #endif
