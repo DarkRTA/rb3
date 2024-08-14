04e86f24c46c05372ecdf0a50a2c53df35e7d14b
Nathan <nathanhurst03@gmail.com>
2024-08-13 21:11:45 -0600

    Fix IDE-only errors in __mem.h

---
+++ src/compiler_macros.h
@@ -0,0 +1,16 @@
+#ifndef COMPILER_MACROS_H
+#define COMPILER_MACROS_H
+
+#ifndef __MWERKS__
+#define __option(x)
+#define __declspec(x)
+#define __attribute__(x)
+#endif
+
+#define ALIGN(x) __attribute__((aligned(x)))
+#define DONT_INLINE __attribute__((never_inline))
+
+#define DECL_SECTION(x) __declspec(section x)
+#define DECL_WEAK __declspec(weak)
+
+#endif
--- src/sdk/PowerPC_EABI_Support/Runtime/__mem.h
+++ src/sdk/PowerPC_EABI_Support/Runtime/__mem.h
@@ -4,13 +4,15 @@
 #include "MSL_C/MSL_Common/size_def.h"
 #include "MSL_C/MSL_Common/restrict_def.h"
 
+#include "compiler_macros.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-__declspec(section ".init") void* memcpy(void *RESTRICT dest, const void *RESTRICT src, size_t count);
-__declspec(section ".init") void __fill_mem(void* dest, int val, size_t count);
-__declspec(section ".init") void* memset(void *dest, int ch, size_t count);
+DECL_SECTION(".init") void* memcpy(void *RESTRICT dest, const void *RESTRICT src, size_t count);
+DECL_SECTION(".init") void __fill_mem(void* dest, int val, size_t count);
+DECL_SECTION(".init") void* memset(void *dest, int ch, size_t count);
 
 #ifdef __cplusplus
 }
