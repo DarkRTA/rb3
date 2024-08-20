adc795babc86e12659c272c8b106de54acefaca7
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-08-19 22:48:03 -0700

    charbonetwist symbols

--- src/system/char/CharBoneTwist.cpp
+++ src/system/char/CharBoneTwist.cpp
@@ -8,6 +8,30 @@ CharBoneTwist::CharBoneTwist() : mBone(this, 0), mTargets(this, kObjListNoNull) 
 }
 
 // fn_804B42D0 - poll
+void CharBoneTwist::Poll(){
+    if(!mBone || mTargets.size() == 0) return;
+    Vector3 v58;
+    v58.Zero();
+    for(ObjPtrList<RndTransformable, ObjectDir>::iterator it = mTargets.begin(); it != mTargets.end(); ++it){
+        Vector3 v64((*it)->WorldXfm().v);
+        Add(v64, v58, v58);
+    }
+    Scale(v58, 1.0f / mTargets.size(), v58);
+    Transform tf48(mBone->WorldXfm());
+    Vector3 v70;
+    Subtract(v58, mBone->WorldXfm().v, v70);
+    Vector3 v7c;
+    Scale(tf48.m.x, Dot(tf48.m.x, v70), v7c);
+    Subtract(v70, v7c, v7c);
+    Normalize(v7c, v7c);
+
+    Interp(tf48.m.y, v7c, Weight(), tf48.m.y);
+    Normalize(tf48.m.y, tf48.m.y);
+    Cross(tf48.m.x, tf48.m.y, tf48.m.z);
+    Normalize(tf48.m.z, tf48.m.z);
+    Scale(tf48.m.z, Length(tf48.m.x), tf48.m.z);
+    mBone->SetWorldXfm(tf48);
+}
 
 void CharBoneTwist::PollDeps(std::list<Hmx::Object*>& changedBy, std::list<Hmx::Object*>& change){
     change.push_back(mBone);
