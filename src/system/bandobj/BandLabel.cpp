c99cf6ace90063a02db777f9dacd981d7a05ef5b
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-08-23 03:18:43 -0700

    match the 2nd largest function in the RB3 codebase nbd (#321)
    * bandlabel header
    
    * chonk

---
+++ src/system/bandobj/BandLabel.cpp
@@ -0,0 +1,11 @@
+#include "bandobj/BandLabel.h"
+#include "ui/UI.h"
+
+void BandLabel::Init(){
+    TheUI->InitResources("BandLabel");
+    Register();
+}
+
+BandLabel::BandLabel() : UITransitionHandler(this), unk1e8(""), unk1f4(0) {
+
+}
