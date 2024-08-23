f48b494595378711702eab373f19efe8283ff2f9
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-08-22 22:25:33 -0700

    bandlabel header

--- src/system/bandobj/BandLabel.h
+++ src/system/bandobj/BandLabel.h
@@ -1,14 +1,36 @@
 #ifndef BANDOBJ_BANDLABEL_H
 #define BANDOBJ_BANDLABEL_H
-
-#include "obj/ObjMacros.h"
+#include "ui/UITransitionHandler.h"
 #include "ui/UILabel.h"
 
-class BandLabel : public UILabel {
-    public:
+class BandLabel : public UILabel, public UITransitionHandler {
+public:
     BandLabel();
-    OBJ_CLASSNAME(BandLabel)
-    OBJ_SET_TYPE(BandLabel)
+    OBJ_CLASSNAME(BandLabel);
+    OBJ_SET_TYPE(BandLabel);
+    virtual DataNode Handle(DataArray*, bool);
+    virtual bool SyncProperty(DataNode&, DataArray*, int, PropOp);
+    virtual void Save(BinStream&);
+    virtual void Copy(const Hmx::Object*, Hmx::Object::CopyType);
+    virtual void Load(BinStream&);
+    virtual ~BandLabel();
+    virtual void Poll();
+    virtual void SetDisplayText(const char*, bool);
+    virtual void Count(int, int, float, Symbol);
+    virtual void FinishCount();
+    virtual bool IsEmptyValue() const;
+    virtual void FinishValueChange();
+
+    static void Init();
+    static void Register(){
+        REGISTER_OBJ_FACTORY(BandLabel);
+    }
+    NEW_OBJ(BandLabel);
+
+    std::vector<int> unk1dc; // 0x1dc
+    Symbol unk1e4; // 0x1e4
+    String unk1e8; // 0x1e8
+    bool unk1f4; // 0x1f4
 };
 
 #endif // BANDOBJ_BANDLABEL_H
