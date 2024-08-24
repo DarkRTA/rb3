84306fb284338d83c6e868dde691f01fe064860a
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-08-24 00:06:06 -0700

    charcollide work

--- src/system/char/CharCollide.cpp
+++ src/system/char/CharCollide.cpp
@@ -1,6 +1,9 @@
 #include "char/CharCollide.h"
+#include "utl/Symbols.h"
 
-CharCollide::CharCollide() : mShape(kSphere), mFlags(0), mMesh(this, 0), unk188(0) {
+INIT_REVS(CharCollide)
+
+CharCollide::CharCollide() : mShape(kSphere), mFlags(0), mMesh(this, 0), mMeshYBias(0) {
     for(int i = 0; i < 2; i++){
         mLength[i] = 0;
         mRadius[i] = 0;
@@ -18,3 +21,85 @@ CharCollide::~CharCollide(){
 }
 
 SAVE_OBJ(CharCollide, 0x58)
+
+BEGIN_LOADS(CharCollide)
+    LOAD_REVS(bs)
+    ASSERT_REVS(7, 0)
+    LOAD_SUPERCLASS(Hmx::Object)
+    LOAD_SUPERCLASS(RndTransformable)
+    bs >> (int&)mShape;
+    bs >> mRadius[0];
+    if(gRev > 4) bs >> mLength[0];
+    if(gRev > 2) bs >> mLength[1];
+    if(gRev > 1) bs >> mFlags;
+    else mFlags = 0;
+    if(gRev > 3) bs >> unk_arr[0];
+    else unk_arr[0] = mRadius[0];
+
+    if(gRev > 5){
+        bs >> mRadius[1];
+        bs >> unk_arr[1];
+        bs >> unk_arr2[0];
+        bs >> unk_arr2[1];
+        bs >> unk148;
+        bs >> mMesh;
+        for(int i = 0; i < 8; i++){
+            bs >> unk_structs[i].unk0;
+            bs >> unk_structs[i].vec;
+        }
+        bs >> mDigest;
+        bs >> mMeshYBias;
+        if(gRev < 7) CopyOriginalToCur();
+    }
+    else {
+        mRadius[1] = mRadius[0];
+        CopyOriginalToCur();
+    }
+END_LOADS
+
+BEGIN_COPYS(CharCollide)
+    COPY_SUPERCLASS(Hmx::Object)
+    COPY_SUPERCLASS(RndTransformable)
+    CREATE_COPY(CharCollide)
+    BEGIN_COPYING_MEMBERS
+        COPY_MEMBER(mShape)
+        COPY_MEMBER(mFlags)
+        memcpy(mRadius, c->mRadius, 8);
+        memcpy(mLength, c->mLength, 8);
+        memcpy(unk_arr, c->unk_arr, 8);
+        memcpy(unk_arr2, c->unk_arr2, 8);
+        COPY_MEMBER(unk148)
+        COPY_MEMBER(mMeshYBias)
+        COPY_MEMBER(mMesh)
+    END_COPYING_MEMBERS
+END_COPYS
+
+void CharCollide::CopyOriginalToCur(){
+    memcpy(unk_arr, mRadius, 8);
+    memcpy(unk_arr2, mLength, 8);
+}
+
+void CharCollide::SyncShape(){
+    if(unk_arr2[1] < unk_arr2[0]){
+        unk_arr2[0] = unk_arr2[1];
+    }
+    CopyOriginalToCur();
+}
+
+BEGIN_HANDLERS(CharCollide)
+    HANDLE_SUPERCLASS(RndTransformable)
+    HANDLE_SUPERCLASS(Hmx::Object)
+    HANDLE_CHECK(0x221)
+END_HANDLERS
+
+BEGIN_PROPSYNCS(CharCollide)
+    SYNC_PROP_MODIFY(shape, (int&)mShape, SyncShape())
+    SYNC_PROP(flags, mFlags)
+    SYNC_PROP_MODIFY(radius0, mRadius[0], SyncShape())
+    SYNC_PROP_MODIFY(radius1, mRadius[1], SyncShape())
+    SYNC_PROP_MODIFY(length0, mLength[0], SyncShape())
+    SYNC_PROP_MODIFY(length1, mLength[1], SyncShape())
+    SYNC_PROP_MODIFY_ALT(mesh, mMesh, SyncShape())
+    SYNC_PROP_MODIFY(mesh_y_bias, mMeshYBias, SyncShape())
+    SYNC_SUPERCLASS(RndTransformable)
+END_PROPSYNCS
--- src/system/char/CharCollide.h
+++ src/system/char/CharCollide.h
@@ -2,6 +2,7 @@
 #define CHAR_CHARCOLLIDE_H
 #include "rndobj/Trans.h"
 #include "rndobj/Mesh.h"
+#include "math/SHA1.h"
 #include "obj/ObjPtr_p.h"
 
 class CharCollide : public RndTransformable {
@@ -35,14 +36,15 @@ public:
     float Radius() const;
     Shape GetShape() const { return mShape; }
     const Vector3& Axis() const;
+    void SyncShape();
 
     float GetRadius(const Vector3& v1, Vector3& vout) const {
         Subtract(v1, unk1a0, vout);
-        float ret = unk178;
+        float ret = unk_arr[0];
         if(mShape >= kCigar){
-            float clamped = Clamp(unk180, unk184, unk190 * Dot(vout, unk194));
+            float clamped = Clamp(unk_arr2[0], unk_arr2[1], unk190 * Dot(vout, unk194));
             ScaleAddEq(vout, unk194, -clamped);
-            Interp(ret, unk17c, unk18c * (clamped - unk180), ret);
+            Interp(ret, unk_arr[1], unk18c * (clamped - unk_arr2[0]), ret);
         }
         else if(mShape == kPlane){
             ret = Dot(vout, unk194);
@@ -51,17 +53,21 @@ public:
         return ret;
     }
 
+    DECLARE_REVS;
+    NEW_OVERLOAD;
+    DELETE_OVERLOAD;
+
     Shape mShape; // 0x90
     int mFlags; // 0x94
     ObjPtr<RndMesh, ObjectDir> mMesh; // 0x98
-    int unka4, unka8, unkac, unkb0, unkb4;
+    CSHA1::Digest mDigest; // 0xa4
     CharCollideStruct unk_structs[8]; // 0xb8 - 0x134, inclusive
     float mRadius[2]; // 0x138 - radius0 at 0x138, radius1 at 0x13c
     float mLength[2]; // 0x140 - length0 at 0x140, length1 at 0x144
     Transform unk148; // 0x148
-    float unk178;
-    float unk17c, unk180, unk184;
-    bool unk188; // 0x188
+    float unk_arr[2]; // 0x178
+    float unk_arr2[2]; // 0x180
+    bool mMeshYBias; // 0x188
     float unk18c;
     float unk190;
     Vector3 unk194;
