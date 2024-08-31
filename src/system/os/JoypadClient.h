397eecd11ac6ebcf3be9d91be7b1f6e167bd88e0
Andrew Betson <AndrewDCBetson@gmail.com>
2024-08-31 08:09:14 -0400

    System, UI, and STLPort work - oh my! (#328)
    * Mark std::find as inline
    
    * JoypadClient work
    
    * UIListProvider work

--- src/system/os/JoypadClient.h
+++ src/system/os/JoypadClient.h
@@ -1,6 +1,7 @@
 #ifndef OS_JOYPADCLIENT_H
 #define OS_JOYPADCLIENT_H
 #include "os/Joypad.h"
+#include "os/JoypadMsgs.h"
 #include "os/Timer.h"
 #include "obj/Object.h"
 
@@ -15,19 +16,34 @@ public:
     JoypadRepeat();
     void Start(JoypadButton, JoypadAction, int);
     void Reset(JoypadButton);
+    void Poll(float, float, Hmx::Object *, int);
 };
 
 class JoypadClient : public Hmx::Object {
 public:
-    int mUser;
-    Hmx::Object* mSink;
-    int mBtnMask;
-    float mHoldMs;
-    float mRepeatMs;
-    JoypadRepeat mRepeats[8];
-    bool mVirtualDpad;
+    JoypadClient(Hmx::Object *sink);
+    ~JoypadClient();
 
+    LocalUser *mUser; // 0x1C
+    Hmx::Object* mSink; // 0x20
+    int mBtnMask; // 0x24
+    float mHoldMs; // 0x28
+    float mRepeatMs; // 0x2C
+    JoypadRepeat mRepeats[4]; // 0x30
+    bool mVirtualDpad; // 0x1F0
+    bool mFilterAllButStart; // 0x1F1
+
+    virtual DataNode Handle(DataArray *, bool);
+
+    void Init();
+    void SetFilterAllButStart(bool);
+    void SetRepeatMask(int);
     void SetVirtualDpad(bool);
+    void Poll();
+    DataNode OnMsg(const ButtonDownMsg &);
+    DataNode OnMsg(const ButtonUpMsg &);
 };
+
+void JoypadClientPoll();
 
 #endif
