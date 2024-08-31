2ca2179ef0aaa829fbfc72b0c0eb8ac6ead48811
AndrewBetson <AndrewDCBetson@gmail.com>
2024-08-31 07:49:33 -0400

    JoypadClient work

--- src/system/os/JoypadClient.cpp
+++ src/system/os/JoypadClient.cpp
@@ -1,5 +1,14 @@
 #include "os/JoypadClient.h"
+#include "Joypad.h"
+#include "JoypadClient.h"
+#include "JoypadMsgs.h"
 #include "os/Debug.h"
+#include <algorithm>
+
+bool gInited = false;
+float gDefaultHoldMs = -1.0f;
+float gDefaultRepeatMs = -1.0f;
+std::vector<JoypadClient *> gClients;
 
 namespace {
     JoypadButton LeftStickToDpad(JoypadButton btn){
@@ -33,5 +42,78 @@ void JoypadRepeat::Reset(JoypadButton btn){
     if(mLastBtn == btn){
         mHoldTimer.Reset();
         mRepeatTimer.Reset();
+    }
+}
+
+JoypadClient::JoypadClient(Hmx::Object *sink)
+    : mUser(NULL), mSink(sink), mBtnMask(0), mHoldMs(-1.0f), mRepeatMs(-1.0f), mVirtualDpad(false), mFilterAllButStart(false) {
+    MILO_ASSERT(sink, 148);
+    Init();
+}
+
+JoypadClient::~JoypadClient() {
+    JoypadUnsubscribe(this);
+    gClients.erase(std::find(gClients.begin(), gClients.end(), this));
+}
+
+void JoypadClient::Init() {
+    if (!gInited) {
+        gClients.clear();
+
+        gInited = true;
+    }
+
+    JoypadSubscribe(this);
+
+    if (gDefaultHoldMs < 0.0f) {
+        SystemConfig("joypad")->FindData("hold_ms", gDefaultHoldMs, true);
+    }
+
+    if (gDefaultRepeatMs < 0.0f) {
+        SystemConfig("joypad")->FindData("repeat_ms", gDefaultRepeatMs, true);
+    }
+
+    mHoldMs = gDefaultHoldMs;
+    mRepeatMs = gDefaultRepeatMs;
+
+    gClients.push_back(this);
+}
+
+void JoypadClient::SetFilterAllButStart(bool ShouldWe) {
+    if (mFilterAllButStart != ShouldWe) {
+        if (ShouldWe) {
+            for (int i = 0; i < 4; i++) {
+                JoypadRepeat *Rep = &mRepeats[i];
+                Rep->Reset(Rep->mLastBtn);
+            }
+        }
+        mFilterAllButStart = ShouldWe;
+    }
+}
+
+void JoypadClient::SetRepeatMask(int NewBtnMask) { mBtnMask = NewBtnMask; }
+void JoypadClient::SetVirtualDpad(bool NewVirtualDpad) {
+    mVirtualDpad = NewVirtualDpad;
+}
+
+void JoypadClient::Poll() {
+    for (int i = 0; i < 4; i++) {
+        mRepeats[i].Poll(mHoldMs, mRepeatMs, mSink, i);
+    }
+}
+
+DataNode JoypadClient::OnMsg(const ButtonUpMsg &Msg) {}
+DataNode JoypadClient::OnMsg(const ButtonDownMsg &Msg) {}
+
+BEGIN_HANDLERS(JoypadClient)
+    HANDLE_MESSAGE(ButtonDownMsg)
+    HANDLE_MESSAGE(ButtonUpMsg)
+    HANDLE_SUPERCLASS(Hmx::Object)
+    HANDLE_CHECK(0x106)
+END_HANDLERS
+
+void JoypadClientPoll() {
+    for (int i = 0; i < gClients.size(); i++) {
+        gClients[i]->Poll();
     }
 }
