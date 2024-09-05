810adfa15ac00c82bd4f39aff97cb80d14f3d8d4
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-05 13:52:23 -0700

    expand bandtrack class

--- src/system/bandobj/BandTrack.cpp
+++ src/system/bandobj/BandTrack.cpp
@@ -1,5 +1,21 @@
 #include "bandobj/BandTrack.h"
 
-BandTrack::BandTrack(Hmx::Object* o){
-    
+BandTrack::BandTrack(Hmx::Object* o) : mDisabled(0), mSimulatedNet(0), mInstrument(""), unk14(4), unk18(0), unk19(0), unk1a(0), unk1b(0), unk1c(0), mInUse(0), unk1e(0), unk1f(0),
+    mPlayerIntro(o, 0), mStarPowerMeter(o, 0), mStreakMeter(o, 0), mPopupObject(o, 0), mPlayerFeedback(o, 0), mFailedFeedback(o, 0), unk68(o, 0), unk74(""), unk78(0),
+    mEndgameFeedback(o, 0), unk88(0), unk8c(0), mParent(o, 0), mRetractTrig(o, 0), mResetTrig(o, 0), mDeployTrig(o, 0), mStopDeployTrig(o, 0), mIntroTrig(o, 0), unkd8(o, 0),
+    unke4(o, 0), unkf0(o, 0), unkfc(this, 0), unk108(1), unk109(1), unk10c(-1) {
+
+}
+
+BEGIN_COPYS(BandTrack)
+END_COPYS
+
+BEGIN_LOADS(BandTrack)
+END_LOADS
+
+void BandTrack::Save(BinStream&){}
+
+void BandTrack::Init(Hmx::Object* o){
+    if(o) mParent = dynamic_cast<TrackInterface*>(o);
+    Reset();
 }
--- src/system/bandobj/BandTrack.h
+++ src/system/bandobj/BandTrack.h
@@ -2,6 +2,11 @@
 #include "obj/Object.h"
 #include "bandobj/CrowdMeterIcon.h"
 #include "bandobj/TrackPanelDirBase.h"
+#include "rndobj/Dir.h"
+#include "bandobj/OverdriveMeter.h"
+#include "bandobj/StreakMeter.h"
+#include "bandobj/UnisonIcon.h"
+#include "bandobj/TrackInterface.h"
 
 class BandTrack : public virtual Hmx::Object {
 public:
@@ -50,17 +55,45 @@ public:
     virtual void TutorialReset();
     virtual ~BandTrack();
 
-    bool unk8;
-    bool unk9;
-    Symbol unkc;
-    int unk10;
-    int unk14;
-    bool unk18;
-    bool unk19;
-    bool unk1a;
-    bool unk1b;
-    bool unk1c;
-    bool unk1d;
-    bool unk1e;
-    bool unk1f;
+    void Init(Hmx::Object*);
+
+    bool mDisabled; // 0x8
+    bool mSimulatedNet; // 0x9
+    Symbol mInstrument; // 0xc
+    int unk10; // 0x10
+    int unk14; // 0x14
+    bool unk18; // 0x18
+    bool unk19; // 0x19
+    bool unk1a; // 0x1a
+    bool unk1b; // 0x1b
+    bool unk1c; // 0x1c
+    bool mInUse; // 0x1d
+    bool unk1e; // 0x1e
+    bool unk1f; // 0x1f
+    ObjPtr<RndDir, ObjectDir> mPlayerIntro; // 0x20
+    ObjPtr<OverdriveMeter, ObjectDir> mStarPowerMeter; // 0x2c
+    ObjPtr<StreakMeter, ObjectDir> mStreakMeter; // 0x38
+    ObjPtr<RndDir, ObjectDir> mPopupObject; // 0x44
+    ObjPtr<RndDir, ObjectDir> mPlayerFeedback; // 0x50
+    ObjPtr<RndDir, ObjectDir> mFailedFeedback; // 0x5c
+    ObjPtr<UnisonIcon, ObjectDir> unk68; // 0x68
+    Symbol unk74; // 0x74
+    bool unk78; // 0x78
+    ObjPtr<RndDir, ObjectDir> mEndgameFeedback; // 0x7c
+    int unk88; // 0x88
+    bool unk8c; // 0x8c
+    ObjPtr<TrackInterface, ObjectDir> mParent; // 0x90
+    ObjPtr<EventTrigger, ObjectDir> mRetractTrig; // 0x9c
+    ObjPtr<EventTrigger, ObjectDir> mResetTrig; // 0xa8
+    ObjPtr<EventTrigger, ObjectDir> mDeployTrig; // 0xb4
+    ObjPtr<EventTrigger, ObjectDir> mStopDeployTrig; // 0xc0
+    ObjPtr<EventTrigger, ObjectDir> mIntroTrig; // 0xcc
+    ObjPtr<Task, ObjectDir> unkd8; // 0xd8
+    ObjPtr<Task, ObjectDir> unke4; // 0xe4
+    ObjPtr<Task, ObjectDir> unkf0; // 0xf0
+    ObjPtr<RndGroup, ObjectDir> unkfc; // 0xfc
+    bool unk108; // 0x108
+    bool unk109; // 0x109
+    int unk10c; // 0x10c
+    Symbol unk110; // 0x110
 };
