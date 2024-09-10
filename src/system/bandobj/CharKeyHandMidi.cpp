4824650a7d0388924d2dd1d7ab641f3c25283a0d
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-09 20:16:35 -0700

    fix charkeyhandmidi derivatives

---
+++ src/system/bandobj/CharKeyHandMidi.cpp
@@ -0,0 +1,9 @@
+#include "bandobj/CharKeyHandMidi.h"
+
+CharKeyHandMidi::CharKeyHandMidi() : unk28(this, 0), unk34(this, 0), unk40(this, 0), unk7c(this, 0) {
+
+}
+
+CharKeyHandMidi::~CharKeyHandMidi(){
+    
+}
--- src/system/bandobj/CharKeyHandMidi.h
+++ src/system/bandobj/CharKeyHandMidi.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "char/CharWeightable.h"
+#include "char/CharPollable.h"
 #include "rndobj/Highlightable.h"
 #include "char/CharIKFingers.h"
 
-class CharKeyHandMidi : public virtual RndHighlightable, public CharWeightable {
+class CharKeyHandMidi : public RndHighlightable, public CharWeightable, public CharPollable {
 public:
     CharKeyHandMidi();
     virtual ~CharKeyHandMidi();
@@ -20,5 +21,21 @@ public:
     virtual void Enter();
     virtual void SetName(const char*, ObjectDir*);
 
+    NEW_OVERLOAD;
+    DELETE_OVERLOAD;
+
     ObjPtr<CharIKFingers, ObjectDir> unk28; // 0x28
+    ObjPtr<RndTransformable, ObjectDir> unk34; // 0x34
+    ObjPtr<RndTransformable, ObjectDir> unk40; // 0x40
+    std::vector<Vector3> unk4c;
+    std::vector<Vector3> unk54;
+    std::vector<int> unk5c;
+    int unk64;
+    int unk68;
+    std::vector<int> unk6c;
+    int unk74;
+    bool unk78;
+    ObjPtr<Character, ObjectDir> unk7c;
+    float unk88;
+    bool unk8c;
 };
