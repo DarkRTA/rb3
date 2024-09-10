af01e29fd1a15f7751676f8ac7603a75866ace68
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-10 16:11:58 -0700

    add bandconfiguration

---
+++ src/system/bandobj/BandConfiguration.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "obj/Object.h"
+#include "math/Mtx.h"
+#include "char/Waypoint.h"
+
+class BandConfiguration : public Hmx::Object {
+public:
+    // size 0x34
+    class TargTransform {
+    public:
+        Symbol targName; // 0x0
+        Transform xfm; // 0x4
+    };
+
+    // size 0xa0
+    class TargTransforms {
+    public:
+        TargTransform xfms[3];
+        Waypoint* mWay;
+    };
+
+    BandConfiguration();
+    virtual ~BandConfiguration();
+    OBJ_CLASSNAME(BandConfiguration);
+    OBJ_SET_TYPE(BandConfiguration);
+    virtual DataNode Handle(DataArray*, bool);
+    virtual bool SyncProperty(DataNode&, DataArray*, int, PropOp);
+    virtual void Save(BinStream&);
+    virtual void Copy(const Hmx::Object*, Hmx::Object::CopyType);
+    virtual void Load(BinStream&);
+
+    DECLARE_REVS;
+    NEW_OVERLOAD;
+    DELETE_OVERLOAD;
+
+    TargTransforms mXfms[4]; // 0x1c, 0xbc, 0x15c, 0x1fc
+    // 0x29c
+};
