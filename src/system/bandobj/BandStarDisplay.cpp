b14750e9bb2f71fe1d1ba2d2bf57e9f61590f31f
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-10 18:46:12 -0700

    surprise it's more bandobjs

---
+++ src/system/bandobj/BandStarDisplay.cpp
@@ -0,0 +1,9 @@
+#include "bandobj/BandStarDisplay.h"
+
+BandStarDisplay::BandStarDisplay() : unk18c(0), unk190(this), unk19c(this), unk1a8(this), unk1b4(this), unk1c0(this, 0), unk1cc(this, 0), unk1d8(this, 0), unk1e4("normal") {
+
+}
+
+BandStarDisplay::~BandStarDisplay(){
+    
+}
