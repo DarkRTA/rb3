3090327d4730eec9ae264434acfb6151dba4bbbb
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-12 13:11:17 -0700

    bands ctx

--- src/system/bandobj/BandSwatch.cpp
+++ src/system/bandobj/BandSwatch.cpp
@@ -1,9 +1,12 @@
 #include "bandobj/BandSwatch.h"
 #include "math/Rand.h"
+#include "ui/UI.h"
+#include "utl/Symbols.h"
 
 ColorPalette* BandSwatch::sDummyPalette;
 
 void BandSwatch::Init(){
+    TheUI->InitResources("BandSwatch");
     Register();
     sDummyPalette = Hmx::Object::New<ColorPalette>();
     if(TheLoadMgr.EditMode()){
@@ -17,10 +20,21 @@ void BandSwatch::Terminate(){
     RELEASE(sDummyPalette);
 }
 
-BandSwatch::BandSwatch() : unk1f0(this, 0) {
+BandSwatch::BandSwatch() : mColorPalette(this, 0) {
     MILO_ASSERT(sDummyPalette, 0x30);
 }
 
 BandSwatch::~BandSwatch(){
     
 }
+
+BEGIN_HANDLERS(BandSwatch)
+    HANDLE_ACTION(set_colors, SetColors(_msg->Obj<ColorPalette>(2)))
+    HANDLE_SUPERCLASS(UIList)
+    HANDLE_CHECK(0x94)
+END_HANDLERS
+
+BEGIN_PROPSYNCS(BandSwatch)
+    SYNC_PROP_MODIFY_ALT(color_palette, mColorPalette, UpdateColors())
+    SYNC_SUPERCLASS(UIList)
+END_PROPSYNCS
