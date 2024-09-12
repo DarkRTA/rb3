3136b65c97411d291475fe649e2009730cfd86c8
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-12 09:49:12 -0700

    more handlers/propsyncs/ctx

--- src/system/bandobj/BandFaceDeform.h
+++ src/system/bandobj/BandFaceDeform.h
@@ -1,12 +1,18 @@
 #pragma once
 #include "obj/Object.h"
+#include "rndobj/MeshAnim.h"
 
 class BandFaceDeform : public Hmx::Object {
 public:
     class DeltaArray {
     public:
         DeltaArray();
+        DeltaArray(const DeltaArray&);
         ~DeltaArray();
+        int NumVerts();
+
+        DeltaArray& operator=(const DeltaArray&);
+
         int unk0;
         int unk4;
     };
@@ -21,9 +27,12 @@ public:
     virtual void Copy(const Hmx::Object*, Hmx::Object::CopyType);
     virtual void Load(BinStream&);
 
+    int TotalSize();
+    void SetFromMeshAnim(RndMeshAnim*, RndMeshAnim*, int, int);
+
     DECLARE_REVS;
     NEW_OVERLOAD;
     DELETE_OVERLOAD;
 
-    std::vector<DeltaArray> unk1c; // 0x1c
+    std::vector<DeltaArray> mFrames; // 0x1c
 };
--- src/system/bandobj/BandHighlight.cpp
+++ src/system/bandobj/BandHighlight.cpp
@@ -1,4 +1,5 @@
 #include "bandobj/BandHighlight.h"
+#include "utl/Symbols.h"
 
 BandHighlight::BandHighlight() : unk148(0), unk154(0), unk158(0) {
 
@@ -7,3 +8,15 @@ BandHighlight::BandHighlight() : unk148(0), unk154(0), unk158(0) {
 BandHighlight::~BandHighlight(){
     
 }
+
+BEGIN_HANDLERS(BandHighlight)
+    HANDLE_MESSAGE(UIComponentFocusChangeMsg)
+    HANDLE_ACTION(register_focus, OnRegisterFocus())
+    HANDLE_ACTION(unregister_focus, OnUnregisterFocus())
+    HANDLE_SUPERCLASS(UIComponent)
+    HANDLE_CHECK(0x12B)
+END_HANDLERS
+
+BEGIN_PROPSYNCS(BandHighlight)
+    SYNC_SUPERCLASS(UIComponent)
+END_PROPSYNCS
