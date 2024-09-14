3d695bfd653253935700ae6c67763f1cae31d7c0
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-13 17:33:30 -0700

    vocaltrackdir propsync

--- src/system/bandobj/VocalTrackDir.cpp
+++ src/system/bandobj/VocalTrackDir.cpp
@@ -1,16 +1,108 @@
 #include "bandobj/VocalTrackDir.h"
+#include "utl/Symbols.h"
 
-VocalTrackDir::VocalTrackDir() : BandTrack(this), unk2a0(0.3f), unk2a4(1), unk2a5(1), unk2a6(1), unk2a7(0), unk2a8(0), unk2ac(0), unk2b0(0), unk2e4(this, 0), unk2f0(this, 0),
-    unk2fc(12.0f), unk300(3.0f), unk304(0.85f), unk308(this, kObjListNoNull), unk318(this, 0), unk324(this, 0), unk330(this, 0), unk33c(this, 0), unk348(this, 0), unk354(this, 0),
-    unk360(this, 0), unk36c(this, 0), unk378(this, 0), unk384(this, 0), unk390(this, 0), unk39c(1), unk3a0(2.5f), unk3a4(this, 0), unk3b0(this, 0), unk3bc(1), unk3c0(0.4f),
-    unk3c4(this, 0), unk3d0(1), unk3d4(0.4f), unk3d8(this, 0), unk3e4(this, 0), unk3f0(this, 0), unk3fc(0.2f), unk400(this, 0), unk40c(0), unk410(-20.0f), unk414(20.0f),
-    unk418(0.0f), unk41c(10.0f), unk420(0.0f), unk424(2.0f), unk428(-10.0f), unk42c(0), unk430("harmonic"), unk434(this, 0), unk440(this, 0), unk44c(this, 0), unk458(this, 0),
-    unk464(this, 0), unk470(this, 0), unk47c(this, 0), unk488(36.0f), unk48c(84.0f), unk490(0), unk494(0x3c), unk498(this, 0), unk4a4(this, 0), unk4b0(1), unk4b4(2), unk4b8(0),
+VocalTrackDir::VocalTrackDir() : BandTrack(this), mHiddenPartAlpha(0.3f), unk2a4(1), unk2a5(1), mIsTop(1), unk2a7(0), mFeedbackStateLead(0), mFeedbackStateHarm1(0), mFeedbackStateHarm2(0), unk2e4(this, 0), unk2f0(this, 0),
+    mMinPitchRange(12.0f), mPitchDisplayMargin(3.0f), mArrowSmoothing(0.85f), mConfigurableObjects(this, kObjListNoNull), mVoxCfg(this, 0), unk324(this, 0), unk330(this, 0), unk33c(this, 0), unk348(this, 0), mPhraseFeedbackTrig(this, 0),
+    mSpotlightSparklesOnlyTrig(this, 0), mSpotlightPhraseSuccessTrig(this, 0), unk378(this, 0), unk384(this, 0), unk390(this, 0), mPitchWindow(1), mPitchWindowHeight(2.5f), mPitchWindowMesh(this, 0), mPitchWindowOverlay(this, 0), mLeadLyrics(1), mLeadLyricHeight(0.4f),
+    mLeadLyricMesh(this, 0), mHarmLyrics(1), mHarmLyricHeight(0.4f), mHarmLyricMesh(this, 0), mLeftDecoMesh(this, 0), mRightDecoMesh(this, 0), mNowBarWidth(0.2f), mNowBarMesh(this, 0), mRemote(0), mTrackLeft(-20.0f), mTrackRight(20.0f),
+    mTrackBottom(0.0f), mTrackTop(10.0f), mPitchBottom(0.0f), mPitchTop(2.0f), mNowBar(-10.0f), unk42c(0), mPitchGuides("harmonic"), mTubeStyle(this, 0), mArrowStyle(this, 0), mFontStyle(this, 0), unk458(this, 0),
+    unk464(this, 0), unk470(this, 0), unk47c(this, 0), mLastMin(36.0f), mLastMax(84.0f), mMiddleCZPos(0), mTonic(0x3c), mRangeScaleAnim(this, 0), mRangeOffsetAnim(this, 0), unk4b0(1), unk4b4(2), unk4b8(0),
     unk4bc(0), unk4c0(0), unk4c4(0), unk4c8(0), unk4cc(0), unk4d0(0), unk4d4(0), unk4d8(this, 0), unk4e4(this, 0), unk4f0(this, 0), unk4fc(this, 0), unk508(this, 0), unk514(this, 0),
     unk520(this, 0), unk52c(this, 0), unk538(this, 0), unk544(this, 0), unk550(this, 0), unk55c(this, 0), unk568(this, 0), unk574(this, 0), unk580(this, 0), unk58c(this, 0),
     unk598(this, 0), unk5a4(this, 0), unk5b0(this, 0), unk5bc(this, 0), unk5c8(this, 0), unk5d4(this, 0), unk5e0(this, 0), unk5ec(this, 0), unk5f8(this, 0), unk604(this, 0),
     unk610(this, 0), unk61c(this, 0), unk628(this, 0), unk634(this, 0), unk640(this, 0), unk64c(this, 0), unk658(this, 0), unk664(this, 0), unk670(this, 0), unk67c(this, 0),
-    unk688(this, 0), unk694(0), unk698(0), unk69c(0), unk6a0(0), unk6a4(this, 0), unk6b0(this, 0), unk6bc(-1.0f), unk6c0(0.3f), unk6c4(-1), unk6c8(0), unk6cc(this, 0),
+    unk688(this, 0), unk694(0), unk698(0), unk69c(0), unk6a0(0), mLeadDeployMat(this, 0), mHarmDeployMat(this, 0), unk6bc(-1.0f), unk6c0(0.3f), unk6c4(-1), unk6c8(0), unk6cc(this, 0),
     unk6d8(18.0f), unk6dc(48.0f), unk6e0(0) {
 
 }
+
+#pragma push
+#pragma dont_inline on
+BEGIN_PROPSYNCS(VocalTrackDir)
+    SYNC_PROP(is_top, mIsTop)
+    SYNC_PROP(feedback_state_lead, mFeedbackStateLead)
+    SYNC_PROP(feedback_state_harm_1, mFeedbackStateHarm1)
+    SYNC_PROP(feedback_state_harm_2, mFeedbackStateHarm2)
+    SYNC_PROP(lead_coming, unk2b4[5])
+    SYNC_PROP(lead_now, unk2b4[6])
+    SYNC_PROP(lead_past, unk2b4[7])
+    SYNC_PROP(lead_od_coming, unk2b4[0x15])
+    SYNC_PROP(lead_od_now, unk2b4[0x16])
+    SYNC_PROP(lead_od_past, unk2b4[0x17])
+    SYNC_PROP(harmony_coming, unk2b4[1])
+    SYNC_PROP(harmony_now, unk2b4[2])
+    SYNC_PROP(harmony_past, unk2b4[3])
+    SYNC_PROP(harmony_od_coming, unk2b4[0x11])
+    SYNC_PROP(harmony_od_now, unk2b4[0x12])
+    SYNC_PROP(harmony_od_past, unk2b4[0x13])
+    SYNC_PROP(lead_static_preview, unk2b4[0xC])
+    SYNC_PROP(lead_static_coming, unk2b4[0xD])
+    SYNC_PROP(lead_static_now, unk2b4[0xE])
+    SYNC_PROP(lead_static_past, unk2b4[0xF])
+    SYNC_PROP(lead_static_od_preview, unk2b4[0x1C])
+    SYNC_PROP(lead_static_od_coming, unk2b4[0x1D])
+    SYNC_PROP(lead_static_od_now, unk2b4[0x1E])
+    SYNC_PROP(lead_static_od_past, unk2b4[0x1F])
+    SYNC_PROP(harmony_static_preview, unk2b4[8])
+    SYNC_PROP(harmony_static_coming, unk2b4[9])
+    SYNC_PROP(harmony_static_now, unk2b4[0xA])
+    SYNC_PROP(harmony_static_past, unk2b4[0xB])
+    SYNC_PROP(harmony_static_od_preview, unk2b4[0x18])
+    SYNC_PROP(harmony_static_od_coming, unk2b4[0x19])
+    SYNC_PROP(harmony_static_od_now, unk2b4[0x1A])
+    SYNC_PROP(harmony_static_od_past, unk2b4[0x1B])
+    SYNC_PROP(scrolling_coming_alpha, unk2cc[1])
+    SYNC_PROP(scrolling_now_alpha, unk2cc[2])
+    SYNC_PROP(scrolling_past_alpha, unk2cc[3])
+    SYNC_PROP(static_preview_alpha, unk2cc[8])
+    SYNC_PROP(static_coming_alpha, unk2cc[9])
+    SYNC_PROP(static_now_alpha, unk2cc[0xA])
+    SYNC_PROP(static_past_alpha, unk2cc[0xB])
+    SYNC_PROP(min_pitch_range, mMinPitchRange)
+    SYNC_PROP(pitch_display_margin, mPitchDisplayMargin)
+    SYNC_PROP(arrow_smoothing, mArrowSmoothing)
+    SYNC_PROP(configurable_objects, mConfigurableObjects)
+    SYNC_PROP(phrase_feedback_trig, mPhraseFeedbackTrig)
+    SYNC_PROP(spotlight_sparkles_only_trig, mSpotlightSparklesOnlyTrig)
+    SYNC_PROP(spotlight_phrase_success_trig, mSpotlightPhraseSuccessTrig)
+    SYNC_PROP(vox_configuration, mVoxCfg)
+    SYNC_PROP(pitch_window, mPitchWindow)
+    SYNC_PROP(pitch_window_height, mPitchWindowHeight)
+    SYNC_PROP(pitch_window_mesh, mPitchWindowMesh)
+    SYNC_PROP(pitch_window_overlay, mPitchWindowOverlay)
+    SYNC_PROP(lead_lyrics, mLeadLyrics)
+    SYNC_PROP(lead_lyric_height, mLeadLyricHeight)
+    SYNC_PROP(lead_lyric_mesh, mLeadLyricMesh)
+    SYNC_PROP(harmony_lyrics, mHarmLyrics)
+    SYNC_PROP(harmony_lyric_height, mHarmLyricHeight)
+    SYNC_PROP(harmony_lyric_mesh, mHarmLyricMesh)
+    SYNC_PROP(left_deco_mesh, mLeftDecoMesh)
+    SYNC_PROP(right_deco_mesh, mRightDecoMesh)
+    SYNC_PROP(now_bar_width, mNowBarWidth)
+    SYNC_PROP(now_bar_mesh, mNowBarMesh)
+    SYNC_PROP(remote, mRemote)
+    SYNC_PROP(track_left, mTrackLeft)
+    SYNC_PROP(track_right, mTrackRight)
+    SYNC_PROP(track_bottom, mTrackBottom)
+    SYNC_PROP(track_top, mTrackTop)
+    SYNC_PROP(pitch_bottom, mPitchBottom)
+    SYNC_PROP(pitch_top, mPitchTop)
+    SYNC_PROP(now_bar, mNowBar)
+    SYNC_PROP(pitch_guides, mPitchGuides)
+    SYNC_PROP(tube_style, mTubeStyle)
+    SYNC_PROP(arrow_style, mArrowStyle)
+    SYNC_PROP(font_style, mFontStyle)
+    SYNC_PROP(last_min, mLastMin)
+    SYNC_PROP(last_max, mLastMax)
+    SYNC_PROP(middle_c_z_pos, mMiddleCZPos)
+    SYNC_PROP(tonic, mTonic)
+    SYNC_PROP(range_scale_anim, mRangeScaleAnim)
+    SYNC_PROP(range_offset_anim, mRangeOffsetAnim)
+    SYNC_PROP(lead_deploy_mat, mLeadDeployMat)
+    SYNC_PROP(harmony_deploy_mat, mHarmDeployMat)
+    SYNC_PROP(hidden_part_alpha, mHiddenPartAlpha)
+    SYNC_PROP(overdrive_meter, mStarPowerMeter)
+    SYNC_SUPERCLASS(BandTrack)
+    SYNC_SUPERCLASS(RndDir)
+END_PROPSYNCS
+#pragma pop
--- src/system/bandobj/VocalTrackDir.h
+++ src/system/bandobj/VocalTrackDir.h
@@ -41,70 +41,70 @@ public:
     NEW_OVERLOAD;
     DELETE_OVERLOAD;
 
-    float unk2a0; // 0x2a0
+    float mHiddenPartAlpha; // 0x2a0
     bool unk2a4; // 0x2a4
     bool unk2a5; // 0x2a5
-    bool unk2a6; // 0x2a6
+    bool mIsTop; // 0x2a6
     bool unk2a7; // 0x2a7
-    int unk2a8; // 0x2a8
-    int unk2ac; // 0x2ac
-    int unk2b0; // 0x2b0
+    int mFeedbackStateLead; // 0x2a8
+    int mFeedbackStateHarm1; // 0x2ac
+    int mFeedbackStateHarm2; // 0x2b0
     std::map<int, Hmx::Color> unk2b4; // 0x2b4
     std::map<int, float> unk2cc; // 0x2cc
     ObjPtr<RndDir, ObjectDir> unk2e4; // 0x2e4
     ObjPtr<RndDir, ObjectDir> unk2f0; // 0x2f0
-    float unk2fc; // 0x2fc
-    float unk300; // 0x300
-    float unk304; // 0x304
-    ObjPtrList<RndTransformable, ObjectDir> unk308; // 0x308
-    ObjPtr<Hmx::Object, ObjectDir> unk318; // 0x318
+    float mMinPitchRange; // 0x2fc
+    float mPitchDisplayMargin; // 0x300
+    float mArrowSmoothing; // 0x304
+    ObjPtrList<RndTransformable, ObjectDir> mConfigurableObjects; // 0x308
+    ObjPtr<Hmx::Object, ObjectDir> mVoxCfg; // 0x318
     ObjPtr<RndDir, ObjectDir> unk324; // 0x324
     ObjPtr<EventTrigger, ObjectDir> unk330; // 0x330
     ObjPtr<EventTrigger, ObjectDir> unk33c; // 0x33c
     ObjPtr<BandLabel, ObjectDir> unk348; // 0x348
-    ObjPtr<EventTrigger, ObjectDir> unk354; // 0x354
-    ObjPtr<EventTrigger, ObjectDir> unk360; // 0x360
-    ObjPtr<EventTrigger, ObjectDir> unk36c; // 0x36c
+    ObjPtr<EventTrigger, ObjectDir> mPhraseFeedbackTrig; // 0x354
+    ObjPtr<EventTrigger, ObjectDir> mSpotlightSparklesOnlyTrig; // 0x360
+    ObjPtr<EventTrigger, ObjectDir> mSpotlightPhraseSuccessTrig; // 0x36c
     ObjPtr<PitchArrow, ObjectDir> unk378; // 0x378
     ObjPtr<PitchArrow, ObjectDir> unk384; // 0x384
     ObjPtr<PitchArrow, ObjectDir> unk390; // 0x390
-    bool unk39c; // 0x39c
-    float unk3a0; // 0x3a0
-    ObjPtr<RndMesh, ObjectDir> unk3a4; // 0x3a4
-    ObjPtr<RndMesh, ObjectDir> unk3b0; // 0x3b0
-    bool unk3bc; // 0x3bc
-    float unk3c0; // 0x3c0
-    ObjPtr<RndMesh, ObjectDir> unk3c4; // 0x3c4
-    bool unk3d0; // 0x3d0
-    float unk3d4; // 0x3d4
-    ObjPtr<RndMesh, ObjectDir> unk3d8; // 0x3d8
-    ObjPtr<RndMesh, ObjectDir> unk3e4; // 0x3e4
-    ObjPtr<RndMesh, ObjectDir> unk3f0; // 0x3f0
-    float unk3fc; // 0x3fc
-    ObjPtr<RndMesh, ObjectDir> unk400; // 0x400
-    bool unk40c; // 0x40c
-    float unk410; // 0x410
-    float unk414; // 0x414
-    float unk418; // 0x418
-    float unk41c; // 0x41c
-    float unk420; // 0x420
-    float unk424; // 0x424
-    float unk428; // 0x428
+    bool mPitchWindow; // 0x39c
+    float mPitchWindowHeight; // 0x3a0
+    ObjPtr<RndMesh, ObjectDir> mPitchWindowMesh; // 0x3a4
+    ObjPtr<RndMesh, ObjectDir> mPitchWindowOverlay; // 0x3b0
+    bool mLeadLyrics; // 0x3bc
+    float mLeadLyricHeight; // 0x3c0
+    ObjPtr<RndMesh, ObjectDir> mLeadLyricMesh; // 0x3c4
+    bool mHarmLyrics; // 0x3d0
+    float mHarmLyricHeight; // 0x3d4
+    ObjPtr<RndMesh, ObjectDir> mHarmLyricMesh; // 0x3d8
+    ObjPtr<RndMesh, ObjectDir> mLeftDecoMesh; // 0x3e4
+    ObjPtr<RndMesh, ObjectDir> mRightDecoMesh; // 0x3f0
+    float mNowBarWidth; // 0x3fc
+    ObjPtr<RndMesh, ObjectDir> mNowBarMesh; // 0x400
+    bool mRemote; // 0x40c
+    float mTrackLeft; // 0x410
+    float mTrackRight; // 0x414
+    float mTrackBottom; // 0x418
+    float mTrackTop; // 0x41c
+    float mPitchBottom; // 0x420
+    float mPitchTop; // 0x424
+    float mNowBar; // 0x428
     float unk42c; // 0x42c
-    Symbol unk430; // 0x430
-    ObjPtr<Hmx::Object, ObjectDir> unk434; // 0x434
-    ObjPtr<Hmx::Object, ObjectDir> unk440; // 0x440
-    ObjPtr<Hmx::Object, ObjectDir> unk44c; // 0x44c
+    Symbol mPitchGuides; // 0x430
+    ObjPtr<Hmx::Object, ObjectDir> mTubeStyle; // 0x434
+    ObjPtr<Hmx::Object, ObjectDir> mArrowStyle; // 0x440
+    ObjPtr<Hmx::Object, ObjectDir> mFontStyle; // 0x44c
     ObjPtr<RndText, ObjectDir> unk458; // 0x458
     ObjPtr<RndText, ObjectDir> unk464; // 0x464
     ObjPtr<RndText, ObjectDir> unk470; // 0x470
     ObjPtr<RndText, ObjectDir> unk47c; // 0x47c
-    float unk488; // 0x488
-    float unk48c; // 0x48c
-    float unk490; // 0x490
-    int unk494; // 0x494
-    ObjPtr<RndAnimatable, ObjectDir> unk498; // 0x498
-    ObjPtr<RndAnimatable, ObjectDir> unk4a4; // 0x4a4
+    float mLastMin; // 0x488
+    float mLastMax; // 0x48c
+    float mMiddleCZPos; // 0x490
+    int mTonic; // 0x494
+    ObjPtr<RndAnimatable, ObjectDir> mRangeScaleAnim; // 0x498
+    ObjPtr<RndAnimatable, ObjectDir> mRangeOffsetAnim; // 0x4a4
     bool unk4b0; // 0x4b0
     int unk4b4; // 0x4b4
     int unk4b8; // 0x4b8
@@ -156,8 +156,8 @@ public:
     float unk698; // 0x698
     float unk69c; // 0x69c
     float unk6a0; // 0x6a0
-    ObjPtr<RndMat, ObjectDir> unk6a4; // 0x6a4
-    ObjPtr<RndMat, ObjectDir> unk6b0; // 0x6b0
+    ObjPtr<RndMat, ObjectDir> mLeadDeployMat; // 0x6a4
+    ObjPtr<RndMat, ObjectDir> mHarmDeployMat; // 0x6b0
     float unk6bc; // 0x6bc
     float unk6c0; // 0x6c0
     int unk6c4; // 0x6c4
