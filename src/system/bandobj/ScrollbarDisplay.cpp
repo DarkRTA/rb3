b2b4a4dbe36a8c97a74435f807a045aa2f0ad039
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-16 11:41:15 -0700

    scrollbardisplay symbols in retail

--- src/system/bandobj/ScrollbarDisplay.cpp
+++ src/system/bandobj/ScrollbarDisplay.cpp
@@ -1,13 +1,78 @@
 #include "bandobj/ScrollbarDisplay.h"
+#include "ui/UI.h"
 #include "utl/Symbols.h"
 
-ScrollbarDisplay::ScrollbarDisplay() : mScrollbarList(this, 0), mScrollbarHeight(200.0f), mAlwaysShow(0), mListXOffset(0), mListYOffset(0), mMinThumbHeight(1.0f),
-    unk12c(0), unk130(0), unk134(0), unk138(0), unk13c(0), unk140(-1), unk144(-1), unk148(-1), unk14c(-1), unk150(0), unk154(0) {
+INIT_REVS(ScrollbarDisplay);
+
+void ScrollbarDisplay::Init(){
+    Register();
+    TheUI->InitResources("ScrollbarDisplay");
+}
+
+ScrollbarDisplay::ScrollbarDisplay() : m_pList(this, 0), mScrollbarHeight(200.0f), mAlwaysShow(0), mListXOffset(0), mListYOffset(0), mMinThumbHeight(1.0f),
+    m_pTopBone(0), m_pBottomBone(0), m_pThumbTopBone(0), m_pThumbBottomBone(0), m_pThumbGroup(0), unk140(-1), unk144(-1), unk148(-1), unk14c(-1), m_fSavedPosition(0), m_fSavedScale(0) {
 
 }
 
 ScrollbarDisplay::~ScrollbarDisplay(){
     
+}
+
+BEGIN_COPYS(ScrollbarDisplay)
+    COPY_SUPERCLASS(UIComponent)
+END_COPYS
+
+void ScrollbarDisplay::CopyMembers(const UIComponent* o, Hmx::Object::CopyType ty){
+    UIComponent::CopyMembers(o, ty);
+    CREATE_COPY_AS(ScrollbarDisplay, pDisplay);
+    MILO_ASSERT(pDisplay, 0x44);
+    COPY_MEMBER_FROM(pDisplay, m_pList)
+    COPY_MEMBER_FROM(pDisplay, mAlwaysShow)
+    COPY_MEMBER_FROM(pDisplay, mListXOffset)
+    COPY_MEMBER_FROM(pDisplay, mListYOffset)
+    COPY_MEMBER_FROM(pDisplay, mScrollbarHeight)
+    COPY_MEMBER_FROM(pDisplay, mMinThumbHeight)
+}
+
+SAVE_OBJ(ScrollbarDisplay, 0x57)
+
+BEGIN_LOADS(ScrollbarDisplay)
+    PreLoad(bs);
+    PostLoad(bs);
+END_LOADS
+
+void ScrollbarDisplay::PreLoad(BinStream& bs){
+    LOAD_REVS(bs);
+    ASSERT_REVS(2, 0);
+    bs >> m_pList;
+    bs >> mAlwaysShow;
+    if(gRev < 1){ bool b; bs >> b; }
+    bs >> mListXOffset;
+    bs >> mListYOffset;
+    bs >> mScrollbarHeight;
+    if(gRev >= 2) bs >> mMinThumbHeight;
+    UIComponent::PreLoad(bs);
+}
+
+void ScrollbarDisplay::PostLoad(BinStream& bs){
+    UIComponent::PostLoad(bs);
+    Update();
+}
+
+void ScrollbarDisplay::Enter(){ UIComponent::Enter(); }
+
+void ScrollbarDisplay::UpdateThumbScaleAndPosition(){
+    GetSavedPosition();
+    GetSavedScale();
+}
+
+void ScrollbarDisplay::SetAlwaysShow(bool b){ mAlwaysShow = b; }
+void ScrollbarDisplay::SetListXOffset(float f){ mListXOffset = f; }
+void ScrollbarDisplay::SetListYOffset(float f){ mListYOffset = f; }
+void ScrollbarDisplay::SetMinThumbHeight(float f){ mMinThumbHeight = f; }
+void ScrollbarDisplay::SetHeight(float f){
+    mScrollbarHeight = f;
+    if(mScrollbarHeight < mMinThumbHeight) mScrollbarHeight = mMinThumbHeight;
 }
 
 BEGIN_HANDLERS(ScrollbarDisplay)
@@ -17,11 +82,11 @@ END_HANDLERS
 
 BEGIN_PROPSYNCS(ScrollbarDisplay)
     SYNC_PROP_SET(scrollbar_list, GetList(), SetList(_val.Obj<BandList>(0)))
-    SYNC_PROP_SET(always_show, mAlwaysShow, SetAlwaysShow(_val.Int(0)))
+    SYNC_PROP_SET(always_show, GetAlwaysShow(), SetAlwaysShow(_val.Int(0)))
     SYNC_PROP_SET(list_attached, GetListAttached(), SetListAttached(_val.Int(0)))
-    SYNC_PROP_SET(list_x_offset, mListXOffset, SetListXOffset(_val.Float(0)))
-    SYNC_PROP_SET(list_y_offset, mListYOffset, SetListYOffset(_val.Float(0)))
-    SYNC_PROP_SET(scrollbar_height, mScrollbarHeight, SetHeight(_val.Float(0)))
-    SYNC_PROP_SET(min_thumb_height, mMinThumbHeight, SetMinThumbHeight(_val.Float(0)))
+    SYNC_PROP_SET(list_x_offset, GetListXOffset(), SetListXOffset(_val.Float(0)))
+    SYNC_PROP_SET(list_y_offset, GetListYOffset(), SetListYOffset(_val.Float(0)))
+    SYNC_PROP_SET(scrollbar_height, GetHeight(), SetHeight(_val.Float(0)))
+    SYNC_PROP_SET(min_thumb_height, GetMinThumbHeight(), SetMinThumbHeight(_val.Float(0)))
     SYNC_SUPERCLASS(UIComponent)
 END_PROPSYNCS
--- src/system/bandobj/ScrollbarDisplay.h
+++ src/system/bandobj/ScrollbarDisplay.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "bandobj/BandList.h"
 #include "ui/UIComponent.h"
+#include "rndobj/Mesh.h"
+#include "rndobj/Group.h"
 
 class ScrollbarDisplay : public UIComponent {
 public:
@@ -20,7 +22,14 @@ public:
     virtual void CopyMembers(const UIComponent*, CopyType);
     virtual void Update();
 
-    BandList* GetList() const { return mScrollbarList.Ptr() ? mScrollbarList.Ptr() : 0; }
+    BandList* GetList() const { return m_pList.Ptr() ? m_pList.Ptr() : 0; }
+    bool GetAlwaysShow() const { return mAlwaysShow; }
+    float GetListXOffset() const { return mListXOffset; }
+    float GetListYOffset() const { return mListYOffset; }
+    float GetHeight() const { return mScrollbarHeight; }
+    float GetMinThumbHeight() const { return mMinThumbHeight; }
+    float GetSavedPosition() const { return m_fSavedPosition; }
+    float GetSavedScale() const { return m_fSavedScale; }
     void SetList(BandList*);
     void SetAlwaysShow(bool);
     void SetListAttached(bool);
@@ -29,26 +38,33 @@ public:
     void SetListYOffset(float);
     void SetHeight(float);
     void SetMinThumbHeight(float);
+    void UpdateThumbScaleAndPosition();
+
+    static void Init();
+    static void Register(){
+        REGISTER_OBJ_FACTORY(ScrollbarDisplay);
+    }
+    NEW_OBJ(ScrollbarDisplay);
 
     DECLARE_REVS;
     NEW_OVERLOAD;
     DELETE_OVERLOAD;
 
-    ObjPtr<BandList, ObjectDir> mScrollbarList; // 0x10c
+    ObjPtr<BandList, ObjectDir> m_pList; // 0x10c
     float mScrollbarHeight; // 0x118
     bool mAlwaysShow; // 0x11c
     float mListXOffset; // 0x120
     float mListYOffset; // 0x124
     float mMinThumbHeight; // 0x128
-    int unk12c; // 0x12c
-    int unk130; // 0x130
-    int unk134; // 0x134
-    int unk138; // 0x138
-    int unk13c; // 0x13c
+    RndMesh* m_pTopBone; // 0x12c
+    RndMesh* m_pBottomBone; // 0x130
+    RndMesh* m_pThumbTopBone; // 0x134
+    RndMesh* m_pThumbBottomBone; // 0x138
+    RndGroup* m_pThumbGroup; // 0x13c
     int unk140; // 0x140
     int unk144; // 0x144
     int unk148; // 0x148
     int unk14c; // 0x14c
-    float unk150; // 0x150
-    float unk154; // 0x154
+    float m_fSavedPosition; // 0x150
+    float m_fSavedScale; // 0x154
 };
