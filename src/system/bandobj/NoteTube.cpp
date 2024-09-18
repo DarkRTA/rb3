7791dd7a2849425c522624c274e56c0f3d299e89
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-18 11:30:10 -0700

    notetube retail work

--- src/system/bandobj/NoteTube.cpp
+++ src/system/bandobj/NoteTube.cpp
@@ -3,20 +3,18 @@
 #include "os/Debug.h"
 #include "decomp.h"
 
-NoteTube::NoteTube() : unk_0x1C(false), unk_0x20(-1), unk_0x24(false), mGlowLevel(-1), unk_0x2C(0), unk_0x2D(0), 
-    unk_0x30(0), unk_0x34(0), unk_0x38(0), unk_0x3C(0), unk_0x40(0), unk_0x44(0), unk_0x48(0), unk_0x4C(0), 
-    unk_0x50(0), unk_0x54(0), unk_0x60(1) { mPoints.reserve(100); }
+NoteTube::NoteTube() : mPitched(false), mPart(-1), unk_0x24(false), mGlowLevel(-1), unk_0x2C(0), unk_0x2D(0), 
+    unk_0x30(0), unk_0x34(0), unk_0x38(0), mBackMat(0), mFrontMat(0), mBackPlate(0), mFrontPlate(0), mBackParent(0), 
+    mFrontParent(0), mXPos(0), unk_0x60(1) { mPoints.reserve(100); }
 
 void NoteTube::SetNumPoints(int i) { 
     if (i > mPoints.capacity()) MILO_WARN("Reallocating NoteTube point buffer to %d; please alert HUD/Track owner!", i);
-    mPoints.resize(i); 
+    mPoints.resize(i);
 }
 
 void NoteTube::SetPointPos(int i, Vector3 v) {
     if (i < mPoints.size() ) mPoints[i] = v;
     else MILO_WARN("note tube has too few points\n");
-    
-    return;
 }
 
 DECOMP_FORCEACTIVE(NoteTube, "point pos query out of bounds\n")
@@ -27,17 +25,28 @@ void NoteTube::SetGlowLevel(int i) {
 }
 
 void NoteTube::BakePlates() {
-    if (unk_0x44) unk_0x44->Bake();
-    if (unk_0x48) unk_0x48->Bake();
+    if(mBackPlate) mBackPlate->Bake();
+    if(mFrontPlate) mFrontPlate->Bake();
 }
 
+void NoteTube::SetDeployTiming(float f1, float f2){
+    if(mBackPlate) mBackPlate->SetDeployTiming(f1, f2);
+    if(mFrontPlate) mFrontPlate->SetDeployTiming(f1, f2);
+}
 
-TubePlate::TubePlate(int i) : unk_0x0(Hmx::Object::New<RndMesh>()), unk_0x4(0), unk_0x8(i), 
+void NoteTube::SetMeshVert(RndMesh::Vert& v, float f1, float f2, float f3, float f4){
+    v.pos.Set(f1, 0, f2);
+    v.uv.Set(f3, f4);
+    v.color.SetAlpha(unk_0x60);
+    unk_0x38 = std::max(unk_0x38, f1);
+}
+
+TubePlate::TubePlate(int i) : mMesh(Hmx::Object::New<RndMesh>()), mParent(0), mAllocationCount(i), 
     unk_0xC(3.40282346638528859812e38), unk_0x10(0), unk_0x14(0), unk_0x18(3.40282346638528859812e38), 
     unk_0x1C(3.40282346638528859812e38), unk_0x20(0), unk_0x24(0) {
-    unk_0x0->mOwner->mVerts.reserve(unk_0x8, true);
-    unk_0x0->mOwner->mFaces.reserve(unk_0x8); // evil regswap
+    mMesh->mOwner->mVerts.reserve(mAllocationCount, true);
+    mMesh->mOwner->mFaces.reserve(mAllocationCount); // evil regswap
     Reset();
 }
 
-TubePlate::~TubePlate() { delete unk_0x0; unk_0x0 = 0; } // why bother setting it to 0 in the dtor...?
+TubePlate::~TubePlate() { RELEASE(mMesh); }
