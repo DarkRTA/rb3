0aa99d5a33f0c0e6aab007ec1aeccaf9395bcf88
Nathan <nathanhurst03@gmail.com>
2024-09-24 15:32:46 -0600

    Fix smaller build issues

--- src/band3/game/Defines.cpp
+++ src/band3/game/Defines.cpp
@@ -2,29 +2,29 @@
 
 TrackType ScoreTypeToTrackType(ScoreType scoreType) {
     TrackType trackType;
-    switch(scoreType) {
-        case 0:
-
+    switch (scoreType) {
+    case 0:
+        break;
     }
     return trackType;
 }
 
 ControllerType TrackTypeToControllerType(TrackType trackType) {
     switch (trackType) {
-        case 0:
+    case 0:
         return kControllerDrum;
-        case 1:
-        case 2:
+    case 1:
+    case 2:
         return kControllerGuitar;
-        case 3:
+    case 3:
         return kControllerVocals;
-        case 4:
-        case 5:
+    case 4:
+    case 5:
         return kControllerNone;
-        case 6:
-        case 8:
+    case 6:
+    case 8:
         return (ControllerType)4;
-        default: 
+    default:
         return (ControllerType)5;
     }
 }
