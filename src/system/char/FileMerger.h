929fbcecf44c31e661a1fcfce985a5cec3446fc4
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-09-28 17:38:31 -0700

    add filemerger

--- src/system/char/FileMerger.h
+++ src/system/char/FileMerger.h
@@ -2,6 +2,7 @@
 #include "obj/Object.h"
 #include "utl/Loader.h"
 #include "obj/Utl.h"
+#include "obj/ObjPtr_p.h"
 
 class OriginalPathable {
 public:
@@ -12,6 +13,23 @@ public:
 
 class FileMerger : public Hmx::Object, public Loader::Callback, public MergeFilter, public OriginalPathable {
 public:
+    class Merger {
+    public:
+        Merger(Hmx::Object*);
+
+        Symbol mName; // 0x0
+        FilePath mSelected; // 0x4
+        FilePath unk10; // 0x10
+        FilePath mLoaded; // 0x1c
+        bool mProxy; // 0x28
+        bool unk29; // 0x29
+        bool mPreClear; // 0x2a
+        int mSubdirs; // 0x2c
+        ObjPtr<ObjectDir,ObjectDir> mDir; // 0x30
+        ObjPtrList<Hmx::Object,ObjectDir> mLoadedObjects; // 0x3c
+        ObjPtrList<ObjectDir,ObjectDir> mLoadedSubdirs; // 0x4c
+    };
+
     FileMerger();
     virtual ~FileMerger();
     OBJ_CLASSNAME(FileMerger);
@@ -34,4 +52,19 @@ public:
     bool StartLoad(bool);
     void Select(Symbol, const FilePath&, bool);
     int FindMergerIndex(Symbol, bool);
+
+    static bool sDisableAll;
+
+    DECLARE_REVS;
+    NEW_OVERLOAD;
+    DELETE_OVERLOAD;
+
+    ObjVector<Merger, unsigned int> mMergers; // 0x30
+    bool mAsyncLoad; // 0x40
+    bool mLoadingLoad; // 0x41
+    int unk44;
+    std::list<int> unk48;
+    int unk50;
+    int mHeap; // 0x54
+    Loader::Callback* unk58; // 0x58
 };
