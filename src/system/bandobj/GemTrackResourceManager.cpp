8b05af1ba7322d83a84bb03e0a023f55251251c2
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-10-02 22:20:31 -0700

    all gemtrackrsrcmgr funcs done

--- src/system/bandobj/GemTrackResourceManager.cpp
+++ src/system/bandobj/GemTrackResourceManager.cpp
@@ -12,29 +12,57 @@ GemTrackResourceManager::~GemTrackResourceManager(){
 void GemTrackResourceManager::InitSmasherPlates(){
     SmasherPlateInfo info(this);
     unk28.reserve(7);
-    info.unk0 = unk1c->Find<RndDir>("smasher_plate_guitar", true);
-    info.unkc = 0;
+    info.mSmasherPlate = unk1c->Find<RndDir>("smasher_plate_guitar", true);
+    info.mTrackInst = kInstGuitar;
     unk28.push_back(info);
-    info.unk0 = unk1c->Find<RndDir>("smasher_plate_bass", true);
-    info.unkc = 2;
+    info.mSmasherPlate = unk1c->Find<RndDir>("smasher_plate_bass", true);
+    info.mTrackInst = kInstBass;
     unk28.push_back(info);
-    info.unk0 = unk1c->Find<RndDir>("smasher_plate_keys", true);
-    info.unkc = 4;
+    info.mSmasherPlate = unk1c->Find<RndDir>("smasher_plate_keys", true);
+    info.mTrackInst = kInstKeys;
     unk28.push_back(info);
-    info.unk0 = unk1c->Find<RndDir>("smasher_plate_drum", true);
-    info.unkc = 1;
+    info.mSmasherPlate = unk1c->Find<RndDir>("smasher_plate_drum", true);
+    info.mTrackInst = kInstDrum;
     unk28.push_back(info);
-    info.unk0 = unk1c->Find<RndDir>("smasher_plate_real_guitar", true);
-    info.unkc = 5;
+    info.mSmasherPlate = unk1c->Find<RndDir>("smasher_plate_real_guitar", true);
+    info.mTrackInst = kInstRealGuitar;
     unk28.push_back(info);
-    info.unk0 = unk1c->Find<RndDir>("smasher_plate_real_bass", true);
-    info.unkc = 6;
+    info.mSmasherPlate = unk1c->Find<RndDir>("smasher_plate_real_bass", true);
+    info.mTrackInst = kInstRealBass;
     unk28.push_back(info);
-    info.unk0 = unk1c->Find<RndDir>("smasher_plate_real_keys", true);
-    info.unkc = 7;
+    info.mSmasherPlate = unk1c->Find<RndDir>("smasher_plate_real_keys", true);
+    info.mTrackInst = kInstRealKeys;
     unk28.push_back(info);
     for(int i = 0; i < unk28.size(); i++){
         static Message setup_draworder("setup_draworder", 0);
-        unk28[i].unk0->HandleType(setup_draworder);
+        unk28[i].mSmasherPlate->HandleType(setup_draworder);
     }
+}
+
+RndDir* GemTrackResourceManager::GetFreeSmasherPlate(TrackInstrument inst){
+    if(inst == kInstPending) return 0;
+    else {
+        for(int i = 0; i < unk28.size(); i++){
+            SmasherPlateInfo& curinfo = unk28[i];
+            if(curinfo.mTrackInst == inst && !curinfo.mInUse){
+                curinfo.mInUse = true;
+                return curinfo.mSmasherPlate;
+            }
+        }
+        MILO_WARN("Could not find free smasher plate for instrument %d", inst);
+    }
+    return 0;
+}
+
+void GemTrackResourceManager::ReleaseSmasherPlate(RndDir* plate){
+    for(int i = 0; i < unk28.size(); i++){
+        SmasherPlateInfo& info = unk28[i];
+        if(info.mSmasherPlate == plate){
+            MILO_ASSERT(info.mInUse, 0x60);
+            info.mInUse = false;
+            info.mSmasherPlate = plate;
+            return;
+        }
+    }
+    MILO_WARN("Tried to release invalid smasher 0x%08x", plate);
 }
--- src/system/bandobj/GemTrackResourceManager.h
+++ src/system/bandobj/GemTrackResourceManager.h
@@ -3,14 +3,16 @@
 #include "obj/Dir.h"
 #include "obj/ObjPtr_p.h"
 #include "rndobj/Dir.h"
+#include "bandobj/TrackPanelDirBase.h"
 
 class GemTrackResourceManager : public Hmx::Object {
 public:
     class SmasherPlateInfo {
     public:
-        SmasherPlateInfo(Hmx::Object* o) : unk0(o, 0), unkc(-1), mInUse(0) {}
-        ObjPtr<RndDir, ObjectDir> unk0; // 0x0
-        int unkc;
+        SmasherPlateInfo(Hmx::Object* o) : mSmasherPlate(o, 0), mTrackInst(kInstNone), mInUse(0) {}
+
+        ObjPtr<RndDir, ObjectDir> mSmasherPlate; // 0x0
+        TrackInstrument mTrackInst; // 0xc
         bool mInUse; // 0x10
     };
 
@@ -18,6 +20,8 @@ public:
     virtual ~GemTrackResourceManager();
 
     void InitSmasherPlates();
+    RndDir* GetFreeSmasherPlate(TrackInstrument);
+    void ReleaseSmasherPlate(RndDir*);
 
     ObjPtr<ObjectDir, ObjectDir> unk1c; // 0x1c
     std::vector<SmasherPlateInfo> unk28; // 0x28
