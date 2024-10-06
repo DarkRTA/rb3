f7839893db95f37695dc971e8581ab14c0861739
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-10-06 15:21:38 -0700

    add quazal synchronize class

---
+++ src/network/net/Synchronize.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "system/utl/BinStream.h"
+#include "system/utl/Str.h"
+#include "system/utl/HxGuid.h"
+
+class Synchronizable {
+public:
+    Synchronizable(const char*);
+    virtual ~Synchronizable();
+    virtual void SyncSave(BinStream&, unsigned int) const = 0;
+    virtual void SyncLoad(BinStream&, unsigned int) = 0;
+    virtual bool HasSyncPermission() const = 0;
+    virtual void OnSynchronizing(unsigned int){}
+    virtual void OnSynchronized(unsigned int){}
+
+    void Publish(const char*);
+    void Unpublish();
+    void SetSyncDirty(unsigned int, bool);
+    void SynchronizeIfDirty();
+    void AddDirtyUser(const UserGuid&);
+    const char* GetUniqueTag() const;
+
+    unsigned int unk4; // 0x4 - flags/mask?
+    std::vector<UserGuid> mUsers; // 0x8
+    String mTag; // 0x10
+};
