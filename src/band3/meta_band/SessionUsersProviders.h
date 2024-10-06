b08e98459df6219201ad841662c840f250179482
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-10-06 16:19:11 -0700

    add suprovider

---
+++ src/band3/meta_band/SessionUsersProviders.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "ui/UIListProvider.h"
+#include "rndobj/Mat.h"
+
+class SessionUsersProvider : public UIListProvider, public Hmx::Object {
+public:
+    SessionUsersProvider(bool, bool, bool);
+    virtual ~SessionUsersProvider();
+    virtual void Text(int, int, UIListLabel*, UILabel*) const;
+    virtual RndMat* Mat(int, int, UIListMesh*) const;
+    virtual int NumData() const;
+    virtual void InitData(RndDir*);
+    virtual DataNode Handle(DataArray*, bool);
+
+    std::vector<int> mUsers; // 0x20
+    bool unk28;
+    bool unk29;
+    bool unk2a;
+    RndMat* unk2c;
+    RndMat* unk30;
+};
