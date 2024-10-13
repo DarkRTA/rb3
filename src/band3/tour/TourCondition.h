259e99ed1ada4e152b38ee5ee4a3ec58440c4462
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-10-12 17:47:36 -0700

    tourcondition linked

--- src/band3/tour/TourCondition.h
+++ src/band3/tour/TourCondition.h
@@ -1,19 +1,28 @@
 #ifndef TOUR_TOURCONDITION_H
 #define TOUR_TOURCONDITION_H
-
 #include "obj/Data.h"
 #include "tour/TourProgress.h"
 
 class TourCondition {
-    public:
+public:
     TourCondition();
     virtual ~TourCondition();
 
     void Init(const DataArray*);
     bool IsMet(const TourProgress&) const;
     bool IsConditionMet(const TourProgress&, const DataArray*) const;
+    bool IsComparisonConditionMet(const TourPropertyCollection&, const DataArray*) const;
+    bool IsAndConditionMet(const TourProgress&, const DataArray*) const;
+    bool IsOrConditionMet(const TourProgress&, const DataArray*) const;
+    bool IsNotConditionMet(const TourProgress&, const DataArray*) const;
+    bool IsGreaterConditionMet(const TourPropertyCollection&, const DataArray*) const;
+    bool IsLessConditionMet(const TourPropertyCollection&, const DataArray*) const;
+    bool IsGreaterEqualConditionMet(const TourPropertyCollection&, const DataArray*) const;
+    bool IsLessEqualConditionMet(const TourPropertyCollection&, const DataArray*) const;
+    bool IsEqualConditionMet(const TourPropertyCollection&, const DataArray*) const;
+    bool IsNotEqualConditionMet(const TourPropertyCollection&, const DataArray*) const;
 
-    DataArray* mConds;
+    const DataArray* mConds; // 0x4
 };
 
 #endif //TOUR_TOURCONDITION_H
