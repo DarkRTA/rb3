4a7b3920b0758906e2fe274be6c92d761bf94888
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-10-20 19:32:44 -0700

    uievent work

--- src/band3/meta_band/UIEvent.cpp
+++ src/band3/meta_band/UIEvent.cpp
@@ -1,10 +1,14 @@
 #include "meta_band/UIEvent.h"
 #include "UIEventMgr.h"
 #include "obj/Data.h"
+#include "obj/Dir.h"
 #include "obj/Msg.h"
 #include "os/Debug.h"
+#include "ui/UI.h"
 #include "ui/UIMessages.h"
+#include "ui/UIScreen.h"
 #include "utl/Messages2.h"
+#include "utl/Symbols4.h"
 
 UIEvent::UIEvent(DataArray* a1, DataArray* a2) : mEventDef(a1), mActive(0) {
     if(a2) mEventInit = a2->Clone(true, true, 0);
@@ -57,4 +61,66 @@ bool DialogEvent::AllowsOverride() const {
     DataArray* overrideArr = mEventDef->FindArray("allows_override", false);
     if(overrideArr) return overrideArr->Int(1);
     else return true;
+}
+
+TransitionEvent::TransitionEvent(DataArray* a1, DataArray* a2) : UIEvent(a1, a2) {
+
+}
+
+DestructiveTransitionEvent::DestructiveTransitionEvent(NetSync* sync, DataArray* a1, DataArray* a2) : TransitionEvent(a1, a2), mNetSync(sync) {
+
+}
+
+void DestructiveTransitionEvent::OnActivate(){
+
+}
+
+void DestructiveTransitionEvent::SetTransitionAndDestination(){
+    mTransitionScreen = ObjectDir::Main()->Find<UIScreen>(mEventDef->FindStr(next_screen), true);
+    mDestinationScreen = ObjectDir::Main()->Find<UIScreen>(mTransitionScreen->Property(next_screen, true)->Str(), true);
+}
+
+bool DestructiveTransitionEvent::IsDestination(const std::vector<UIScreen*>& screens) const {
+    return screens.size() == 1 && screens[0] == mDestinationScreen;
+}
+
+bool DestructiveTransitionEvent::IsTransitionAllowed(UIScreen* screen) const {
+    return screen == mTransitionScreen || screen == mDestinationScreen;
+}
+
+NonDestructiveTransitionEvent::NonDestructiveTransitionEvent(NetSync* sync, const std::vector<UIScreen*>& screens) : TransitionEvent(0, 0), mNetSync(sync) {
+    mDestState = screens;
+    MILO_ASSERT(!mDestState.empty(), 0xAA);
+    MILO_ASSERT(mDestState.size() <= 2, 0xAB);
+}
+
+Symbol NonDestructiveTransitionEvent::Type() const { return non_destructive_transition; }
+
+void NonDestructiveTransitionEvent::Poll(){
+    if(TheUI->GetTransitionState() == kTransitionNone) AttemptNextTransition();
+}
+
+bool NonDestructiveTransitionEvent::IsDestination(const std::vector<UIScreen*>& screens) const {
+    bool samesizes = mDestState.size() == screens.size();
+    if(samesizes){
+        std::vector<UIScreen*>::const_iterator sit = mDestState.begin();
+        for(std::vector<UIScreen*>::const_iterator it = screens.begin(); it != screens.end(); ++it, ++sit){
+            if(*it != *sit) return false;
+        }
+        return true;
+    }
+    return samesizes;
+}
+
+bool NonDestructiveTransitionEvent::IsTransitionAllowed(UIScreen* screen) const {
+    for(int i = 0; i < mDestState.size(); i++){
+        if(mDestState[i] == screen) return true;
+    }
+    return false;
+}
+
+void NonDestructiveTransitionEvent::OnActivate(){ AttemptNextTransition(); }
+
+void NonDestructiveTransitionEvent::AttemptNextTransition(){
+    
 }
--- src/band3/meta_band/UIEvent.h
+++ src/band3/meta_band/UIEvent.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "NetSync.h"
 #include "obj/Data.h"
+#include "ui/UIScreen.h"
 
 class UIEvent {
 public:
@@ -32,4 +34,49 @@ public:
     virtual bool AllowsOverride() const;
     virtual void OnActivate();
     virtual void OnDismiss();
+};
+
+class TransitionEvent : public UIEvent {
+public:
+    TransitionEvent(DataArray*, DataArray*);
+    virtual ~TransitionEvent(){}
+    virtual bool IsDialogEvent() const { return false; }
+    virtual bool IsTransitionEvent() const { return true; }
+    virtual bool IsDestructiveEvent() const = 0;
+    virtual bool AllowsOverride() const { return false; }
+    virtual bool IsDestination(const std::vector<UIScreen*>&) const = 0;
+    virtual bool IsTransitionAllowed(UIScreen*) const = 0;
+};
+
+class DestructiveTransitionEvent : public TransitionEvent {
+public:
+    DestructiveTransitionEvent(NetSync*, DataArray*, DataArray*);
+    virtual ~DestructiveTransitionEvent(){}
+    virtual bool IsDestructiveEvent() const { return true; }
+    virtual void OnActivate();
+    virtual bool IsDestination(const std::vector<UIScreen*>&) const;
+    virtual bool IsTransitionAllowed(UIScreen*) const;
+
+    void SetTransitionAndDestination();
+
+    UIScreen* mTransitionScreen; // 0x10
+    UIScreen* mDestinationScreen; // 0x14
+    NetSync* mNetSync; // 0x18
+};
+
+class NonDestructiveTransitionEvent : public TransitionEvent {
+public:
+    NonDestructiveTransitionEvent(NetSync*, const std::vector<UIScreen*>&);
+    virtual ~NonDestructiveTransitionEvent(){}
+    virtual Symbol Type() const;
+    virtual bool IsDestructiveEvent() const { return false; }
+    virtual void Poll();
+    virtual void OnActivate();
+    virtual bool IsDestination(const std::vector<UIScreen*>&) const;
+    virtual bool IsTransitionAllowed(UIScreen*) const;
+
+    void AttemptNextTransition();
+
+    std::vector<UIScreen*> mDestState; // 0x10
+    NetSync* mNetSync; // 0x18
 };
