df59ec6736842b3136023eb1dc03f05838941e83
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-10-22 21:15:00 -0700

    meta messages

--- src/band3/meta_band/ClosetMgr.h
+++ src/band3/meta_band/ClosetMgr.h
@@ -90,25 +90,3 @@ public:
     bool mCharacterLoading; // 0x60
     bool unk61;
 };
-
-class CharacterFinishedLoadingMsg : public Message {
-public:
-    CharacterFinishedLoadingMsg() : Message(Type()) {}
-    CharacterFinishedLoadingMsg(DataArray *da) : Message(da) {}
-    virtual ~CharacterFinishedLoadingMsg() {}
-    static Symbol Type() {
-        static Symbol t("character_finished_loading_msg");
-        return t;
-    }
-};
-
-class FinalizedColorsMsg : public Message {
-public:
-    FinalizedColorsMsg() : Message(Type()) {}
-    FinalizedColorsMsg(DataArray *da) : Message(da) {}
-    virtual ~FinalizedColorsMsg() {}
-    static Symbol Type() {
-        static Symbol t("finalized_colors_msg");
-        return t;
-    }
-};
---
+++ src/band3/meta_band/MetaMessages.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "obj/Msg.h"
+
+class CharacterFinishedLoadingMsg : public Message {
+public:
+    CharacterFinishedLoadingMsg() : Message(Type()) {}
+    CharacterFinishedLoadingMsg(DataArray *da) : Message(da) {}
+    virtual ~CharacterFinishedLoadingMsg() {}
+    static Symbol Type() {
+        static Symbol t("character_finished_loading_msg");
+        return t;
+    }
+};
+
+class FinalizedColorsMsg : public Message {
+public:
+    FinalizedColorsMsg() : Message(Type()) {}
+    FinalizedColorsMsg(DataArray *da) : Message(da) {}
+    virtual ~FinalizedColorsMsg() {}
+    static Symbol Type() {
+        static Symbol t("finalized_colors_msg");
+        return t;
+    }
+};
