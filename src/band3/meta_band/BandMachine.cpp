f29b3c9b4ef30709c63ad4d74c376266880eb8af
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-10-25 13:28:54 -0700

    add bandmachine

--- src/band3/meta_band/BandMachine.cpp
+++ src/band3/meta_band/BandMachine.cpp
@@ -1,18 +1,104 @@
 #include "meta_band/BandMachine.h"
 #include "game/NetGameMsgs.h"
+#include "obj/ObjMacros.h"
+#include "utl/Symbols3.h"
 
 bool IsWaitingNetUIState(NetUIState state){
     return state >= 3 && state <= 8;
 }
 
-BandMachine::BandMachine() : unk1c(0), unk20(0), unk60(""), unk6c(""), unk78(0) {
+BandMachine::BandMachine() : mNetUIState(kNetUI_None), mNetUIStateParam(0), mPrimaryBandName(""), mPrimaryProfileName(""), mPrimaryMetaScore(0) {
 
 }
 
 void BandMachine::Reset(){
-    unk1c = 0;
-    unk20 = 0;
-    unk60 = "";
-    unk6c = "";
-    unk78 = 0;
+    mNetUIState = kNetUI_None;
+    mNetUIStateParam = 0;
+    mPrimaryBandName = "";
+    mPrimaryProfileName = "";
+    mPrimaryMetaScore = 0;
 }
+
+void BandMachine::SyncSave(BinStream& bs, unsigned char mask) const {
+    if(mask & 1){
+        bs << (unsigned char)mNetUIState;
+        bs << mNetUIStateParam;
+        bs << mCurrentSongPreview;
+    }
+    if(mask & 2){
+        bs << mSongs.size();
+        for(std::set<int>::const_iterator it = mSongs.begin(); it != mSongs.end(); ++it){
+            bs << *it;
+        }
+    }
+    if(mask & 8){
+        bs << mSongsWithProStrings.size();
+        for(std::set<int>::const_iterator it = mSongsWithProStrings.begin(); it != mSongsWithProStrings.end(); ++it){
+            bs << *it;
+        }
+    }
+    if(mask & 4){
+        bs << mPrimaryBandName;
+        bs << mPrimaryProfileName;
+        bs << mPrimaryMetaScore;
+    }
+}
+
+void BandMachine::SyncLoad(BinStream& bs, unsigned char mask){
+    if(mask & 1){
+        unsigned char state;
+        bs >> state;
+        mNetUIState = (NetUIState)state;
+        bs >> mNetUIStateParam;
+        bs >> mCurrentSongPreview;
+    }
+    if(mask & 2){
+        int size;
+        bs >> size;
+        mSongs.clear();
+        for(int i = 0; i < size; i++){
+            int key;
+            bs >> key;
+            mSongs.insert(key);
+        }
+    }
+    if(mask & 8){
+        int size;
+        bs >> size;
+        mSongsWithProStrings.clear();
+        for(int i = 0; i < size; i++){
+            int key;
+            bs >> key;
+            mSongsWithProStrings.insert(key);
+        }
+    }
+    if(mask & 4){
+        bs >> mPrimaryBandName;
+        bs >> mPrimaryProfileName;
+        bs >> mPrimaryMetaScore;
+    }
+}
+
+NetUIState BandMachine::GetNetUIState() const { return mNetUIState; }
+int BandMachine::GetNetUIStateParam() const { return mNetUIStateParam; }
+
+bool BandMachine::HasSong(int i) const {
+    return mSongs.find(i) != mSongs.end();
+}
+
+bool BandMachine::HasProGuitarOrBass(int i) const {
+    return mSongsWithProStrings.find(i) != mSongsWithProStrings.end();
+}
+
+BEGIN_HANDLERS(BandMachine)
+    HANDLE_EXPR(get_primary_profile_name, GetPrimaryProfileName())
+    HANDLE_EXPR(get_primary_band_name, GetPrimaryBandName())
+    HANDLE_EXPR(get_primary_metascore, mPrimaryMetaScore)
+    HANDLE_EXPR(get_net_ui_state, mNetUIState)
+    HANDLE_EXPR(get_net_ui_state_param, mNetUIStateParam)
+    HANDLE_EXPR(get_current_song_preview, mCurrentSongPreview.c_str())
+    HANDLE_CHECK(0xAB)
+END_HANDLERS
+
+String BandMachine::GetPrimaryProfileName(){ return mPrimaryProfileName; }
+String BandMachine::GetPrimaryBandName(){ return mPrimaryBandName; }
--- src/band3/meta_band/BandMachine.h
+++ src/band3/meta_band/BandMachine.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "game/NetGameMsgs.h"
 #include "obj/Object.h"
 #include "utl/Str.h"
 #include <set>
@@ -9,16 +10,23 @@ public:
     virtual ~BandMachine(){}
     virtual DataNode Handle(DataArray*, bool);
     virtual bool IsLocal() const = 0;
-    virtual String GetPrimaryBandName(){ return unk60; }
+    virtual String GetPrimaryBandName();
 
+    String GetPrimaryProfileName();
     void Reset();
+    void SyncSave(BinStream&, unsigned char) const;
+    void SyncLoad(BinStream&, unsigned char);
+    NetUIState GetNetUIState() const;
+    int GetNetUIStateParam() const;
+    bool HasSong(int) const;
+    bool HasProGuitarOrBass(int) const;
 
-    int unk1c;
-    int unk20;
-    std::set<int> unk24;
-    std::set<int> unk3c;
-    String unk54;
-    String unk60;
-    String unk6c;
-    int unk78;
+    NetUIState mNetUIState; // 0x1c
+    int mNetUIStateParam; // 0x20
+    std::set<int> mSongs; // 0x24
+    std::set<int> mSongsWithProStrings; // 0x3c
+    String mCurrentSongPreview; // 0x54
+    String mPrimaryBandName; // 0x60
+    String mPrimaryProfileName; // 0x6c
+    int mPrimaryMetaScore; // 0x78
 };
