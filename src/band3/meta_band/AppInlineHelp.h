70aedf8edc9bfc8f301774735cc17f58fce886e3
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-10-27 11:39:29 -0700

    appinlinehelp added

--- src/band3/meta_band/AppInlineHelp.h
+++ src/band3/meta_band/AppInlineHelp.h
@@ -1,11 +1,25 @@
 #pragma once
 #include "bandobj/InlineHelp.h"
 #include "game/BandUser.h"
+#include "game/GameMessages.h"
+#include "meta_band/MetaMessages.h"
 
 class AppInlineHelp : public InlineHelp {
 public:
     AppInlineHelp();
-    // virtual funcs go here...
+    OBJ_CLASSNAME(AppInlineHelp)
+    OBJ_SET_TYPE(AppInlineHelp)
+    virtual DataNode Handle(DataArray*, bool);
+    virtual ~AppInlineHelp(){}
+    virtual void Enter();
+    virtual void Exit();
+    virtual void UpdateIconTypes(bool);
 
     void SetOverrideUser(LocalBandUser*);
+
+    DataNode OnMsg(const InputStatusChangedMsg&);
+    DataNode OnMsg(const LocalUserLeftMsg&);
+    DataNode OnMsg(const AddLocalUserResultMsg&);
+
+    LocalBandUser* mOverrideUser; // 0x13c
 };
