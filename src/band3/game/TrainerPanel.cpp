477d241f6d2147c6c892a1f613778722e3ab6249
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-11-02 19:16:57 -0700

    begin work on trainerpanel

--- src/band3/game/TrainerPanel.cpp
+++ src/band3/game/TrainerPanel.cpp
@@ -1,1 +1,71 @@
 #include "game/TrainerPanel.h"
+#include "game/TrainerProgressMeter.h"
+#include "ui/UI.h"
+#include "ui/UIPanel.h"
+#include "utl/DataPointMgr.h"
+#include "utl/Messages.h"
+#include "utl/Symbols.h"
+
+TrainerPanel* TheTrainerPanel;
+
+TrainerPanel::TrainerPanel() : mProgressMeter(0), mCurrSection(-1), mShowProgressMeter(1) {
+    mProgressMeter = new TrainerProgressMeter();
+}
+
+TrainerPanel::~TrainerPanel(){
+    delete mProgressMeter;
+}
+
+void TrainerPanel::Enter(){
+    UIPanel::Enter();
+    InitSections();
+    mCurrSection = -1;
+    TheTrainerPanel = this;
+    RndDir* progressMeter = DataDir()->Find<RndDir>("progress_meter", true);
+    mProgressMeter->Init(progressMeter, mSections.size());
+    UpdateProgressMeter();
+    unk4c.clear();
+    unk4c.resize(mSections.size());
+}
+
+void TrainerPanel::Exit(){
+    UIPanel::Exit();
+    TheTrainerPanel = nullptr;
+}
+
+void TrainerPanel::Draw(){
+    UIPanel::Draw();
+    if(mShowProgressMeter){
+        TheUI->unk34->Select();
+        mProgressMeter->Draw();
+    }
+    else {
+        mProgressMeter->Hide();
+    }
+}
+
+void TrainerPanel::StartSection(int sect){
+    if(mCurrSection >= 0){
+        TrainerChallenge* tc = mSections[mCurrSection].mChallenge;
+        if(tc) tc->Exit();
+    }
+    mCurrSection = sect;
+    Message setSectNameMsg("set_section_name", mSections[sect].mName, sect);
+    Handle(setSectNameMsg, true);
+
+
+    TrainerChallenge* tc = mSections[mCurrSection].mChallenge;
+    if(mCurrSection >= 0 && tc){
+        Message updateRestsMsg("update_restrictions", tc->GetRestrictionToken());
+        Handle(updateRestsMsg, true);
+        tc->Enter();
+    }
+
+    Handle(reset_score_msg, true);
+    StartSectionImpl();
+    SendDataPoint("trainers/section", start_section, sect);
+}
+
+void TrainerPanel::RestartSection(){
+    StartSection(mCurrSection);
+}
--- src/band3/game/TrainerPanel.h
+++ src/band3/game/TrainerPanel.h
@@ -1,5 +1,35 @@
 #pragma once
+#include "game/Defines.h"
+#include "game/Player.h"
+#include "game/TrainerProgressMeter.h"
+#include "meta_band/BandProfile.h"
+#include "obj/Object.h"
 #include "ui/UIPanel.h"
+
+class TrainerChallenge : public Hmx::Object {
+public:
+    TrainerChallenge(){}
+    virtual ~TrainerChallenge(){}
+    OBJ_CLASSNAME(TrainerChallenge);
+    OBJ_SET_TYPE(TrainerChallenge);
+
+    void Enter();
+    void Exit();
+    bool Success();
+    Symbol GetRestrictionToken();
+};
+
+class TrainerSection {
+public:
+    TrainerSection();
+
+    Symbol mName; // 0x0
+    int mStartTick; // 0x4
+    int mEndTick; // 0x8
+    Symbol mChallengeName; // 0xc
+    TrainerChallenge* mChallenge; // 0x10
+    bool mStartEarly; // 0x14
+};
 
 class TrainerPanel : public UIPanel {
 public:
@@ -9,13 +39,24 @@ public:
     virtual void Draw();
     virtual void Enter();
     virtual void Exit();
+    virtual void HandlePlayerDeleted(Player*);
+    virtual void StartSectionImpl() = 0;
+    virtual void SetLessonComplete(int);
+    virtual bool AllSectionsFinished() const;
+    virtual bool ShouldStartEarly() const;
+    virtual bool IsSongSectionComplete(BandProfile*, int, Difficulty, int);
+    virtual void UpdateProgressMeter();
+    virtual void NewDifficulty(int, int);
+
+    void InitSections();
+    void StartSection(int);
+    void RestartSection();
+
+    TrainerProgressMeter* mProgressMeter; // 0x38
+    std::vector<TrainerSection> mSections; // 0x3c
+    int mCurrSection; // 0x44
+    bool mShowProgressMeter; // 0x48
+    std::vector<int> unk4c; // 0x4c
 };
 
-        // 80b848dc 80  15  e6  b0    addr       TrainerPanel::HandlePlayerDeleted
-        // 80b848e0 00  00  00  00    addr       00000000
-        // 80b848e4 80  20  0b  70    addr       TrainerPanel::SetLessonComplete
-        // 80b848e8 80  20  11  30    addr       TrainerPanel::AllSectionsFinished
-        // 80b848ec 80  20  11  70    addr       TrainerPanel::ShouldStartEarly
-        // 80b848f0 80  20  01  20    addr       TrainerPanel::IsSongSectionComplete
-        // 80b848f4 80  20  12  20    addr       TrainerPanel::UpdateProgressMeter
-        // 80b848f8 80  20  12  70    addr       TrainerPanel::NewDifficulty
+extern TrainerPanel* TheTrainerPanel;
