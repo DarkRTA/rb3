ca16ab1137832ab8c2f6c2bcf329ddcb5435d2f0
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-11-05 21:42:27 -0700

    trackerdisplays done

--- src/band3/game/TrackerDisplay.cpp
+++ src/band3/game/TrackerDisplay.cpp
@@ -1,6 +1,11 @@
 #include "game/TrackerDisplay.h"
+#include "bandobj/BandTrack.h"
+#include "bandtrack/TrackPanel.h"
+#include "beatmatch/TrackType.h"
 #include "obj/Data.h"
+#include "os/Debug.h"
 #include "utl/Messages.h"
+#include "utl/Messages2.h"
 #include "utl/Symbols.h"
 #include "utl/Symbols2.h"
 #include "utl/Symbols3.h"
@@ -102,4 +107,211 @@ void TrackerDisplay::HandleTargetPass(int i, DataArrayPtr& ptr) const {
 void TrackerDisplay::LastTargetPass() const {
     static Message msg(last_target_passed);
     SendMsg(msg);
+}
+
+TrackerBandDisplay::TrackerBandDisplay(){
+
+}
+
+TrackerBandDisplay::~TrackerBandDisplay(){
+
+}
+
+void TrackerBandDisplay::SetType(TrackerBandDisplayType ty) const {
+    static Message msg(set_display_type, 0);
+    msg[0] = ty;
+    SendMsg(msg);
+}
+
+void TrackerBandDisplay::SetStyle(TrackerBandDisplayStyle sty) const {
+    static Message msg(set_display_style, 0);
+    msg[0] = sty;
+    SendMsg(msg);
+}
+
+void TrackerBandDisplay::SetSuccessState(bool b) const {
+    static Message msg(set_success_state, 0);
+    msg[0] = b;
+    SendMsg(msg);
+}
+
+void TrackerBandDisplay::SetProgressPercentage(float f) const {
+    static Message msg(set_progress_percentage, 0.0f);
+    msg[0] = f;
+    SendMsg(msg);
+}
+
+void TrackerBandDisplay::SendMsg(const Message& msg) const {
+    GetTrackPanel()->SendTrackerDisplayMessage(msg);
+}
+
+TrackerPlayerDisplay::TrackerPlayerDisplay() : mPlayer(0) {
+
+}
+
+TrackerPlayerDisplay::~TrackerPlayerDisplay(){
+
+}
+
+void TrackerPlayerDisplay::Hide() const { TrackerDisplay::Hide(); }
+
+void TrackerPlayerDisplay::Enable() const {
+    SendMsg(enable_msg);
+    bool cansend = mPlayer && mPlayer->IsLocal();
+    if(cansend){
+        SendPlayerDisplayMsg((NetDisplayMsg)0, 0, 0);
+    }
+}
+
+void TrackerPlayerDisplay::Disable() const { return SendMsg(disable_msg); }
+
+void TrackerPlayerDisplay::GainFocus(bool gain) const {
+    static Message msg(gain_focus, 0);
+    msg[0] = gain;
+    SendMsg(msg);
+    bool cansend = mPlayer && mPlayer->IsLocal();
+    if(cansend){
+        SendPlayerDisplayMsg((NetDisplayMsg)1, gain, 0);
+    }
+}
+
+void TrackerPlayerDisplay::LoseFocus(bool lose) const {
+    static Message msg(lose_focus, 0);
+    msg[0] = lose;
+    SendMsg(msg);
+    bool cansend = mPlayer && mPlayer->IsLocal();
+    if(cansend){
+        SendPlayerDisplayMsg((NetDisplayMsg)2, lose, 0);
+    }
+}
+
+void TrackerPlayerDisplay::SetSuccessState(bool succ) const {
+    static Message msg(set_success_state, 0);
+    msg[0] = succ;
+    SendMsg(msg);
+}
+
+void TrackerPlayerDisplay::Pulse(bool topulse) const {
+    static Message msg(pulse, 0);
+    msg[0] = topulse;
+    SendMsg(msg);
+    bool cansend = mPlayer && mPlayer->IsLocal();
+    if(cansend){
+        SendPlayerDisplayMsg((NetDisplayMsg)3, topulse, 0);
+    }
+}
+
+void TrackerPlayerDisplay::SetProgressPercentage(float perc, bool b) const {
+    static Message msg(set_progress_percentage, 0.0f, 0);
+    msg[0] = perc;
+    msg[1] = b;
+    SendMsg(msg);
+}
+
+void TrackerPlayerDisplay::FillProgressAndReset(bool b) const {
+    if(!b || (mPlayer && mPlayer->GetTrackType() == kTrackVocals)){
+        SendMsg(fill_progress_and_reset_msg);
+    }
+    bool cansend = mPlayer && mPlayer->IsLocal();
+    if(cansend){
+        SendPlayerDisplayMsg((NetDisplayMsg)4, b, 0);
+    }
+}
+
+void TrackerPlayerDisplay::SetSecondaryStateLevel(int i) const {
+    static Message msg(set_secondary_state_level, 0.0f);
+    msg[0] = i;
+    SendMsg(msg);
+    bool cansend = mPlayer && mPlayer->IsLocal();
+    if(cansend){
+        SendPlayerDisplayMsg((NetDisplayMsg)5, i, 0);
+    }
+}
+
+void TrackerPlayerDisplay::RemotePlayerDisplayMsg(int i1, int i2, int i3) const {
+    switch(i1){
+        case 0:
+            Enable();
+            break;
+        case 1:
+            GainFocus(i2);
+            break;
+        case 2:
+            LoseFocus(i2);
+            break;
+        case 3:
+            Pulse(i2);
+            break;
+        case 4:
+            FillProgressAndReset(i2);
+            break;
+        case 5:
+            SetSecondaryStateLevel(i2);
+            break;
+        default:
+            MILO_WARN("Unhandled tracker player display msg: %d %d %d.\n", i1, i2, i3);
+            break;
+    }
+}
+
+void TrackerPlayerDisplay::SendPlayerDisplayMsg(NetDisplayMsg msg, int i1, int i2) const {
+    if(mPlayer){
+        static Message displayMsg("send_tracker_player_display", 0, 0, 0);
+        displayMsg[0] = msg;
+        displayMsg[1] = i1;
+        displayMsg[2] = i2;
+        mPlayer->HandleType(displayMsg);
+    }
+}
+
+void TrackerPlayerDisplay::SendMsg(const Message& msg) const {
+    MILO_ASSERT(mPlayer, 0x233);
+    BandTrack* track = mPlayer->GetBandTrack();
+    if(track) track->SendTrackerDisplayMessage(msg);
+}
+
+TrackerBroadcastDisplay::TrackerBroadcastDisplay(){
+
+}
+
+TrackerBroadcastDisplay::~TrackerBroadcastDisplay(){
+
+}
+
+void TrackerBroadcastDisplay::Broadcast(const DataArrayPtr& ptr, Symbol s){
+    SetType((BroadcastDisplayType)0);
+    static Message msg(broadcast, 0, 0);
+    msg[0] = ptr;
+    msg[1] = s;
+    SendMsg(msg);
+}
+
+void TrackerBroadcastDisplay::SetType(BroadcastDisplayType ty) const {
+    static Message msg(set_display_type, 0);
+    msg[0] = ty;
+    SendMsg(msg);
+}
+
+void TrackerBroadcastDisplay::SetSecondaryStateLevel(int level) const {
+    static Message msg(set_secondary_state_level, 0);
+    msg[0] = level;
+    SendMsg(msg);
+}
+
+void TrackerBroadcastDisplay::SetBandMessage(const DataArrayPtr& ptr) const {
+    SetType((BroadcastDisplayType)1);
+    static Message msg(set_band_message, 0);
+    msg[0] = ptr;
+    SendMsg(msg);
+}
+
+void TrackerBroadcastDisplay::ShowBriefBandMessage(const DataArrayPtr& ptr) const {
+    SetType((BroadcastDisplayType)1);
+    static Message msg(show_brief_band_message, 0);
+    msg[0] = ptr;
+    SendMsg(msg);
+}
+
+void TrackerBroadcastDisplay::SendMsg(const Message& msg) const {
+    GetTrackPanel()->SendTrackerBroadcastDisplayMessage(msg);
 }
--- src/band3/game/TrackerDisplay.h
+++ src/band3/game/TrackerDisplay.h
@@ -1,8 +1,17 @@
 #pragma once
+#include "game/Player.h"
 #include "obj/Data.h"
 #include "obj/Msg.h"
 
 enum TrackerChallengeType {
+
+};
+
+enum TrackerBandDisplayType {
+
+};
+
+enum TrackerBandDisplayStyle {
 
 };
 
@@ -28,4 +37,59 @@ public:
     static DataArrayPtr MakeIntegerTargetDescription(int);
     static DataArrayPtr MakePercentTargetDescription(float);
     static DataArrayPtr MakeTimeTargetDescription(float);
+};
+
+class TrackerBandDisplay : public TrackerDisplay {
+public:
+    TrackerBandDisplay();
+    virtual ~TrackerBandDisplay();
+    virtual void SendMsg(const Message&) const;
+
+    void SetType(TrackerBandDisplayType) const;
+    void SetStyle(TrackerBandDisplayStyle) const;
+    void SetSuccessState(bool) const;
+    void SetProgressPercentage(float) const;
+};
+
+class TrackerPlayerDisplay : public TrackerDisplay {
+public:
+    enum NetDisplayMsg {
+        
+    };
+
+    TrackerPlayerDisplay();
+    virtual ~TrackerPlayerDisplay();
+    virtual void Hide() const;
+    virtual void SendMsg(const Message&) const;
+
+    void Enable() const;
+    void Disable() const;
+    void GainFocus(bool) const;
+    void LoseFocus(bool) const;
+    void SetSuccessState(bool) const;
+    void Pulse(bool) const;
+    void SetProgressPercentage(float, bool) const;
+    void FillProgressAndReset(bool) const;
+    void SetSecondaryStateLevel(int) const;
+    void RemotePlayerDisplayMsg(int, int, int) const;
+    void SendPlayerDisplayMsg(NetDisplayMsg, int, int) const;
+
+    Player* mPlayer; // 0x4
+};
+
+class TrackerBroadcastDisplay : public TrackerDisplay {
+public:
+    enum BroadcastDisplayType {
+
+    };
+    
+    TrackerBroadcastDisplay();
+    virtual ~TrackerBroadcastDisplay();
+    virtual void SendMsg(const Message&) const;
+
+    void Broadcast(const DataArrayPtr&, Symbol);
+    void SetType(BroadcastDisplayType) const;
+    void SetSecondaryStateLevel(int) const;
+    void SetBandMessage(const DataArrayPtr&) const;
+    void ShowBriefBandMessage(const DataArrayPtr&) const;
 };
