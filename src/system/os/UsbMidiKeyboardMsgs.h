bea5f6b1edc62d884783ea22d4ae9573e0e77f4d
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-11-10 15:06:03 -0700

    refactor key msgs

--- src/system/os/UsbMidiKeyboardMsgs.h
+++ src/system/os/UsbMidiKeyboardMsgs.h
@@ -2,47 +2,53 @@
 #define OS_USBMIDIKEYBOARDMSGS_H
 #include "obj/Msg.h"
 
-BEGIN_MESSAGE(KeyboardKeyPressedMsg, keyboard_key_pressed, int, int, int);
-    MESSAGE_ARRAY_CTOR(KeyboardKeyPressedMsg)
+DECLARE_MESSAGE(KeyboardKeyPressedMsg, "keyboard_key_pressed");
+    KeyboardKeyPressedMsg(int, int, int);
     int GetMidiNote() const { return mData->Int(2); }
     int GetNode3() const { return mData->Int(3); } // fret button? but this is keys what frets are there
     int GetPadNum() const { return mData->Int(4); }
 END_MESSAGE;
 
-BEGIN_MESSAGE(KeyboardKeyReleasedMsg, keyboard_key_released, int, int);
-    MESSAGE_ARRAY_CTOR(KeyboardKeyReleasedMsg)
+DECLARE_MESSAGE(KeyboardKeyReleasedMsg, "keyboard_key_released")
+    KeyboardKeyReleasedMsg(int, int);
     int GetMidiNote() const { return mData->Int(2); }
     int GetPadNum() const { return mData->Int(3); }
 END_MESSAGE;
 
-BEGIN_MESSAGE(KeyboardModMsg, keyboard_mod, int, int);
-    MESSAGE_ARRAY_CTOR(KeyboardModMsg)
+DECLARE_MESSAGE(KeyboardModMsg, "keyboard_mod")
+    KeyboardModMsg(int, int);
     int GetNode2() const { return mData->Int(2); } // pitch/velocity?
     int GetPadNum() const { return mData->Int(3); }
 END_MESSAGE;
 
-BEGIN_MESSAGE(KeyboardExpressionPedalMsg, keyboard_expression_pedal, int, int);
+DECLARE_MESSAGE(KeyboardExpressionPedalMsg, "keyboard_expression_pedal")
+    KeyboardExpressionPedalMsg(int, int);
 END_MESSAGE;
 
-BEGIN_MESSAGE(KeyboardConnectedAccessoriesMsg, keyboard_connected_accessories, int, int);
+DECLARE_MESSAGE(KeyboardConnectedAccessoriesMsg, "keyboard_connected_accessories")
+    KeyboardConnectedAccessoriesMsg(int, int);
 END_MESSAGE;
 
-BEGIN_MESSAGE(KeyboardSustainMsg, keyboard_sustain, bool, int);
-    MESSAGE_ARRAY_CTOR(KeyboardSustainMsg)
+DECLARE_MESSAGE(KeyboardSustainMsg, "keyboard_sustain")
+    KeyboardSustainMsg(bool, int);
     int GetNode2() const { return mData->Int(2); }
     int GetPadNum() const { return mData->Int(3); }
 END_MESSAGE;
 
-BEGIN_MESSAGE(KeyboardStompBoxMsg, keyboard_stomp_box, bool, int);
+DECLARE_MESSAGE(KeyboardStompBoxMsg, "keyboard_stomp_box")
+    KeyboardStompBoxMsg(bool, int);
 END_MESSAGE;
 
-BEGIN_MESSAGE(KeysAccelerometerMsg, keys_accelerometer, int, int, int, int);
+DECLARE_MESSAGE(KeysAccelerometerMsg, "keys_accelerometer")
+    KeysAccelerometerMsg(int, int, int, int);
 END_MESSAGE;
 
-BEGIN_MESSAGE(KeyboardLowHandPlacementMsg, keyboard_low_hand_placement, int, int);
+DECLARE_MESSAGE(KeyboardLowHandPlacementMsg, "keyboard_low_hand_placement")
+    KeyboardLowHandPlacementMsg(int, int);
 END_MESSAGE;
 
-BEGIN_MESSAGE(KeyboardHighHandPlacementMsg, keyboard_high_hand_placement, int, int);
+DECLARE_MESSAGE(KeyboardHighHandPlacementMsg, "keyboard_high_hand_placement")
+    KeyboardHighHandPlacementMsg(int, int);
 END_MESSAGE;
 
 #endif
