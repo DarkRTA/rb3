2ec89e6217ee38bbf1a8e3e426a9eafca79b234c
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-11-14 13:50:29 -0700

    dirunloader should be equivalent

--- src/system/obj/DirUnloader.cpp
+++ src/system/obj/DirUnloader.cpp
@@ -24,36 +24,21 @@ DirUnloader::~DirUnloader(){
 bool DirUnloader::IsLoaded() const { return false; }
 
 void DirUnloader::PollLoading(){
+    MILO_ASSERT(gSuppressPointTest>=0, 0x33);
     gSuppressPointTest++;
     TheLoadMgr.StartAsyncUnload();
-    while(true){
-
+    while(!TheLoadMgr.CheckSplit() && TheLoadMgr.GetFirstLoading() == this){
+        if(mObjects.empty()){
+            delete this;
+            break;
+        }
+        Hmx::Object* obj = mObjects.back();
+        if(obj) delete obj;
+        mObjects.pop_back();
     }
     TheLoadMgr.FinishAsyncUnload();
     MILO_ASSERT(gSuppressPointTest>0, 0x47);
     gSuppressPointTest--;
-
-//       gSuppressPointTest = gSuppressPointTest + 1;
-//   LoadMgr::StartAsyncUnload(TheLoadMgr);
-//   while( true ) {
-//     iVar2 = fn_8031A7A0(TheLoadMgr);
-//     if ((iVar2 != 0) || (piVar3 = fn_8031A6E8(TheLoadMgr), piVar3 != param_1)) goto LAB_803317c0;
-//     iVar2 = stlpmtx_std::vector<><>::empty(param_1 + 0xd);
-//     if (iVar2 != 0) break;
-//     pvVar1 = fn_80331864(param_1 + 0xd);
-//     piVar3 = MergedGet0x8(pvVar1);
-//     if ((piVar3 != 0x0) && (piVar3 != 0x0)) {
-//       (**(*piVar3 + 8))(piVar3,1);
-//     }
-//     fn_803317F8(param_1 + 0xd);
-//   }
-//   if (param_1 != 0x0) {
-//     (**(*param_1 + 8))(param_1,1);
-//   }
-// LAB_803317c0:
-//   LoadMgr::FinishAsyncUnload(TheLoadMgr);
-//   gSuppressPointTest = gSuppressPointTest + -1;
-//   return;
 }
 
 const char* DirUnloader::DebugText(){ return MakeString("UnLoader: %s", mFile.c_str()); }
