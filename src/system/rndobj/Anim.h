98c5cdecf9db130bc58db044cfe87882164a3dbc
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-11-21 12:27:16 -0700

    anim doc

--- src/system/rndobj/Anim.h
+++ src/system/rndobj/Anim.h
@@ -1,5 +1,4 @@
-#ifndef RNDOBJ_ANIM_H
-#define RNDOBJ_ANIM_H
+#pragma once
 #include "obj/Object.h"
 #include "obj/Task.h"
 #include "obj/ObjPtr_p.h"
@@ -51,15 +50,46 @@ public:
     DECLARE_REVS;
     DELETE_OVERLOAD;
 
-    void StopAnimation();
+    /** Determine if this animatable has any active tasks associated with it. */
     bool IsAnimating();
+    /** Kill any active tasks associated with this animatable. */
+    void StopAnimation();
+    /** Create a new task to animate this.
+     * @param [in] blend The animatable's desired blend.
+     * @param [in] wait If true, wait until blending finishes before animating.
+     * @param [in] delay How long to wait before the task should begin.
+     * @returns The newly created task.
+     */
     Task* Animate(float blend, bool wait, float delay);
+    /** Create a new task to animate this.
+     * @param [in] blend The animatable's desired blend.
+     * @param [in] wait If true, wait until blending finishes before animating.
+     * @param [in] delay How long to wait before the task should begin.
+     * @param [in] rate The rate to animate.
+     * @param [in] start The first frame to animate.
+     * @param [in] end The last frame to animate.
+     * @param [in] period Alternative to scale, overridden period of animation.
+     * @param [in] scale Multiplier to speed of animation.
+     * @param [in] type How to treat the frame outside of start and end (range, loop, shuttle)
+     * @returns The newly created task.
+     */
     Task* Animate(float blend, bool wait, float delay, Rate rate, float start, float end, float period, float scale, Symbol type);
+    /** Create a new task to animate this.
+     * @param [in] start The first frame to animate.
+     * @param [in] end The last frame to animate.
+     * @param [in] units The rate at which this Task should run.
+     * @param [in] period Alternative to scale, overridden period of animation.
+     * @param [in] blend The animatable's desired blend.
+     * @returns The newly created task.
+     */
     Task* Animate(float start, float end, TaskUnits units, float period, float blend);
     TaskUnits Units() const;
     float FramesPerUnit();
     bool ConvertFrames(float& frames);
 
+    /** Create a new AnimTask using the configuration in the supplied DataArray.
+     * @returns A DataNode housing the newly created task.
+     */
     DataNode OnAnimate(DataArray*);
     DataNode OnConvertFrames(DataArray*);
 
@@ -76,6 +106,7 @@ public:
     Rate mRate; // 0xc
 };
 
+/** A task meant for animating. */
 class AnimTask : public Task {
 public:
     AnimTask(RndAnimatable* anim, float start, float end, float fpu, bool loop, float blend);
@@ -91,17 +122,24 @@ public:
     NEW_POOL_OVERLOAD(AnimTask);
     DELETE_POOL_OVERLOAD(AnimTask);
 
+    /** The animatable this task should be animating. */
     ObjOwnerPtr<RndAnimatable> mAnim; // 0x1c
     ObjPtr<Hmx::Object> mAnimTarget; // 0x28
+    /** The anim task to blend into. */
     ObjPtr<AnimTask> mBlendTask; // 0x34
+    /** Whether or not this animation should blend into another. */
     bool mBlending; // 0x40
+    /** The time it takes to blend into mBlendTask. */
     float mBlendTime; // 0x44
     float mBlendPeriod; // 0x48
+    /** Start animation frame. */
     float mMin; // 0x4c
+    /** End animation frame. */
     float mMax; // 0x50
+    /** Multiplier to speed of animation. */
     float mScale; // 0x54
+    /** "Amount to offset frame for animation" */
     float mOffset; // 0x58
+    /** Whether or not the animation should loop. */
     bool mLoop; // 0x5c
 };
-
-#endif
