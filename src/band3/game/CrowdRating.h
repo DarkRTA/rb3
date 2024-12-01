57dc9369b344f89a7646568b6a325f5929cb3a7e
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-12-01 14:38:54 -0700

    add context to crowdrating

--- src/band3/game/CrowdRating.h
+++ src/band3/game/CrowdRating.h
@@ -20,19 +20,19 @@ public:
     bool IsBelowLoseLevel() const;
     bool CantFailYet() const;
     void ChangeDifficulty(BandUser *, Difficulty);
-    float GetLoseLevel() const { return unk18; }
-    void SetLoseLevel(float level) { unk18 = level; }
-    float GetMinValue(){ return unk10; }
-    float GetValue() const { return unkc; }
-    float GetRawValue() const { return unk8; }
+    float GetLoseLevel() const { return mLoseLevel; }
+    void SetLoseLevel(float level) { mLoseLevel = level; }
+    float GetMinValue(){ return mRunningMin; }
+    float GetValue() const { return mValue; }
+    float GetRawValue() const { return mRawValue; }
     bool IsActive() const { return mActive; }
 
     bool mActive; // 0x4
-    float unk8;
-    float unkc;
-    float unk10;
-    float unk14;
-    float unk18;
+    float mRawValue; // 0x8
+    float mValue; // 0xc
+    float mRunningMin; // 0x10
+    float mSongFraction; // 0x14
+    float mLoseLevel; // 0x18
     char unk1c[16];
     float unk2c;
 };
