92e1694f46d49515fe27d0c8e16c501809174126
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-12-17 14:00:45 -0500

    cheatprov doc

--- src/system/ui/CheatProvider.h
+++ src/system/ui/CheatProvider.h
@@ -14,8 +14,11 @@ public:
     public:
         Cheat(const char* desc) : mKey(), mDesc(desc), mScript(0) {}
         Cheat(const String& key, const String& desc, DataArray* script) : mKey(key), mDesc(desc), mScript(script) {}
+        /** The key(s) to press to activate this cheat. */
         String mKey; // 0x0
+        /** A quick description of what this cheat does. */
         String mDesc; // 0xc
+        /** The script performing this cheat. */
         DataArray* mScript; // 0x18
     };
 
@@ -27,16 +30,26 @@ public:
     virtual void InitData(RndDir*);
     virtual DataNode Handle(DataArray*, bool);
 
-    void Invoke(int, LocalUser*);
+    /** Invoke a cheat.
+     * @param [in] idx The index in mFilterCheats of the cheat to invoke.
+     * @param [in] user The user to which the cheat should apply.
+     */
+    void Invoke(int idx, LocalUser* user);
+    /** Set the current filter in place to the next available filter. */
     void NextFilter();
+    /** Update the filtered cheats vector based on the current filter. */
     void ApplyFilter();
 
     static void Init();
     static void Terminate();
     static CheatProvider* sInstance;
 
+    /** The series of cheats. */
     std::vector<Cheat> mCheats; // 0x20
+    /** The series of filtered cheats. */
     std::vector<Cheat> mFilterCheats; // 0x28
+    /** The series of filters (places where certain cheats are only allowed to work, i.e. game, meta, store, writer) */
     std::vector<Symbol> mFilters; // 0x30
+    /** The current filter in place. */
     int mFilterIdx; // 0x38
 };
