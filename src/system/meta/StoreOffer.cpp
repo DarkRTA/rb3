5f0c4c456dc9bb8f349b5d1c74bc3d964f5183e8
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-12-21 12:38:09 -0500

    storeoffer handler

--- src/system/meta/StoreOffer.cpp
+++ src/system/meta/StoreOffer.cpp
@@ -15,8 +15,12 @@ namespace {
     const char* PACK_INCLUDES = "This pack includes";
 }
 
-const char* gGenreStrs[29] = {
-    "alternative", "blues", "classicrock"
+const char* gGenreStrs[30] = {
+    "alternative", "blues", "classicrock", "country", "emo", "fusion", "glam",
+    "grunge", "indierock", "jazz", "metal", "new_wave", "novelty", "numetal",
+    "other", "poprock", "prog", "punk", "rock", "southernrock", "urban",
+    "classical", "hiphoprap", "inspirational", "jrock", "latin", "popdanceelectronic",
+    "rbsoulfunk", "reggaeska", "world"
 };
 
 StorePurchaseable::StorePurchaseable() : unk1c(0), unk20(0) {
@@ -32,8 +36,13 @@ const char* StorePurchaseable::CostStr() const {
     else return "%i";
 }
 
+bool operator==(const StoreOffer* o, Symbol s){
+    return o->ShortName() == s;
+}
+
 Symbol StoreOffer::Decade() const {
-    return "the%is";
+    int year = ((unk1c->mYearReleased + 1900) / 10) * 10;
+    return MakeString("the%is", year);
 }
 
 Symbol StoreOffer::LengthSym() const {
@@ -76,10 +85,10 @@ StoreOffer::~StoreOffer(){
 }
 
 Symbol StoreOffer::OfferType() const {
-    int offerType = unk1c->mOfferType;
+    StoreOfferType offerType = (StoreOfferType)unk1c->mOfferType;
     switch(offerType){
-        case 1: return "album";
-        case 2: return "pack";
+        case kStoreOfferAlbum: return "album";
+        case kStoreOfferPack: return "pack";
         default: return "song";
     }
 }
@@ -87,7 +96,8 @@ Symbol StoreOffer::OfferType() const {
 bool StoreOffer::IsNewRelease() const { return unk1c->mNewRelease; }
 
 Symbol StoreOffer::Genre() const {
-    if(unk1c->mOfferType != 2){
+    StoreOfferType offerType = (StoreOfferType)unk1c->mOfferType;
+    if(offerType != kStoreOfferPack){
         int idx = unk1c->mGenre;
         if(idx > 29) idx = 29;
         return gGenreStrs[idx];
@@ -102,3 +112,56 @@ bool StoreOffer::HasPreviewAudio() const { return unk1c->mOfferType == 0; }
 
 bool StoreOffer::HasGenre() const { return unk1c->mOfferType != 2; }
 bool StoreOffer::HasVocalParts() const { return unk1c->mVocalParts > 0; }
+
+#pragma push
+#pragma dont_inline on
+BEGIN_HANDLERS(StoreOffer)
+    HANDLE_EXPR(short_name, ShortName())
+    HANDLE_EXPR(offer_type, OfferType())
+    HANDLE_EXPR(offer_name, OfferName())
+    HANDLE_EXPR(description, Description())
+    HANDLE_EXPR(description_data, (DescriptionData(_msg->Obj<UILabel>(2)), kDataArray))
+    HANDLE_EXPR(is_new_release, IsNewRelease())
+    HANDLE_EXPR(year_released, YearReleased())
+    HANDLE_EXPR(genre, Genre())
+    HANDLE_EXPR(sub_genre, SubGenre())
+    HANDLE_EXPR(cost_str, CostStr())
+    HANDLE_EXPR(in_library, InLibrary())
+    HANDLE_EXPR(is_available, IsAvailable())
+    HANDLE_EXPR(is_purchased, IsPurchased())
+    HANDLE_EXPR(has_available_pack, IsAvailable())
+    HANDLE_EXPR(has_available_album, IsAvailable())
+    HANDLE_EXPR(pack_name, mPack.unk1c ? mPack.unk1c->GetName() : "")
+    HANDLE_EXPR(album_name, mAlbum.unk1c ? mAlbum.unk1c->GetName() : "")
+    HANDLE_EXPR(is_completely_unavailable, IsCompletelyUnavailable())
+    HANDLE_EXPR(is_test, IsTest())
+    HANDLE_EXPR(pack_first_letter, PackFirstLetter())
+    HANDLE_EXPR(release_date_str, ReleaseDateStr())
+    HANDLE_EXPR(review, Review())
+    HANDLE_EXPR(artist, Artist())
+    HANDLE_EXPR(rank, PartRank(_msg->Sym(2)))
+    HANDLE_EXPR(label, Label())
+    HANDLE_EXPR(author, Author())
+    HANDLE_EXPR(language, Language())
+    HANDLE_EXPR(vocal_parts, VocalParts())
+    HANDLE_EXPR(preview_audio, PreviewAudio())
+    HANDLE_EXPR(preview_art, PreviewArt())
+    HANDLE_EXPR(rating, RatingSym())
+    HANDLE_EXPR(has_preview_audio, HasPreviewAudio())
+    HANDLE_EXPR(has_genre, HasGenre())
+    HANDLE_EXPR(has_vocal_parts, HasVocalParts())
+    HANDLE_EXPR(has_preview_art, HasPreviewArt())
+    HANDLE_EXPR(first_char_artist, FirstCharArtist(_msg->Int(2)))
+    HANDLE_EXPR(first_char_name, FirstCharName(_msg->Int(2)))
+    HANDLE_EXPR(album_link, AlbumLink())
+    HANDLE_EXPR(pack_link, PackLink())
+    HANDLE_EXPR(rating_num, Rating())
+    HANDLE_EXPR(has_rating, Rating() != 0)
+    // HANDLE_EXPR(price, )
+    HANDLE_EXPR(is_downloaded, IsDownloaded())
+    HANDLE_EXPR(is_partially_downloaded, IsPartiallyDownloaded())
+    HANDLE_EXPR(is_partially_purchased, IsPartiallyPurchased())
+    HANDLE_SUPERCLASS(Hmx::Object)
+    HANDLE_CHECK(0x673)
+END_HANDLERS
+#pragma pop
--- src/system/meta/StoreOffer.h
+++ src/system/meta/StoreOffer.h
@@ -1,9 +1,18 @@
 #pragma once
 #include "obj/Object.h"
 #include "meta/SongMgr.h"
+#include "ui/UILabel.h"
+
+enum StoreOfferType {
+    kStoreOfferSong = 0,
+    kStoreOfferAlbum = 1,
+    kStoreOfferPack = 2
+};
 
 class StorePackedOfferBase {
 public:
+    const char* GetName() const;
+
     // 0x0
     unsigned char sometopthing : 1;
     unsigned char mOfferType : 2;
@@ -14,6 +23,18 @@ public:
     unsigned char mGenre : 5;
     unsigned char mCover : 1;
     unsigned char mVocalParts : 2;
+    char filler[2]; // 0x2-3
+    int unk4;
+    int unk8;
+    int unkc;
+    int unk10;
+    int unk14;
+    int unk18;
+    int unk1c;
+    int unk20;
+    int unk24;
+    char unk25;
+    unsigned char mYearReleased; // 0x26
 };
 
 class StorePurchaseable : public Hmx::Object {
@@ -23,6 +44,9 @@ public:
 
     bool Exists() const;
     const char* CostStr() const;
+    bool IsDownloaded() const;
+    bool IsPartiallyDownloaded() const;
+    bool IsPartiallyPurchased() const;
 
     const StorePackedOfferBase* unk1c; // 0x1c - union between PackedOffer and RBNPackedOffer?
     int unk20; // 0x20 - ptr to some struct
@@ -46,6 +70,32 @@ public:
     bool HasVocalParts() const;
     Symbol LengthSym() const;
     Symbol Decade() const;
+    Symbol ShortName() const;
+    const char* OfferName() const;
+    const char* Description() const;
+    DataArray* DescriptionData(UILabel*) const;
+    int YearReleased() const;
+    Symbol SubGenre() const;
+    bool InLibrary() const;
+    bool IsAvailable() const;
+    bool IsPurchased() const;
+    bool IsTest() const;
+    Symbol PackFirstLetter() const;
+    const char* ReleaseDateStr() const;
+    float Review() const;
+    const char* Artist() const;
+    float PartRank(Symbol) const;
+    Symbol Label() const;
+    Symbol Author() const;
+    Symbol Language() const;
+    const char* PreviewAudio() const;
+    const char* PreviewArt() const;
+    Symbol RatingSym() const;
+    bool HasPreviewArt() const;
+    Symbol FirstCharArtist(bool) const;
+    Symbol FirstCharName(bool) const;
+    const char* AlbumLink() const;
+    const char* PackLink() const;
 
     StorePurchaseable mAlbum; // 0x24
     StorePurchaseable mPack; // 0x48
