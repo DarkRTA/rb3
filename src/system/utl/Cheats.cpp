367e0e6d2889eee89b89ea2800fbeb28d61e0710
rjkiv <76180273+rjkiv@users.noreply.github.com>
2024-12-24 15:51:21 -0500

    cheats

---
+++ src/system/utl/Cheats.cpp
@@ -0,0 +1,61 @@
+#include "utl/Cheats.h"
+#include "os/Joypad.h"
+#include "os/UserMgr.h"
+#include "obj/Dir.h"
+
+CheatsManager* gCheatsManager;
+bool sKeyCheatsEnabled = true;
+
+CheatsManager::CheatsManager() : mKeyCheatsEnabled(sKeyCheatsEnabled) {
+    mLastButtonTime.Start();
+    SystemConfig()->FindData("cheats_buffer", mMaxBuffer, true);
+}
+
+void CheatsManager::CallCheatScript(bool b1, DataArray* arr2, LocalUser* user, bool b2){
+    if(!user && TheUserMgr){
+        std::vector<LocalUser*> localUsers;
+        TheUserMgr->GetLocalUsers(localUsers);
+        for(std::vector<LocalUser*>::iterator it = localUsers.begin(); it != localUsers.end(); ++it){
+            if((*it)->GetPadNum() == -1) break;
+            JoypadData* data = JoypadGetPadData((*it)->GetPadNum());
+            if(b1 && b2 && data->mType - 1U > 2 && data->mType - 0x13U > 2){
+                user = *it;
+                break;
+            }
+        }
+    }
+    if(user){
+        switch(JoypadGetPadData(user->GetPadNum())->mType){
+            case kJoypadDigital:
+            case kJoypadAnalog:
+            case kJoypadWiiCore:
+            case kJoypadWiiFS:
+            case kJoypadWiiClassic:
+            case kJoypadDualShock:
+                DataVariable("cheat_pad") = user ? user->GetPadNum() : 0;
+                LogCheat(user ? user->GetPadNum() : -1, b1, arr2);
+                if(b1){
+                    int i = 2;
+                    for(; arr2->Node(i).Type() != kDataCommand && i < arr2->Size(); i++);
+                    if(i < arr2->Size()){
+                        arr2->ExecuteScript(i, nullptr, nullptr, 1);
+                    }
+                }
+                else arr2->Execute();
+                Hmx::Object* uiObj = ObjectDir::Main()->Find<Hmx::Object>("ui", true);
+                static Message msg("cheat_invoked", 0, 0);
+                msg[0] = b1;
+                msg[1] = DataNode(arr2, kDataArray);
+                uiObj->Handle(msg, false);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+BEGIN_HANDLERS(CheatsManager)
+    HANDLE_MESSAGE(ButtonDownMsg)
+    HANDLE_MESSAGE(KeyboardKeyMsg)
+    HANDLE_CHECK(0x115)
+END_HANDLERS
--- src/system/utl/Cheats.h
+++ src/system/utl/Cheats.h
@@ -1,8 +1,55 @@
 #pragma once
 #include "utl/Symbol.h"
+#include "obj/Object.h"
+#include "os/Timer.h"
+#include "os/JoypadMsgs.h"
+#include "os/Keyboard.h"
 
 class DataArray;
 class LocalUser;
+
+struct QuickJoyCheat {
+    int unk0;
+    int unk4;
+};
+
+struct LongJoyCheat {
+
+};
+
+struct KeyCheat {
+
+};
+
+class CheatsManager : public Hmx::Object {
+public:
+    enum ShiftMode {
+        kLeftShift = 0,
+        kRightShift = 1
+    };
+
+    CheatsManager();
+    virtual ~CheatsManager(){}
+    virtual DataNode Handle(DataArray*, bool);
+
+    void CallCheatScript(bool, DataArray*, LocalUser*, bool);
+    bool OnMsg(const ButtonDownMsg&);
+    
+    DataNode OnMsg(const KeyboardKeyMsg&);
+
+    std::vector<LongJoyCheat> mLongJoyCheats; // 0x1c
+    std::vector<QuickJoyCheat> mQuickJoyCheats[2]; // 0x20
+    std::vector<int> mKeyCheats; // 0x34
+    Symbol mSymMode; // 0x3c
+    std::vector<QuickJoyCheat*> mJoyCheatPtrsMode[2]; // 0x40
+    std::vector<int> mKeyCheatPtrsMode; // 0x50
+    Timer mLastButtonTime; // 0x58
+    bool mKeyCheatsEnabled; // 0x88
+    std::list<int> mBuffer; // 0x8c
+    int mMaxBuffer; // 0x94
+};
+
+extern CheatsManager* gCheatsManager;
 
 void EnableKeyCheats(bool);
 bool GetEnabledKeyCheats();
@@ -10,3 +57,4 @@ void CallQuickCheat(DataArray*, LocalUser*);
 bool CheatsInitialized();
 Symbol GetCheatMode();
 void AppendCheatsLog(char*);
+void LogCheat(int, bool, DataArray*);
