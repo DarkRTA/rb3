deb7371cb58aec939e8fd7dc7c45e79b7b737aaf
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-01-01 16:19:53 -0500

    block stuff

--- src/system/os/AsyncTask.h
+++ src/system/os/AsyncTask.h
@@ -1,21 +1,20 @@
-#ifndef OS_ASYNCTASK_H
-#define OS_ASYNCTASK_H
+#pragma once
 
 class ArkFile;
 
 class AsyncTask {
 public:
-    int mArkfileNum;
-    int mBlockNum;
-    int mOffsetStart;
-    int mOffsetEnd;
-    void* mBuffer;
-    const char* mStr;
-    ArkFile* mOwner;
+    int mArkfileNum; // 0x0
+    int mBlockNum; // 0x4
+    int mOffsetStart; // 0x8
+    int mOffsetEnd; // 0xc
+    void* mBuffer; // 0x10
+    const char* mStr; // 0x14
+    ArkFile* mOwner; // 0x18
 
     AsyncTask(ArkFile*, void*, int, int, int, int, const char*);
     AsyncTask(int, int);
     bool FillData();
+    int GetBlockNum() const { return mBlockNum; }
+    const char* GetStr() const { return mStr; }
 };
-
-#endif
