9b4d05be5dca535acf80d3ff2c0f81a0f1a7c8cf
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-01-27 11:42:07 -0700

    link timespanvector

--- src/system/beatmatch/TimeSpanVector.cpp
+++ src/system/beatmatch/TimeSpanVector.cpp
@@ -1,5 +1,16 @@
 #include "beatmatch/TimeSpanVector.h"
+#include "os/Debug.h"
+#include <utility>
 
-void IntersectTimeSpans(const std::vector<std::pair<float, float> >&, const std::vector<std::pair<float, float> >&, std::vector<std::pair<float, float> >&){
-    
+void IntersectTimeSpans(const std::vector<std::pair<float, float> >& v1, const std::vector<std::pair<float, float> >& v2, std::vector<std::pair<float, float> >& o_rIntersection){
+    MILO_ASSERT(o_rIntersection.empty(), 0x14);
+    for(std::vector<std::pair<float, float> >::const_iterator it1 = v1.begin(), it2 = v2.begin(); it1 != v1.end() && it2 != v2.end(); ){
+        float max1 = Max(it1->first, it2->first);
+        float min2 = Min(it1->second, it2->second);
+        if(max1 < min2){
+            o_rIntersection.push_back(std::make_pair(max1, min2));
+        }
+        if(it1->second <= min2) ++it1;
+        if(it2->second <= min2) ++it2;
+    }
 }
