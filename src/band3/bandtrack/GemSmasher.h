a36a1279dbacde74faa8de5f30397b8dbc1fa9a4
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-02-05 13:45:36 -0700

    gemsmasher refactor

--- src/band3/bandtrack/GemSmasher.h
+++ src/band3/bandtrack/GemSmasher.h
@@ -10,53 +10,55 @@ public:
     ~GemSmasher();
     void Reset(bool);
     RndDir *Dir() const { return mDir; }
+    bool Null() const;
+    bool Showing() const;
+    void Hit();
+    void HitDrum();
+    void HitDrumBonus();
+    void HitCymbal();
+    void HitCymbalBonus();
+    void HitChord();
+    void HitChordBonus();
+    void HitBonus();
+    void CodaHit();
+    void CodaHitChord();
+    void Miss();
+    void Burn();
+    void BurnBonus();
+    void BurnChord();
+    void CodaBurn();
+    void CodaBurnChord();
+    void StopBurn();
+    void FillHit(int);
+    bool Glowing() const;
+    void SetGlowing(bool);
 
     int mSlot; // 0x0
-    RndDir *mDir;
-    bool Null() const;
-    bool Showing() const; // 0x4
-    EventTrigger *mHitSmashTrig;
-    void Hit(); // 0x8
-    EventTrigger *mHitDrumsTrig;
-    void HitDrum(); // 0xC
-    EventTrigger *mHitDrumsBonus;
-    void HitDrumBonus(); // 0x10
-    EventTrigger *mHitCymsTrig;
-    void HitCymbal(); // 0x14
-    EventTrigger *mHitCymsBonus;
-    void HitCymbalBonus(); // 0x18
-    EventTrigger *mHitChordTrig;
-    void HitChord(); // 0x1C
-    EventTrigger *mHitChordBonus;
-    void HitChordBonus(); // 0x20
-    EventTrigger *mHitBonus;
-    void HitBonus(); // 0x24
-    EventTrigger *mBREHitTrig;
-    void CodaHit(); // 0x28
-    EventTrigger *mBREHitChord;
-    void CodaHitChord(); // 0x2C
-    EventTrigger *mMissTrig;
-    void Miss(); // 0x30
-    EventTrigger *mBurnTrig;
-    void Burn(); // 0x34
-    EventTrigger *mBurnBonusTrig;
-    void BurnBonus(); // 0x38
-    EventTrigger *mBurnChordTrig;
-    void BurnChord(); // 0x3C
-    EventTrigger *mBREBurnTrig;
-    void CodaBurn(); // 0x40
-    EventTrigger *mBREBurnChord;
-    void CodaBurnChord(); // 0x44
-    EventTrigger *mStopBurnTrig;
-    void StopBurn(); // 0x48
-    EventTrigger *mHitFillTrig;
-    void FillHit(int); // 0x4C
+    RndDir *mDir; // 0x4
+    EventTrigger *mHitSmashTrig; // 0x8
+    EventTrigger *mHitDrumsTrig; // 0xc
+    EventTrigger *mHitDrumsBonus; // 0x10
+    EventTrigger *mHitCymsTrig; // 0x14
+    EventTrigger *mHitCymsBonus; // 0x18
+    EventTrigger *mHitChordTrig; // 0x1c
+    EventTrigger *mHitChordBonus; // 0x20
+    EventTrigger *mHitBonus; // 0x24
+    EventTrigger *mBREHitTrig; // 0x28
+    EventTrigger *mBREHitChord; // 0x2c
+    EventTrigger *mMissTrig; // 0x30
+    EventTrigger *mBurnTrig; // 0x34
+    EventTrigger *mBurnBonusTrig; // 0x38
+    EventTrigger *mBurnChordTrig; // 0x3c
+    EventTrigger *mBREBurnTrig; // 0x40
+    EventTrigger *mBREBurnChord; // 0x44
+    EventTrigger *mStopBurnTrig; // 0x48
+    EventTrigger *mHitFillTrig; // 0x4c
     std::vector<EventTrigger *> mMoreTriggers; // 0x50
     EventTrigger *mPressTrig; // 0x58
     EventTrigger *mReleaseTrig; // 0x5C
     EventTrigger *mMissAfterRelease; // 0x60
     RndMesh *mGemSmasherGlow; // 0x64
-    bool mGlowing, mBurning, unk_0x6A; // 0x6a - is keyboard track?
-    bool Glowing() const;
-    void SetGlowing(bool); // 0x68, 0x69, 0x6A
+    bool mGlowing; // 0x68
+    bool mBurning; // 0x69
+    bool mIsKeyboardTrack; // 0x6a - is keyboard track?
 };
