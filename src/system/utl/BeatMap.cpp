9a91db651f980a5444d07d949ad1456b7678469b
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-04-07 13:22:22 -0700

    beatmap funcs and doc

--- src/system/utl/BeatMap.cpp
+++ src/system/utl/BeatMap.cpp
@@ -20,6 +20,8 @@ bool BeatMap::AddBeat(int tick, int level) {
         return false;
 }
 
+// https://decomp.me/scratch/h18a4
+// matches in retail with the right inline settings
 float BeatMap::Beat(int tick) const {
     if (mInfos.empty())
         return (float)tick / 480.0f;
@@ -32,34 +34,64 @@ float BeatMap::Beat(int tick) const {
             if (tick >= mInfos[i2 - 1].mTick)
                 i2 = mInfos.size() - 2;
             else {
-                const BeatInfo &frontInfo = mInfos.front();
                 const BeatInfo *lowerInfo =
                     std::lower_bound(mInfos.begin(), mInfos.end(), tick, BeatInfoCmp);
-                i2 = lowerInfo - &frontInfo - 1;
+                i2 = lowerInfo - &mInfos.front() - 1;
             }
         }
         return Interpolate(tick, i2);
     }
 }
 
+// also matches in retail with the right inline settings
 float BeatMap::Beat(float tick) const {
     if (mInfos.empty())
         return tick / 480.0f;
+
+    int firstTick = mInfos[0].mTick;
+    int i2 = tick;
+    if (i2 <= firstTick)
+        i2 = 0;
+    else if (i2 >= mInfos[mInfos.size() - 1].mTick)
+        i2 = mInfos.size() - 2;
     else {
-        int i2 = tick;
-        if (tick < mInfos[0].mTick)
+        const BeatInfo &frontInfo = mInfos.front();
+        int sp08 = i2;
+        const BeatInfo *lowerInfo =
+            std::lower_bound(mInfos.begin(), mInfos.end(), sp08, BeatInfoCmp);
+        i2 = lowerInfo - &frontInfo - 1;
+    }
+    return Interpolate(tick, i2);
+}
+
+float BeatMap::BeatToTick(float f1) const {
+    if (mInfos.empty())
+        return f1 * 480.0f;
+    else {
+        int i2;
+        if (f1 < 0) {
             i2 = 0;
-        else {
-            i2 = mInfos.size();
-            if (tick > mInfos[i2 - 1].mTick)
+        } else {
+            if (f1 > mInfos.size() - 2) {
                 i2 = mInfos.size() - 2;
-            else {
-                const BeatInfo &frontInfo = mInfos.front();
-                const BeatInfo *lowerInfo =
-                    std::lower_bound(mInfos.begin(), mInfos.end(), tick, BeatInfoCmp);
-                i2 = lowerInfo - &frontInfo - 1;
+            } else {
+                i2 = f1;
             }
         }
-        return Interpolate(tick, i2);
+
+        const BeatInfo &r30 = mInfos[i2];
+        const BeatInfo &r31 = mInfos[i2 + 1];
+        int k1 = mInfos[i2].mTick;
+        return static_cast<float>(f1 - i2) * static_cast<float>(r31.mTick - r30.mTick)
+            + k1;
     }
+}
+
+bool BeatMap::IsDownbeat(int i1) const {
+    if (mInfos.empty())
+        return i1 % 4 == 0;
+    else if (i1 >= mInfos.size()) {
+        return false;
+    } else
+        return mInfos[i1].mLevel > 0;
 }
