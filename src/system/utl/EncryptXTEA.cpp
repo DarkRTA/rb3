f2229a07a8690c1943d4429625067b79aa6189b6
jsenior10 <jacobsenior4@gmail.com>
2025-05-25 02:42:03 +0100

    XTEA cleanup

--- src/system/utl/EncryptXTEA.cpp
+++ src/system/utl/EncryptXTEA.cpp
@@ -9,20 +9,19 @@ XTEABlockEncrypter::XTEABlockEncrypter() {
 
 void XTEABlockEncrypter::SetKey(const unsigned char *uc) { memcpy(mKey, uc, 0x10); }
 
-void XTEABlockEncrypter::SetNonce(const unsigned long long *ull, unsigned int ui) {
-    mNonce[0] = ull[0] + ui;
-    mNonce[1] = ull[1] + ui;
+void XTEABlockEncrypter::SetNonce(const unsigned long long *nonce, unsigned int shift) {
+    mNonce[0] = nonce[0] + shift;
+    mNonce[1] = nonce[1] + shift;
 }
 
 unsigned long long
-XTEABlockEncrypter::Encipher(unsigned long long ull, unsigned int *ui) {
-    unsigned long v[2] = { (ull >> 32), (ull & 0xFFFFFFFF) }; // only god knows if this is
-                                                                      // right
+XTEABlockEncrypter::Encipher(unsigned long long nonce, unsigned int *key) {
+    unsigned long v[2] = { (nonce >> 32), (nonce & 0xFFFFFFFF) }; // only god knows if this is right
     unsigned int sum = 0;
     for (int i = 0; i < 4; i++) {
-        v[1] += (v[0] + (v[0] << 4 ^ v[0] >> 5)) ^ sum + (ui[(sum & 3)]);
+        v[1] += (v[0] + (v[0] << 4 ^ v[0] >> 5)) ^ sum + (key[(sum & 3)]);
         sum += 0x9E3779B9;
-        v[0] += (v[1] + (v[1] << 4 ^ v[1] >> 5)) ^ sum + ui[(sum >> 11) & 3];
+        v[0] += (v[1] + (v[1] << 4 ^ v[1] >> 5)) ^ sum + key[(sum >> 11) & 3];
     }
     return *v;
 }
