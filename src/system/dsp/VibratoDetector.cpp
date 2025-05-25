b1819d406438fdf189e3f6407417e7efd5c5cc35
jsenior10 <jacobsenior4@gmail.com>
2025-05-25 02:37:06 +0100

    named variable

--- src/system/dsp/VibratoDetector.cpp
+++ src/system/dsp/VibratoDetector.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 
 
+
 VibratoDetector::VibratoDetector(int i1, int i2)
     : mBufIdx(0), mHi(0), mY0(0.0), mY1(0.0), mY2(0.0), mSample(0) ,mLastDetect(0),
     mMaxPeriod(i2), mMinPeriod(i1)
@@ -16,7 +17,7 @@ VibratoDetector::~VibratoDetector() {
 }
 
 int VibratoDetector::Analyze(float f1) {
-    int v3 = 0;
+    int vibratoLength = 0;
     //double v5, v7;
     if (f1 == 0.0) {
         ++mSample;
@@ -24,20 +25,19 @@ int VibratoDetector::Analyze(float f1) {
     }
 
     else {
-        //v5 = (0.300000001f * f1) + (1.0f - 0.300000001f) * unk30;
         mY0 = (0.300000001f * f1) + (1.0f - 0.300000001f) * mY0;
         if(mY1 > mY0 && mY1 > mY2 || (mY1 < mY0) && mY1 < mY2) {
             mBuffer[mBufIdx % 5] = mSample;
             mPitches[mBufIdx++ % 5] = mY1;
             if(Detect()) {
-
+                 // missing something here
                 mLastDetect = mSample;
             }
         }
         mY2 = mY1;
         mY1 = mY0;
         ++mSample;
-        return v3;
+        return vibratoLength;
     }
 }
 
