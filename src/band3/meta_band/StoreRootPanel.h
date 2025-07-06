3b0edc20908a5fc32b4a31ecd4eeca5c4db99a44
jsenior10 <jacobsenior4@gmail.com>
2025-07-06 01:35:21 +0100

    StoreRootPanel partial

---
+++ src/band3/meta_band/StoreRootPanel.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "obj/Data.h"
+#include "rndobj/Overlay.h"
+#include "ui/UIPanel.h"
+#include "BandSongMetadata.h"
+
+class StoreRootPanel : public UIPanel {
+public:
+    StoreRootPanel();
+    OBJ_CLASSNAME(StoreRootPanel);
+    OBJ_SET_TYPE(StoreRootPanel);
+    virtual ~StoreRootPanel();
+    virtual DataNode Handle(DataArray *, bool);
+    virtual void Enter();
+    virtual void Exit();
+    virtual void Unload();
+
+    DataNode OnMsg(const MetadataLoadedMsg&);
+
+    State mMetadataState;
+    DataArray *mDLCMetadata;
+    DataArray *mUGCMetadata;
+};
+
+int InitStoreOverlay();
+extern RndOverlay *gStoreUIOverlay; 
